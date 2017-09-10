43dd5a23574af3fc993218fb90721a022a0f2c98
Brandon Williams <mbw234@gmail.com>
2017-09-10 10:53:51 -0400

    clean upg

--- Tests/PointFreeTests/LaunchSignupTests.swift
+++ Tests/PointFreeTests/LaunchSignupTests.swift
@@ -10,7 +10,7 @@ import HttpPipelineTestSupport
 
 class LaunchSignupTests: TestCase {
   func testHome() {
-    let request = URLRequest(url: URL(string: "http://localhost:8080/")!)
+    let request = URLRequest(url: URL(string: "https://localhost:8080/")!)
 
     let conn = connection(from: request)
     let result = conn |> siteMiddleware
@@ -19,7 +19,7 @@ class LaunchSignupTests: TestCase {
   }
 
   func testHome_SuccessfulSignup() {
-    let request = URLRequest(url: URL(string: "http://localhost:8080/?success=true")!)
+    let request = URLRequest(url: URL(string: "https://localhost:8080/?success=true")!)
 
     let conn = connection(from: request)
     let result = conn |> siteMiddleware
@@ -28,7 +28,7 @@ class LaunchSignupTests: TestCase {
   }
 
   func testSignup() {
-    var request = URLRequest(url: URL(string: "http://localhost:8080/launch-signup")!)
+    var request = URLRequest(url: URL(string: "https://localhost:8080/launch-signup")!)
     request.httpMethod = "POST"
     request.httpBody = "email=hello@pointfree.co".data(using: .utf8)
 
