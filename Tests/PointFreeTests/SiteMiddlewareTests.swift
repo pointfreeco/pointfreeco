ec96ac9f0f9c8cc3e236cbf6f95152718a740b96
Brandon Williams <mbw234@gmail.com>
2017-09-11 18:46:57 -0400

    proper redirects for heroku (#8)
    * proper redirects for heroku
    
    * boop

--- Tests/PointFreeTests/SiteMiddlewareTests.swift
+++ Tests/PointFreeTests/SiteMiddlewareTests.swift
@@ -7,35 +7,41 @@ import XCTest
 @testable import PointFree
 @testable import HttpPipeline
 import HttpPipelineTestSupport
+import Optics
+
+private func secureRequest(_ urlString: String) -> URLRequest {
+  return URLRequest(url: URL(string: urlString)!)
+    |> \.allHTTPHeaderFields .~ ["X-Forwarded-Proto": "https"]
+}
 
 class SiteMiddlewareTests: TestCase {
   func testWithoutWWW() {
     assertSnapshot(
-      matching: connection(from: URLRequest(url: URL(string: "https://pointfree.co")!)) |> siteMiddleware
+      matching: connection(from: secureRequest("https://pointfree.co")) |> siteMiddleware
     )
 
     assertSnapshot(
-      matching: connection(from: URLRequest(url: URL(string: "https://pointfree.co/episodes")!)) |> siteMiddleware
+      matching: connection(from: secureRequest("https://pointfree.co/episodes")) |> siteMiddleware
     )
   }
 
   func testWithoutHeroku() {
     assertSnapshot(
-      matching: connection(from: URLRequest(url: URL(string: "https://pointfreeco.herokuapp.com")!)) |> siteMiddleware
+      matching: connection(from: secureRequest("https://pointfreeco.herokuapp.com")) |> siteMiddleware
     )
 
     assertSnapshot(
-      matching: connection(from: URLRequest(url: URL(string: "https://pointfreeco.herokuapp.com/episodes")!)) |> siteMiddleware
+      matching: connection(from: secureRequest("https://pointfreeco.herokuapp.com/episodes")) |> siteMiddleware
     )
   }
 
   func testWithWWW() {
     assertSnapshot(
-      matching: connection(from: URLRequest(url: URL(string: "https://www.pointfree.co")!)) |> siteMiddleware
+      matching: connection(from: secureRequest("https://www.pointfree.co")) |> siteMiddleware
     )
 
     assertSnapshot(
-      matching: connection(from: URLRequest(url: URL(string: "https://www.pointfree.co/episodes")!)) |> siteMiddleware
+      matching: connection(from: secureRequest("https://www.pointfree.co/episodes")) |> siteMiddleware
     )
   }
 
