e0f5d67573875510cf5bf4fb545bf55194c2d4fc
Brandon Williams <mbw234@gmail.com>
2017-09-12 19:51:24 -0400

    Helper for webview snapshots

--- Tests/PointFreeTests/LaunchSignupTests.swift
+++ Tests/PointFreeTests/LaunchSignupTests.swift
@@ -8,6 +8,49 @@ import XCTest
 @testable import HttpPipeline
 import HttpPipelineTestSupport
 
+#if os(iOS)
+  import UIKit
+
+let sizes = [
+  CGSize(width: 320, height: 568),
+  CGSize(width: 375, height: 667),
+  CGSize(width: 768, height: 1024),
+  CGSize(width: 800, height: 600),
+]
+#endif
+
+extension XCTestCase {
+  func assertWebPageSnapshot<I>(
+    matching conn: Conn<I, Data?>,
+    named name: String? = nil,
+    record recording: Bool = SnapshotTesting.record,
+    file: StaticString = #file,
+    function: String = #function,
+    line: UInt = #line) {
+    #if os(iOS)
+      sizes.forEach { size in
+        let webView = UIWebView(frame: .init(origin: .zero, size: size))
+        webView.loadHTMLString(String(data: conn.response.body!, encoding: .utf8)!, baseURL: nil)
+        let exp = expectation(description: "webView")
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+          assertSnapshot(
+            matching: webView,
+            named: (name ?? "") + "_\(size.width)x\(size.height)",
+            record: record,
+            file: file,
+            function: function,
+            line: line
+          )
+          exp.fulfill()
+        }
+
+        waitForExpectations(timeout: 4, handler: nil)
+      }
+    #endif
+  }
+}
+
 class LaunchSignupTests: TestCase {
   func testHome() {
     let request = URLRequest(url: URL(string: "http://localhost:8080/")!)
@@ -15,6 +58,7 @@ class LaunchSignupTests: TestCase {
     let conn = connection(from: request)
     let result = conn |> siteMiddleware
 
+    assertWebPageSnapshot(matching: result)
     assertSnapshot(matching: result)
   }
 
@@ -24,6 +68,7 @@ class LaunchSignupTests: TestCase {
     let conn = connection(from: request)
     let result = conn |> siteMiddleware
 
+    assertWebPageSnapshot(matching: result)
     assertSnapshot(matching: result)
   }
 
