9f0526f74698aaefa113228fe24fd967549b7481
Brandon Williams <mbw234@gmail.com>
2017-09-12 20:27:58 -0400

    Basics of screenshot testing! (#13)
    * Helper for webview snapshots
    
    * added open source callout
    
    * Revert "added open source callout"
    
    This reverts commit 7dffa614e66ccab7285d1bbc5dcf4e25def7cb0f.
    
    * fix snaps
    
    * fix timeout
    
    * update circle
    
    * hack
    
    * New screens
    
    
    Former-commit-id: 3c706dda7fa221797a5bcf094c925d9a9a6b229e

--- Tests/PointFreeTests/LaunchSignupTests.swift
+++ Tests/PointFreeTests/LaunchSignupTests.swift
@@ -8,6 +8,49 @@ import XCTest
 @testable import HttpPipeline
 import HttpPipelineTestSupport
 
+#if os(iOS)
+  import UIKit
+
+let sizes = [
+  CGSize(width: 320, height: 568),
+  CGSize(width: 375, height: 667),
+  CGSize(width: 768, height: 1024),
+  CGSize(width: 800, height: 600),
+]
+#endif
+
+extension XCTestCase {
+  func assertWebPageSnapshot<I>(
+    matching conn: Conn<I, Data?>,
+    named name: String? = nil,
+    record recording: Bool = SnapshotTesting.record,
+    file: StaticString = #file,
+    function: String = #function,
+    line: UInt = #line) {
+    #if os(iOS)
+      sizes.forEach { size in
+        let webView = UIWebView(frame: .init(origin: .zero, size: size))
+        webView.loadHTMLString(String(data: conn.response.body!, encoding: .utf8)!, baseURL: nil)
+        let exp = expectation(description: "webView")
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+          assertSnapshot(
+            matching: webView,
+            named: (name ?? "") + "_\(size.width)x\(size.height)",
+            record: record,
+            file: file,
+            function: function,
+            line: line
+          )
+          exp.fulfill()
+        }
+
+        waitForExpectations(timeout: 4, handler: nil)
+      }
+    #endif
+  }
+}
+
 class LaunchSignupTests: TestCase {
   func testHome() {
     let request = URLRequest(url: URL(string: "http://localhost:8080/")!)
@@ -16,6 +59,7 @@ class LaunchSignupTests: TestCase {
     let result = conn |> siteMiddleware
 
     assertSnapshot(matching: result)
+    assertWebPageSnapshot(matching: result)
   }
 
   func testHome_SuccessfulSignup() {
@@ -25,6 +69,7 @@ class LaunchSignupTests: TestCase {
     let result = conn |> siteMiddleware
 
     assertSnapshot(matching: result)
+    assertWebPageSnapshot(matching: result)
   }
 
   func testSignup() {
