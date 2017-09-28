41db450ed20a4b68f9c2d5c9243d4376dc30c458
Brandon Williams <mbw234@gmail.com>
2017-09-28 10:46:47 +0200

    wip

--- Sources/PointFree/EnvVars.swift
+++ Sources/PointFree/EnvVars.swift
@@ -5,4 +5,10 @@ enum EnvVars {
   static let airbaseBase2 = ProcessInfo.processInfo.environment["AIRTABLE_BASE_2"] ?? "deadbeef-base-2"
   static let airbaseBase3 = ProcessInfo.processInfo.environment["AIRTABLE_BASE_3"] ?? "deadbeef-base-3"
   static let airtableBearer = ProcessInfo.processInfo.environment["AIRTABLE_BEARER"] ?? "deadbeef-bearer"
+
+
+  enum GitHub {
+    static let clientId = ProcessInfo.processInfo.environment["GITHUB_CLIENT_ID"] ?? "deadbeef-gh-client-id"
+    static let clientSecret = ProcessInfo.processInfo.environment["GITHUB_CLIENT_SECRET"] ?? "deadbeef-gh-client-secret"
+  }
 }
