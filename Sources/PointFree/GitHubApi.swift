6db45fb149fe5280076a8b022b3526e976706cae
Brandon Williams <mbw234@gmail.com>
2017-10-19 09:08:16 -0400

    Add GitHub auth! (#21)
    * wip
    
    * wip
    
    * clean
    
    * tests and clean up
    
    * protect new routes with basic auth
    
    * clean
    
    * comments
    
    * clean
    
    * tests for secret home
    
    * added screenshot tests
    
    * Revert "added screenshot tests"
    
    This reverts commit 5fd70f342bf74a19dfb8a010b70f51792c9c6e61 [formerly 4798223b1f72fd455532a23d3eb504e1544ff3d7].
    
    * rename some stuff
    
    * cleanup
    
    * clean
    
    * url session
    
    * typo
    
    
    Former-commit-id: 575032adade5fe8e59cc554cb1a75ddb958e9d6c

---
+++ Sources/PointFree/GitHubApi.swift
@@ -0,0 +1,109 @@
+import Either
+import Foundation
+import Optics
+import Prelude
+
+public struct GitHubAccessToken: Codable {
+  let accessToken: String
+
+  enum CodingKeys: String, CodingKey {
+    case accessToken = "access_token"
+  }
+}
+
+public struct GitHubUser: Codable {
+  let email: String
+  let id: Int
+  let name: String
+}
+
+public struct GitHubUserEnvelope: Codable {
+  let accessToken: GitHubAccessToken
+  let gitHubUser: GitHubUser
+}
+
+/// Fetches an access token from GitHub from a `code` that was obtained from the callback redirect.
+func fetchAuthToken(forCode code: String) -> EitherIO<Prelude.Unit, GitHubAccessToken> {
+
+  let request = URLRequest(url: URL(string: "https://github.com/login/oauth/access_token")!)
+    |> \.httpMethod .~ "POST"
+    |> \.httpBody .~ (try? JSONEncoder().encode(
+      [
+        "client_id": EnvVars.GitHub.clientId,
+        "client_secret": EnvVars.GitHub.clientSecret,
+        "code": code,
+        "accept": "json"
+      ]))
+    |> \.allHTTPHeaderFields .~ [
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+  ]
+
+  return .init(
+    run: .init { callback in
+      session
+        .dataTask(with: request) { data, response, error in
+          callback(
+            data.flatMap { try? JSONDecoder().decode(GitHubAccessToken.self, from: $0) }
+              .map(Either.right)
+              ?? Either.left(unit)
+          )
+        }
+        .resume()
+    }
+  )
+}
+
+func mockFetchAuthToken(
+  result: Either<Prelude.Unit, GitHubAccessToken>
+  )
+  -> (String)
+  -> EitherIO<Prelude.Unit, GitHubAccessToken> {
+    return { code in
+      return .init(
+        run: .init { callback in
+          callback(result)
+        }
+      )
+    }
+}
+
+/// Fetches a GitHub user from an access token.
+func fetchGitHubUser(accessToken: GitHubAccessToken) -> EitherIO<Prelude.Unit, GitHubUser> {
+
+  let request = URLRequest(url: URL(string: "https://api.github.com/user")!)
+    |> \.allHTTPHeaderFields .~ [
+      "Authorization": "token \(accessToken.accessToken)",
+      "Accept": "application/vnd.github.v3+json"
+  ]
+
+  return .init(
+    run: .init { callback in
+      session
+        .dataTask(with: request) { data, response, error in
+          callback(
+            data.flatMap { try? JSONDecoder().decode(GitHubUser.self, from: $0) }
+              .map(Either.right)
+              ?? Either.left(unit)
+          )
+        }
+        .resume()
+    }
+  )
+}
+
+func mockFetchGithubUser(
+  result: Either<Prelude.Unit, GitHubUser>
+  )
+  -> (GitHubAccessToken)
+  -> EitherIO<Prelude.Unit, GitHubUser> {
+    return { code in
+      return .init(
+        run: .init { callback in
+          callback(result)
+        }
+      )
+    }
+}
+
+private let session = URLSession(configuration: .default)
---
+++ Tests/PointFreeTests/AuthTests.swift
@@ -0,0 +1,108 @@
+import HttpPipeline
+import HttpPipelineTestSupport
+import Optics
+@testable import PointFree
+import Prelude
+import SnapshotTesting
+import XCTest
+
+class AuthTests: TestCase {
+  func testAuth() {
+    let request = URLRequest(url: URL(string: "http://localhost:8080/github-auth?code=deadbeef")!)
+      |> \.allHTTPHeaderFields .~ [
+        "Authorization": "Basic " + Data("hello:world".utf8).base64EncodedString()
+    ]
+
+    let conn = connection(from: request)
+    let result = conn |> siteMiddleware
+
+    assertSnapshot(matching: result)
+  }
+
+  func testAuth_WithFetchAuthTokenFailure() {
+    AppEnvironment.with(fetchAuthToken: mockFetchAuthToken(result: .left(unit))) {
+      let request = URLRequest(url: URL(string: "http://localhost:8080/github-auth?code=deadbeef")!)
+        |> \.allHTTPHeaderFields .~ [
+          "Authorization": "Basic " + Data("hello:world".utf8).base64EncodedString()
+      ]
+
+      let conn = connection(from: request)
+      let result = conn |> siteMiddleware
+
+      assertSnapshot(matching: result)
+    }
+  }
+
+  func testAuth_WithFetchUserFailure() {
+    AppEnvironment.with(fetchGitHubUser: mockFetchGithubUser(result: .left(unit))) {
+      let request = URLRequest(url: URL(string: "http://localhost:8080/github-auth?code=deadbeef")!)
+        |> \.allHTTPHeaderFields .~ [
+          "Authorization": "Basic " + Data("hello:world".utf8).base64EncodedString()
+      ]
+
+      let conn = connection(from: request)
+      let result = conn |> siteMiddleware
+
+      assertSnapshot(matching: result)
+    }
+  }
+
+  func testLogin() {
+    let request = URLRequest(url: URL(string: "http://localhost:8080/login")!)
+      |> \.allHTTPHeaderFields .~ [
+        "Authorization": "Basic " + Data("hello:world".utf8).base64EncodedString()
+    ]
+
+    let conn = connection(from: request)
+    let result = conn |> siteMiddleware
+
+    assertSnapshot(matching: result)
+  }
+
+  func testLogout() {
+    let request = URLRequest(url: URL(string: "http://localhost:8080/logout")!)
+      |> \.allHTTPHeaderFields .~ [
+        "Cookie": "github_session=deadbeef; HttpOnly; Secure",
+        "Authorization": "Basic " + Data("hello:world".utf8).base64EncodedString()
+    ]
+
+    let conn = connection(from: request)
+    let result = conn |> siteMiddleware
+
+    assertSnapshot(matching: result)
+  }
+
+  func testSecretHome_LoggedOut() {
+    let request = URLRequest(url: URL(string: "http://localhost:8080/home")!)
+      |> \.allHTTPHeaderFields .~ [
+        "Authorization": "Basic " + Data("hello:world".utf8).base64EncodedString()
+    ]
+
+    let conn = connection(from: request)
+    let result = conn |> siteMiddleware
+
+    assertSnapshot(matching: result)
+  }
+
+  func testSecretHome_LoggedIn() {
+    let sessionCookie = """
+    8ec59947a58e227d0901ff96530b96c9b1e419ddd6781c6bacf97acdf784529d\
+    bd79e710821f29430d5df62438730123873e105aacaa6374bd5db7f4e937432c\
+    c9e0837bc28d511d5f8cde122ed8e18be42e2148cfb2ed1620bcd3418a6c93a6\
+    a801ee0c463888e40a0838d0ebc94c7fc1e30e130423b84edfcdccbdde146731\
+    45a2cf192aed0443ceee4ca5d283e12a2319a0a733c66f9f83514ec9622db1f8\
+    f864744d82395e50e1e4f5f480d28af3182f782db235c45a9d68cb8876328f33
+    """
+
+    let request = URLRequest(url: URL(string: "http://localhost:8080/home")!)
+      |> \.allHTTPHeaderFields .~ [
+        "Cookie": "github_session=\(sessionCookie)",
+        "Authorization": "Basic " + Data("hello:world".utf8).base64EncodedString()
+    ]
+
+    let conn = connection(from: request)
+    let result = conn |> siteMiddleware
+
+    assertSnapshot(matching: result)
+  }
+}
