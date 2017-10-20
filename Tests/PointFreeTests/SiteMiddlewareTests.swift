47546469e1e83661caaf247d4e79e3bdf8f5621c
Brandon Williams <mbw234@gmail.com>
2017-10-20 17:10:33 -0400

    Update swift web to use new router (#23)
    * wip
    
    * wip
    
    * verified diffs
    
    * fixing, kinda
    
    * fix tests
    
    * clean up
    
    * 11.0.1
    
    
    Former-commit-id: 52b8072a47ae418f1fd3f0ca6d5215616b59359e

--- Tests/PointFreeTests/SiteMiddlewareTests.swift
+++ Tests/PointFreeTests/SiteMiddlewareTests.swift
@@ -17,57 +17,79 @@ private func secureRequest(_ urlString: String) -> URLRequest {
 class SiteMiddlewareTests: TestCase {
   func testWithoutWWW() {
     assertSnapshot(
-      matching: connection(from: secureRequest("https://pointfree.co")) |> siteMiddleware
+      matching: connection(from: secureRequest("https://pointfree.co"))
+        |> siteMiddleware
+        |> Prelude.perform
     )
 
     assertSnapshot(
-      matching: connection(from: secureRequest("https://pointfree.co/episodes")) |> siteMiddleware
+      matching: connection(from: secureRequest("https://pointfree.co/episodes"))
+        |> siteMiddleware
+        |> Prelude.perform
     )
   }
 
   func testWithoutHeroku() {
     assertSnapshot(
-      matching: connection(from: secureRequest("https://pointfreeco.herokuapp.com")) |> siteMiddleware
+      matching: connection(from: secureRequest("https://pointfreeco.herokuapp.com"))
+        |> siteMiddleware
+        |> Prelude.perform
     )
 
     assertSnapshot(
-      matching: connection(from: secureRequest("https://pointfreeco.herokuapp.com/episodes")) |> siteMiddleware
+      matching: connection(from: secureRequest("https://pointfreeco.herokuapp.com/episodes"))
+        |> siteMiddleware
+        |> Prelude.perform
     )
   }
 
   func testWithWWW() {
     assertSnapshot(
-      matching: connection(from: secureRequest("https://www.pointfree.co")) |> siteMiddleware
+      matching: connection(from: secureRequest("https://www.pointfree.co"))
+        |> siteMiddleware
+        |> Prelude.perform
     )
 
     assertSnapshot(
-      matching: connection(from: secureRequest("https://www.pointfree.co/episodes")) |> siteMiddleware
+      matching: connection(from: secureRequest("https://www.pointfree.co/episodes"))
+        |> siteMiddleware
+        |> Prelude.perform
     )
   }
 
   func testWithHttps() {
     assertSnapshot(
-      matching: connection(from: URLRequest(url: URL(string: "http://www.pointfree.co")!)) |> siteMiddleware,
+      matching: connection(from: URLRequest(url: URL(string: "http://www.pointfree.co")!))
+        |> siteMiddleware
+        |> Prelude.perform,
       named: "1.redirects_to_https"
     )
 
     assertSnapshot(
-      matching: connection(from: URLRequest(url: URL(string: "http://www.pointfree.co/episodes")!)) |> siteMiddleware,
+      matching: connection(from: URLRequest(url: URL(string: "http://www.pointfree.co/episodes")!))
+        |> siteMiddleware
+        |> Prelude.perform,
       named: "2.redirects_to_https"
     )
 
     assertSnapshot(
-      matching: connection(from: URLRequest(url: URL(string: "http://0.0.0.0:8080/")!)) |> siteMiddleware,
+      matching: connection(from: URLRequest(url: URL(string: "http://0.0.0.0:8080/")!))
+        |> siteMiddleware
+        |> Prelude.perform,
       named: "0.0.0.0_allowed"
     )
 
     assertSnapshot(
-      matching: connection(from: URLRequest(url: URL(string: "http://127.0.0.1:8080/")!)) |> siteMiddleware,
+      matching: connection(from: URLRequest(url: URL(string: "http://127.0.0.1:8080/")!))
+        |> siteMiddleware
+        |> Prelude.perform,
       named: "127.0.0.0_allowed"
     )
 
     assertSnapshot(
-      matching: connection(from: URLRequest(url: URL(string: "http://localhost:8080/")!)) |> siteMiddleware,
+      matching: connection(from: URLRequest(url: URL(string: "http://localhost:8080/")!))
+        |> siteMiddleware
+        |> Prelude.perform,
       named: "localhost_allowed"
     )
   }
