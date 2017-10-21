3fb893475064e37d6fbbbc038d75635aea3b1234
Brandon Williams <mbw234@gmail.com>
2017-10-21 08:32:36 -0400

    clean up
    Former-commit-id: f7b76353c065f0ff2e031a042951da13b24eb674

--- Sources/PointFree/Stripe/StripeApi.swift
+++ Sources/PointFree/Stripe/StripeApi.swift
@@ -4,10 +4,11 @@ import Prelude
 import Optics
 
 func fetch(planId id: String) -> EitherIO<Prelude.Unit, StripeSubscriptionPlan> {
+  let request = URLRequest(url: URL.init(string: "https://api.stripe.com/v1/plans/\(id)")!)
+    |> \.allHTTPHeaderFields %~ attachStripeAuthorization
 
+  // todo: can EitherIO be a typealias with overloads?
   return EitherIO<Prelude.Unit, StripeSubscriptionPlan>(run: .init { callback in
-    let request = URLRequest(url: URL.init(string: "https://api.stripe.com/v1/plans/\(id)")!)
-      |> \.allHTTPHeaderFields %~ attachStripeAuthorization
 
     let session = URLSession(configuration: .default)
     session.dataTask(with: request) { data, response, error in
