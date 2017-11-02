6c90d8345fbba23980aa85f4412655f6f8e31f92
Stephen Celis <stephen@stephencelis.com>
2017-11-01 23:12:42 -0400

    Inverted EitherIO
    Former-commit-id: 78410cd0d33f77eeb253370fd8e6315f8d528f83

--- Sources/PointFree/SiteMiddleware.swift
+++ Sources/PointFree/SiteMiddleware.swift
@@ -4,21 +4,21 @@ import Foundation
 import HttpPipeline
 import Prelude
 
-public let siteMiddleware: Middleware<StatusLineOpen, ResponseEnded, Prelude.Unit, Data> =
-  requireHerokuHttps(allowedInsecureHosts: allowedInsecureHosts)
-    <<< redirectUnrelatedHosts(allowedHosts: allowedHosts, canonicalHost: canonicalHost)
-    <<< route(router: router)
-    <<< basicAuth(
-      user: EnvVars.BasicAuth.username,
-      password: EnvVars.BasicAuth.password,
-      realm: "Point-Free",
-      protect: isProtected
-    )
-    <<< handleErrors
-    <| render(conn:)
+//public let siteMiddleware: Middleware<StatusLineOpen, ResponseEnded, Prelude.Unit, Data> =
+//  requireHerokuHttps(allowedInsecureHosts: allowedInsecureHosts)
+//    <<< redirectUnrelatedHosts(allowedHosts: allowedHosts, canonicalHost: canonicalHost)
+//    <<< route(router: router)
+//    <<< basicAuth(
+//      user: EnvVars.BasicAuth.username,
+//      password: EnvVars.BasicAuth.password,
+//      realm: "Point-Free",
+//      protect: isProtected
+//    )
+//    <<< handleErrors
+//    <| render(conn:)
 
 private func render(conn: Conn<StatusLineOpen, Route>)
-  -> IO<Conn<ResponseEnded, Either<Error, Data>>> {
+  -> IO<Either<Error, Conn<ResponseEnded, Data>>> {
 
     switch conn.data {
     case let .githubCallback(code, redirect):
@@ -28,27 +28,10 @@ private func render(conn: Conn<StatusLineOpen, Route>)
     case let .home(signedUpSuccessfully):
       return conn.map(const(signedUpSuccessfully))
         |> homeResponse
-        >>> map(map(pure))
+        >>> map(pure)
 
-    case let .launchSignup(email):
-      return conn.map(const(email))
-        |> signupResponse
-        >>> map(map(pure))
-
-    case let .login(redirect):
-      return conn.map(const(redirect))
-        |> loginResponse
-        >>> map(map(pure))
-
-    case .logout:
-      return conn.map(const(unit))
-        |> logoutResponse
-        >>> map(map(pure))
-
-    case .secretHome:
-      return conn.map(const(unit))
-        |> secretHomeResponse
-        >>> map(map(pure))
+    default:
+      fatalError()
     }
 }
 
@@ -78,6 +61,68 @@ public func handleErrors<A>(
       }
     }
 }
+
+//private func render(conn: Conn<StatusLineOpen, Route>)
+//  -> IO<Conn<ResponseEnded, Either<Error, Data>>> {
+//
+//    switch conn.data {
+//    case let .githubCallback(code, redirect):
+//      return conn.map(const((code, redirect)))
+//        |> githubCallbackResponse
+//
+//    case let .home(signedUpSuccessfully):
+//      return conn.map(const(signedUpSuccessfully))
+//        |> homeResponse
+//        >>> map(map(pure))
+//
+//    case let .launchSignup(email):
+//      return conn.map(const(email))
+//        |> signupResponse
+//        >>> map(map(pure))
+//
+//    case let .login(redirect):
+//      return conn.map(const(redirect))
+//        |> loginResponse
+//        >>> map(map(pure))
+//
+//    case .logout:
+//      return conn.map(const(unit))
+//        |> logoutResponse
+//        >>> map(map(pure))
+//
+//    case .secretHome:
+//      return conn.map(const(unit))
+//        |> secretHomeResponse
+//        >>> map(map(pure))
+//    }
+//}
+//
+//public func handleErrors<A>(
+//  _ middleware: @escaping Middleware<StatusLineOpen, ResponseEnded, A, Either<Error, Data>>
+//  )
+//  -> Middleware<StatusLineOpen, ResponseEnded, A, Data> {
+//
+//    return { conn in
+//      middleware(conn)
+//        .flatMap {
+//          switch $0.data {
+//          case let .left(error):
+//            switch AppEnvironment.current.deployedTo {
+//            case .production:
+//              return conn
+//                |> writeStatus(.internalServerError)
+//                >-> respond(text: "An error occurred.")
+//            case .development, .staging:
+//              return conn
+//                |> writeStatus(.internalServerError)
+//                >-> respond(text: error.localizedDescription)
+//            }
+//          case let .right(data):
+//            return pure($0.map(const(data)))
+//          }
+//      }
+//    }
+//}
 
 private let canonicalHost = "www.pointfree.co"
 private let allowedHosts: [String] = [
