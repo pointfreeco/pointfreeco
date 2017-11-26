368d672531ca4f4d7c4a82e29e1ab94d552a9bcf
Stephen Celis <stephen@stephencelis.com>
2017-11-26 13:03:25 -0500

    Bump

--- Package.swift
+++ Package.swift
@@ -9,8 +9,8 @@ let package = Package(
     .library(name: "PointFree", targets: ["PointFree"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/pointfreeco/swift-prelude.git", .revision("e341975")),
-    .package(url: "https://github.com/pointfreeco/swift-web.git", .revision("15ff68a")),
+    .package(url: "https://github.com/pointfreeco/swift-prelude.git", .revision("df1b68e")),
+    .package(url: "https://github.com/pointfreeco/swift-web.git", .revision("c631f9c")),
     .package(url: "https://github.com/vapor/postgresql.git", from: "2.0.0"),
   ],
   targets: [
