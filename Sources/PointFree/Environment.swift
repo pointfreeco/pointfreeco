e8090ee2c95d6b5aa97caeec61d89287d0f48434
Stephen Celis <stephen.celis@gmail.com>
2017-11-26 13:13:04 -0500

    Database Signup (#33)
    * WIP
    
    * WIP
    
    * WIP
    
    * Updates for heroku format
    
    * Bumps
    
    * Migration
    
    * Quote!
    
    * foreign key
    
    * Constraints
    
    * case-insensitive email constraint
    
    * Fixes
    
    * Comment
    
    * Ids are string uuids now
    
    * UUID and reproducibility
    
    * Single line
    
    * point free
    
    * Fix tests
    
    * Add postgres to circle
    
    * Add temporary Postgres mock for iOS
    
    * Indent
    
    * Bump
    
    * Test swift generically
    
    * Bump
    
    * Line length
    
    
    Former-commit-id: 0852463d81e056427332d539723f1c2f7efc508f

--- Sources/PointFree/Environment.swift
+++ Sources/PointFree/Environment.swift
@@ -3,27 +3,38 @@ import Foundation
 import Optics
 import Prelude
 
-public typealias AirtableCreateRow = (_ email: String) -> (_ baseId: String) -> EitherIO<Prelude.Unit, Prelude.Unit>
-public typealias FetchGitHubUser = (GitHubAccessToken) -> EitherIO<Prelude.Unit, GitHubUser>
+public typealias AirtableCreateRow = (_ email: String) -> (_ baseId: String)
+  -> EitherIO<Prelude.Unit, Prelude.Unit>
+public typealias CreateUser = (GitHubUserEnvelope) -> EitherIO<Error, Prelude.Unit>
 public typealias FetchAuthToken = (_ code: String) -> EitherIO<Prelude.Unit, GitHubAccessToken>
+public typealias FetchGitHubUser = (GitHubAccessToken) -> EitherIO<Prelude.Unit, GitHubUser>
+public typealias FetchUser = (GitHubAccessToken) -> EitherIO<Error, User?>
 public typealias SendEmail = (_ email: Email) -> EitherIO<Prelude.Unit, SendEmailResponse>
 
 public struct Environment {
   public private(set) var airtableStuff: AirtableCreateRow
+  public private(set) var createUser: CreateUser
   public private(set) var envVars: EnvVars
   public private(set) var fetchAuthToken: FetchAuthToken
   public private(set) var fetchGitHubUser: FetchGitHubUser
+  public private(set) var fetchUser: FetchUser
   public private(set) var sendEmail: SendEmail
 
-  init(airtableStuff: @escaping AirtableCreateRow = createRow,
-       envVars: EnvVars = EnvVars.default,
-       fetchAuthToken: @escaping FetchAuthToken = PointFree.fetchAuthToken,
-       fetchGitHubUser: @escaping FetchGitHubUser = PointFree.fetchGitHubUser,
-       sendEmail: @escaping SendEmail = PointFree.mailgunSend) {
+  init(
+    airtableStuff: @escaping AirtableCreateRow = createRow,
+    createUser: @escaping CreateUser = PointFree.createUser,
+    envVars: EnvVars = EnvVars.default,
+    fetchAuthToken: @escaping FetchAuthToken = PointFree.fetchAuthToken,
+    fetchGitHubUser: @escaping FetchGitHubUser = PointFree.fetchGitHubUser,
+    fetchUser: @escaping FetchUser = PointFree.fetchUser,
+    sendEmail: @escaping SendEmail = PointFree.mailgunSend) {
+
     self.airtableStuff = airtableStuff
+    self.createUser = createUser
     self.envVars = envVars
     self.fetchAuthToken = fetchAuthToken
     self.fetchGitHubUser = fetchGitHubUser
+    self.fetchUser = fetchUser
     self.sendEmail = sendEmail
   }
 }
