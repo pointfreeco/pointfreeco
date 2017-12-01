8857840f78df082575c7a9091ffe515f007d28ad
Stephen Celis <stephen@stephencelis.com>
2017-12-01 10:39:36 -0500

    Use lenses for env modification

--- Sources/PointFree/Environment.swift
+++ Sources/PointFree/Environment.swift
@@ -43,30 +43,12 @@ public struct AppEnvironment {
   private static var stack: [Environment] = [Environment()]
   public static var current: Environment { return stack.last! }
 
-  public static func push(env: Environment) {
+  public static func push(_ env: Environment) {
     self.stack.append(env)
   }
 
-  public static func with(
-    airtableStuff: @escaping AirtableCreateRow = AppEnvironment.current.airtableStuff,
-    createUser: @escaping CreateUser = AppEnvironment.current.createUser,
-    envVars: EnvVars = AppEnvironment.current.envVars,
-    fetchAuthToken: @escaping FetchAuthToken = AppEnvironment.current.fetchAuthToken,
-    fetchGitHubUser: @escaping FetchGitHubUser = AppEnvironment.current.fetchGitHubUser,
-    fetchUser: @escaping FetchUser = AppEnvironment.current.fetchUser,
-    sendEmail: @escaping SendEmail = AppEnvironment.current.sendEmail,
-    block: @escaping () -> Void) {
-
-    self.push(
-      env: AppEnvironment.current
-        |> \.airtableStuff .~ airtableStuff
-        |> \.createUser .~ createUser
-        |> \.envVars .~ envVars
-        |> \.fetchAuthToken .~ fetchAuthToken
-        |> \.fetchGitHubUser .~ fetchGitHubUser
-        |> \.fetchUser .~ fetchUser
-        |> \.sendEmail .~ sendEmail
-    )
+  public static func with(_ env: (Environment) -> Environment, _ block: () -> Void) {
+    self.push(AppEnvironment.current |> env)
     block()
     self.pop()
   }
