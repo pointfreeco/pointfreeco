453a5ed2a2be703c5fd67587301902ca131e40b2
Brandon Williams <mbw234@gmail.com>
2017-12-01 09:31:23 -0500

    Code gen the route partial isos (#38)
    * Code gen the partial isos
    
    * access level
    
    * fix circle
    
    * clean up
    
    
    Former-commit-id: 755aeb1fee67cc4fc68b002545ec2c9d75e35956

---
+++ Sources/PointFree/RoutePartialIsos.generated.swift
@@ -0,0 +1,76 @@
+// Generated using Sourcery 0.9.0 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+import ApplicativeRouter
+import Prelude
+
+  extension Route {
+    enum iso {
+            public static let about = parenthesize <| PartialIso<Prelude.Unit, Route>(
+              apply: const(.some(.about)),
+              unapply: {
+                guard case .about = $0 else { return nil }
+                return unit
+            })
+            public static let episode = parenthesize <| PartialIso(
+              apply: Route.episode,
+              unapply: {
+                guard case let .episode(result) = $0 else { return nil }
+                return result
+            })
+            public static let episodes = parenthesize <| PartialIso(
+              apply: Route.episodes,
+              unapply: {
+                guard case let .episodes(result) = $0 else { return nil }
+                return result
+            })
+            public static let githubCallback = parenthesize <| PartialIso(
+              apply: Route.githubCallback,
+              unapply: {
+                guard case let .githubCallback(result) = $0 else { return nil }
+                return result
+            })
+            public static let home = parenthesize <| PartialIso(
+              apply: Route.home,
+              unapply: {
+                guard case let .home(result) = $0 else { return nil }
+                return result
+            })
+            public static let launchSignup = parenthesize <| PartialIso(
+              apply: Route.launchSignup,
+              unapply: {
+                guard case let .launchSignup(result) = $0 else { return nil }
+                return result
+            })
+            public static let login = parenthesize <| PartialIso(
+              apply: Route.login,
+              unapply: {
+                guard case let .login(result) = $0 else { return nil }
+                return result
+            })
+            public static let logout = parenthesize <| PartialIso<Prelude.Unit, Route>(
+              apply: const(.some(.logout)),
+              unapply: {
+                guard case .logout = $0 else { return nil }
+                return unit
+            })
+            public static let pricing = parenthesize <| PartialIso(
+              apply: Route.pricing,
+              unapply: {
+                guard case let .pricing(result) = $0 else { return nil }
+                return result
+            })
+            public static let secretHome = parenthesize <| PartialIso<Prelude.Unit, Route>(
+              apply: const(.some(.secretHome)),
+              unapply: {
+                guard case .secretHome = $0 else { return nil }
+                return unit
+            })
+            public static let terms = parenthesize <| PartialIso<Prelude.Unit, Route>(
+              apply: const(.some(.terms)),
+              unapply: {
+                guard case .terms = $0 else { return nil }
+                return unit
+            })
+    }
+  }
