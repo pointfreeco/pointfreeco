58a2ceb90893db4fb3c097a87034f048bb64626a
Stephen Celis <stephen.celis@gmail.com>
2017-12-02 19:02:34 -0500

    Modularize side effect interfaces (#40)
    * Modularize side effect interfaces
    
    * Fix indent
    
    * Feedback

--- Sources/PointFree/Environment.swift
+++ Sources/PointFree/Environment.swift
@@ -5,36 +5,26 @@ import Prelude
 
 public typealias AirtableCreateRow = (_ email: String) -> (_ baseId: String)
   -> EitherIO<Prelude.Unit, Prelude.Unit>
-public typealias CreateUser = (GitHubUserEnvelope) -> EitherIO<Error, Prelude.Unit>
-public typealias FetchAuthToken = (_ code: String) -> EitherIO<Prelude.Unit, GitHubAccessToken>
-public typealias FetchGitHubUser = (GitHubAccessToken) -> EitherIO<Prelude.Unit, GitHubUser>
-public typealias FetchUser = (GitHubAccessToken) -> EitherIO<Error, User?>
 public typealias SendEmail = (_ email: Email) -> EitherIO<Prelude.Unit, SendEmailResponse>
 
 public struct Environment {
   public private(set) var airtableStuff: AirtableCreateRow
-  public private(set) var createUser: CreateUser
+  public private(set) var database: Database
   public private(set) var envVars: EnvVars
-  public private(set) var fetchAuthToken: FetchAuthToken
-  public private(set) var fetchGitHubUser: FetchGitHubUser
-  public private(set) var fetchUser: FetchUser
+  public private(set) var gitHub: GitHub
   public private(set) var sendEmail: SendEmail
 
   init(
     airtableStuff: @escaping AirtableCreateRow = createRow,
-    createUser: @escaping CreateUser = PointFree.createUser,
+    database: PointFree.Database = .live,
     envVars: EnvVars = EnvVars(),
-    fetchAuthToken: @escaping FetchAuthToken = PointFree.fetchAuthToken,
-    fetchGitHubUser: @escaping FetchGitHubUser = PointFree.fetchGitHubUser,
-    fetchUser: @escaping FetchUser = PointFree.fetchUser,
+    gitHub: GitHub = .live,
     sendEmail: @escaping SendEmail = PointFree.mailgunSend) {
 
     self.airtableStuff = airtableStuff
-    self.createUser = createUser
+    self.database = database
     self.envVars = envVars
-    self.fetchAuthToken = fetchAuthToken
-    self.fetchGitHubUser = fetchGitHubUser
-    self.fetchUser = fetchUser
+    self.gitHub = gitHub
     self.sendEmail = sendEmail
   }
 }
