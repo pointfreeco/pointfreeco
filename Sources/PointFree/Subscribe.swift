8d8c1826814548631d62f2b9b88f1119bd6a51d6
Stephen Celis <stephen.celis@gmail.com>
2017-12-04 18:01:31 -0500

    Logging (#49)
    * Add context to subscribe
    
    * Basic logging
    
    * Clean
    
    
    Former-commit-id: 7bb11fcab3d0dc7c3f8a0869a2b4fae73553645d

--- Sources/PointFree/Subscribe.swift
+++ Sources/PointFree/Subscribe.swift
@@ -5,27 +5,29 @@ import Prelude
 typealias SubscribeData = (plan: Stripe.Plan.Id, token: String)
 
 let subscribeResponse: Middleware<StatusLineOpen, ResponseEnded, SubscribeData, Data> =
-  subscribe
+  requestContextMiddleware
+    >-> subscribe
 
-private func subscribe(_ conn: Conn<StatusLineOpen, SubscribeData>) -> IO<Conn<ResponseEnded, Data>> {
+private func subscribe(_ conn: Conn<StatusLineOpen, RequestContext<SubscribeData>>)
+  -> IO<Conn<ResponseEnded, Data>> {
 
-  return AppEnvironment.current.stripe.createCustomer(conn.data.token)
-    .flatMap { customer in
-      AppEnvironment.current.stripe.createSubscription(customer.id, conn.data.plan)
-    }
-    .run
-    .flatMap { subscription -> IO<Conn<ResponseEnded, Data>> in
-
-      switch subscription {
-      case .left:
-        return conn
-          |> writeStatus(.internalServerError)
-          >-> respond(text: "Error creating subscription!")
-
-      case let .right(sub):
-        return conn
-          |> writeStatus(.created)
-          >-> respond(text: "Created subscription! id: " + sub.id)
+    return AppEnvironment.current.stripe.createCustomer(conn.data.data.token)
+      .flatMap { customer in
+        AppEnvironment.current.stripe.createSubscription(customer.id, conn.data.data.plan)
       }
-  }
+      .run
+      .flatMap { subscription -> IO<Conn<ResponseEnded, Data>> in
+
+        switch subscription {
+        case .left:
+          return conn
+            |> writeStatus(.internalServerError)
+            >-> respond(text: "Error creating subscription!")
+
+        case let .right(sub):
+          return conn
+            |> writeStatus(.created)
+            >-> respond(text: "Created subscription! id: " + sub.id)
+        }
+    }
 }
