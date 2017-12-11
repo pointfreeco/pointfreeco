29538c3ac815c9115177065a4ae49ba42f5d2725
Brandon Williams <mbw234@gmail.com>
2017-12-11 07:23:46 -0500

    fix sourcery

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -4,8 +4,86 @@
 import XCTest
 
 @testable import PointFreeTests; @testable import StyleguideTests;
+extension AuthTests {
+  static var allTests: [(String, (AuthTests) -> () throws -> Void)] = [
+    ("testAuth", testAuth),
+    ("testAuth_WithFetchAuthTokenFailure", testAuth_WithFetchAuthTokenFailure),
+    ("testAuth_WithFetchUserFailure", testAuth_WithFetchUserFailure),
+    ("testLogin", testLogin),
+    ("testLogout", testLogout),
+    ("testSecretHome_LoggedOut", testSecretHome_LoggedOut),
+    ("testSecretHome_LoggedIn", testSecretHome_LoggedIn),
+    ("testRegistrationEmail", testRegistrationEmail)
+  ]
+}
+extension EnvVarTests {
+  static var allTests: [(String, (EnvVarTests) -> () throws -> Void)] = [
+    ("testDecoding", testDecoding)
+  ]
+}
+extension EpisodeTests {
+  static var allTests: [(String, (EpisodeTests) -> () throws -> Void)] = [
+    ("testEpisodePage", testEpisodePage),
+    ("testEpisodeNotFound", testEpisodeNotFound)
+  ]
+}
+extension EpisodesTests {
+  static var allTests: [(String, (EpisodesTests) -> () throws -> Void)] = [
+    ("testEpisodesList_NoTagSelected", testEpisodesList_NoTagSelected),
+    ("testEpisodesList_TagSelected", testEpisodesList_TagSelected)
+  ]
+}
+extension HomeTests {
+  static var allTests: [(String, (HomeTests) -> () throws -> Void)] = [
+    ("testHomepage", testHomepage)
+  ]
+}
+extension LaunchSignupTests {
+  static var allTests: [(String, (LaunchSignupTests) -> () throws -> Void)] = [
+    ("testHome", testHome),
+    ("testHome_SuccessfulSignup", testHome_SuccessfulSignup),
+    ("testSignup", testSignup),
+    ("testConfirmationEmail", testConfirmationEmail)
+  ]
+}
+extension MetaLayoutTests {
+  static var allTests: [(String, (MetaLayoutTests) -> () throws -> Void)] = [
+    ("testMetaTagsWithStyleTag", testMetaTagsWithStyleTag)
+  ]
+}
+extension NavViewTests {
+  static var allTests: [(String, (NavViewTests) -> () throws -> Void)] = [
+    ("testNav_LoggedOut", testNav_LoggedOut),
+    ("testNav_LoggedIn", testNav_LoggedIn)
+  ]
+}
+extension SiteMiddlewareTests {
+  static var allTests: [(String, (SiteMiddlewareTests) -> () throws -> Void)] = [
+    ("testWithoutWWW", testWithoutWWW),
+    ("testWithoutHeroku", testWithoutHeroku),
+    ("testWithWWW", testWithWWW),
+    ("testWithHttps", testWithHttps)
+  ]
+}
+extension StyleguideTests {
+  static var allTests: [(String, (StyleguideTests) -> () throws -> Void)] = [
+    ("testStyleguide", testStyleguide),
+    ("testDesignSystem", testDesignSystem),
+    ("testPointFreeStyles", testPointFreeStyles)
+  ]
+}
 
 // swiftlint:disable trailing_comma
 XCTMain([
+  testCase(AuthTests.allTests),
+  testCase(EnvVarTests.allTests),
+  testCase(EpisodeTests.allTests),
+  testCase(EpisodesTests.allTests),
+  testCase(HomeTests.allTests),
+  testCase(LaunchSignupTests.allTests),
+  testCase(MetaLayoutTests.allTests),
+  testCase(NavViewTests.allTests),
+  testCase(SiteMiddlewareTests.allTests),
+  testCase(StyleguideTests.allTests),
 ])
 // swiftlint:enable trailing_comma
