9673f8f8e31992c8de2d6c8e8b747f870d5b4b02
Stephen Celis <stephen@stephencelis.com>
2017-12-19 11:24:49 -0500

    Fake sequence

--- Sources/PointFreeTestSupport/TestCase.swift
+++ Sources/PointFreeTestSupport/TestCase.swift
@@ -19,6 +19,16 @@ open class TestCase: XCTestCase {
       .flatMap(const(execute("GRANT ALL ON SCHEMA public TO pointfreeco;")))
       .flatMap(const(execute("GRANT ALL ON SCHEMA public TO public;")))
       .flatMap(const(AppEnvironment.current.database.migrate()))
+      .flatMap(const(execute("CREATE SEQUENCE test_uuids")))
+      .flatMap(const(execute(
+        """
+        CREATE OR REPLACE FUNCTION uuid_generate_v1mc() RETURNS uuid AS $$
+        BEGIN
+          RETURN ('00000000-0000-0000-0000-'||LPAD(nextval('test_uuids')::text, 12, '0'))::uuid;
+        END; $$
+        LANGUAGE PLPGSQL;
+        """
+      )))
       .run
       .perform()
       .unwrap()
