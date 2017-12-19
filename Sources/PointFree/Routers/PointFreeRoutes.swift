8db8532a66943f99236555187a239cc2ed3da069
Brandon Williams <mbw234@gmail.com>
2017-12-19 13:55:12 -0500

    [wip] Team invites (#60)
    * lots of fun stuff
    
    * some basics
    
    * basics of resend and revoke
    
    * clean up
    
    * rearrange
    
    * clean up
    
    * clean
    
    * update snaps
    
    * clean up
    
    * send acceptable email and delete invite and clean up;
    
    * clean up
    
    * dont need to use tuples in views
    
    * clean up
    
    * clean up
    
    * clean up
    
    * now we show actual teammates
    
    * stubbed out a remove teammate endpoint
    
    * add some todos
    
    * some todos
    
    * took care of a bunch of todos
    
    * added some todos
    
    * clean up and added todos
    
    * dont update email with upsert
    
    * fixes
    
    
    Former-commit-id: a2c20fd14db03c73a16e90a89efae308d0558e0f

--- Sources/PointFree/Routers/PointFreeRoutes.swift
+++ Sources/PointFree/Routers/PointFreeRoutes.swift
@@ -1,4 +1,5 @@
 import ApplicativeRouter
+import Foundation
 import Either
 import Prelude
 
@@ -6,22 +7,41 @@ public protocol DerivePartialIsos {}
 
 public enum Route: DerivePartialIsos {
   case about
+  case account
   case episode(Either<String, Int>)
   case gitHubCallback(code: String?, redirect: String?)
   case home(signedUpSuccessfully: Bool?)
+  case invite(Invite)
   case launchSignup(EmailAddress)
   case login(redirect: String?)
   case logout
   case pricing(Stripe.Plan.Id?)
   case secretHome
   case subscribe(SubscribeData)
+  case team(Team)
   case terms
+
+  public enum Team: DerivePartialIsos {
+    case remove(Database.User.Id)
+    case show
+  }
+
+  public enum Invite: DerivePartialIsos {
+    case accept(Database.TeamInvite.Id)
+    case resend(Database.TeamInvite.Id)
+    case revoke(Database.TeamInvite.Id)
+    case send(EmailAddress?)
+    case show(Database.TeamInvite.Id)
+  }
 }
 
 private let routers: [Router<Route>] = [
 
   Route.iso.about
     <¢> get %> lit("about") <% end,
+
+  Route.iso.account
+    <¢> get %> lit("account") <% end,
 
   Route.iso.episode
     <¢> get %> lit("episodes") %> pathParam(.intOrString) <% end,
@@ -33,6 +53,25 @@ private let routers: [Router<Route>] = [
 
   Route.iso.home
     <¢> get %> queryParam("success", opt(.bool)) <% end,
+
+  Route.iso.invite <<< Route.Invite.iso.accept
+    // TODO: double raw representable is needed cause we have to go String -> UUID -> Tagged. Might need
+    //       a tagged combinator?
+    <¢> post %> lit("invites") %> pathParam((._rawRepresentable) >>> (._rawRepresentable)) <% lit("accept") <% end,
+
+  Route.iso.invite <<< Route.Invite.iso.resend
+    <¢> post %> lit("invites") %> pathParam((._rawRepresentable) >>> (._rawRepresentable)) <% lit("resend") <% end,
+
+  Route.iso.invite <<< Route.Invite.iso.revoke
+    <¢> post %> lit("invites") %> pathParam((._rawRepresentable) >>> (._rawRepresentable)) <% lit("revoke") <% end,
+
+  Route.iso.invite <<< Route.Invite.iso.send
+    // TODO: this weird Optional.iso.some is cause `formField` takes a partial iso `String -> A` instead of
+    //       `(String?) -> A` like it is for `queryParam`.
+    <¢> post %> lit("invites") %> formField("email", Optional.iso.some >>> opt(.rawRepresentable)) <% end,
+
+  Route.iso.invite <<< Route.Invite.iso.show
+    <¢> get %> lit("invites") %> pathParam((._rawRepresentable) >>> (._rawRepresentable)) <% end,
 
   Route.iso.launchSignup
     <¢> post %> formField("email", .rawRepresentable) <% lit("launch-signup") <% end,
@@ -51,6 +90,15 @@ private let routers: [Router<Route>] = [
 
   Route.iso.subscribe
     <¢> post %> lit("subscribe") %> formBody(SubscribeData.self) <% end,
+
+  Route.iso.team <<< Route.Team.iso.remove
+    <¢> post %> lit("account") %> lit("team") %> lit("members")
+    %> pathParam((._rawRepresentable) >>> (._rawRepresentable))
+    <% lit("remove")
+    <% end,
+
+  Route.iso.team <<< Route.Team.iso.show
+    <¢> get %> lit("account") %> lit("team") <% end,
 
   Route.iso.terms
     <¢> get %> lit("terms") <% end,
