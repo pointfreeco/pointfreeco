844bca8ca1117338d2528ccf21356009480a8219
Stephen Celis <stephen.celis@gmail.com>
2017-12-20 16:09:38 -0500

    Database tests (#61)
    * Test the database
    
    * Basic database integration test
    
    * Update PointFreeTestSupport.swift
    
    * Update DatabaseTests.swift
    
    * blow away db on EVERY TEST
    
    * indentation
    
    * Fake sequence
    
    * clean up
    
    * update snaps
    
    * linux main
    
    * make when test
    
    * maybe this is needed
    
    * i mean cmon
    
    * ok thats my b
    
    * 9.6
    
    * lets try this
    
    * put that back
    
    * lets try this
    
    * update snaps
    
    * um ok
    
    * Database tests
    
    * Fix
    
    * Case
    
    * Fix
    
    * More fix
    
    * Append keys
    
    
    Former-commit-id: a0ab45fb26f5c1d7ff9740ccf5b9b5902a18b3cd

--- Sources/PointFreeTestSupport/TestCase.swift
+++ Sources/PointFreeTestSupport/TestCase.swift
@@ -1,13 +1,38 @@
-import Prelude
+import Optics
 @testable import PointFree
+import Prelude
 import SnapshotTesting
 import XCTest
 
 open class TestCase: XCTestCase {
   override open func setUp() {
     super.setUp()
-    AppEnvironment.push(const(.mock))
-//    record = true
+    AppEnvironment.push(
+      const(
+        .mock
+          |> \.database .~ .live
+          |> \.envVars %~ { $0.assigningValuesFrom(ProcessInfo.processInfo.environment) }
+      )
+    )
+
+    _ = try! execute("DROP SCHEMA public CASCADE;")
+      .flatMap(const(execute("CREATE SCHEMA public;")))
+      .flatMap(const(execute("GRANT ALL ON SCHEMA public TO pointfreeco;")))
+      .flatMap(const(execute("GRANT ALL ON SCHEMA public TO public;")))
+      .flatMap(const(AppEnvironment.current.database.migrate()))
+      .flatMap(const(execute("CREATE SEQUENCE test_uuids")))
+      .flatMap(const(execute(
+        """
+        CREATE OR REPLACE FUNCTION uuid_generate_v1mc() RETURNS uuid AS $$
+        BEGIN
+          RETURN ('00000000-0000-0000-0000-'||LPAD(nextval('test_uuids')::text, 12, '0'))::uuid;
+        END; $$
+        LANGUAGE PLPGSQL;
+        """
+      )))
+      .run
+      .perform()
+      .unwrap()
   }
 
   override open func tearDown() {
