e33644538b2c89f9ca3a6258c0d7237a96c26e30
Brandon Williams <mbw234@gmail.com>
2017-12-22 11:19:08 -0500

    more clean

--- Tests/PointFreeTests/HtmlCssInlinerTests.swift
+++ Tests/PointFreeTests/HtmlCssInlinerTests.swift
@@ -9,44 +9,44 @@ import XCTest
 
 class HtmlCssInlinerTests: TestCase {
   func testHtmlCssInliner() {
-let stylesheet =
-  body % fontSize(.px(16))
-    <> p % color(.black)
-    <> "#hero" % maxWidth(.pct(100))
-    <> "#some-id" % backgroundColor(.red)
-    <> ".p1" % padding(all: .px(16))
-    <> ".bold" % fontWeight(.bold)
-    <> ".leading" % fontSize(.px(18))
-    <> (body | html) % height(.pct(100))
-    <> ("#footer" | footer | ".footer") % display(.block)
+    let stylesheet =
+      body % fontSize(.px(16))
+        <> p % color(.black)
+        <> "#hero" % maxWidth(.pct(100))
+        <> "#some-id" % backgroundColor(.red)
+        <> ".p1" % padding(all: .px(16))
+        <> ".bold" % fontWeight(.bold)
+        <> ".leading" % fontSize(.px(18))
+        <> (body | html) % height(.pct(100))
+        <> ("#footer" | footer | ".footer") % display(.block)
 
-let doc = document([
-  html([
-    body([
-      div([id("hero")], [
-        h1(["Point-Free"])
-        ]),
+    let doc = document([
+      html([
+        body([
+          div([id("hero")], [
+            h1(["Point-Free"])
+            ]),
 
-      p([Html.`class`("p1")], [
-        "This p tag gets styles from the tag element and classes."
-        ]),
+          p([Html.`class`("p1")], [
+            "This p tag gets styles from the tag element and classes."
+            ]),
 
-      p([id("some-id"), Html.`class`("bold leading"), style(lineHeight(1.25))], [
-        "This p tag gets styles from the id, classes, inline styles, and element tag!"
-        ]),
+          p([id("some-id"), Html.`class`("bold leading"), style(lineHeight(1.25))], [
+            "This p tag gets styles from the id, classes, inline styles, and element tag!"
+            ]),
 
-      div([id("footer")], [
-        "I'm a div footer with an id"
-        ]),
-      div([Html.`class`("footer")], [
-        "I'm a div footer with a class"
-        ]),
-      footer([
-        "I'm a footer element"
+          div([id("footer")], [
+            "I'm a div footer with an id"
+            ]),
+          div([Html.`class`("footer")], [
+            "I'm a div footer with a class"
+            ]),
+          footer([
+            "I'm a footer element"
+            ])
+          ])
         ])
       ])
-    ])
-  ])
 
     assertSnapshot(matching: applyInlineStyles(node: doc, stylesheet: stylesheet))
   }
