3208947081bd2fb0d5f17db09f9977c6eca6d38e
Brandon Williams <mbw234@gmail.com>
2017-12-22 15:09:22 -0500

    clean up

--- pointfreeco.playground/Pages/Team Invites.xcplaygroundpage/Contents.swift
+++ pointfreeco.playground/Pages/Team Invites.xcplaygroundpage/Contents.swift
@@ -1,45 +1,33 @@
-import Css
-import CssReset
-import Either
-import Html
-import HtmlCssSupport
-import HttpPipeline
-import PlaygroundSupport
+import Foundation
 @testable import PointFree
-@testable import PointFreeTestSupport
-import Prelude
-import WebKit
-import Optics
-import SnapshotTesting
+import PlaygroundSupport
 
-inviteeAcceptedEmailView.view((.mock, .mock))
+PlaygroundPage.current.needsIndefiniteExecution = true
 
-//PlaygroundPage.current.needsIndefiniteExecution = true
-//
-//AppEnvironment.current.database.migrate().run.perform()
-//
-//AppEnvironment.current.database
-//  .fetchUserById(Database.User.Id(unwrap: UUID(uuidString: "df73ae7c-e12f-11e7-82c0-afa1915eb872")!))
-//  .run
-//  .perform()
-//
-//AppEnvironment.current.database
-//  .upsertUser(.init(accessToken: .init(accessToken: "deadbeef"), gitHubUser: .init(email: .init(unwrap: "me@pointfree.co"), id: .init(unwrap: 123), name: "Blobg")))
-//  .run
-//  .perform()
-//
-//AppEnvironment.current.database
-//  .insertTeamInvite(
-//    EmailAddress(unwrap: "mcclane@pointfree.co"),
-//    Database.User.Id(unwrap: UUID(uuidString: "df73ae7c-e12f-11e7-82c0-afa1915eb872")!)
-//    )
-//  .run
-//  .perform()
-//
-//AppEnvironment.current.database
-//  .fetchTeamInvite(.init(unwrap: UUID(uuidString: "5ba328c8-e131-11e7-a5f1-fbef0b8d9eca")!))
-//  .run
-//  .perform()
+AppEnvironment.current.database.migrate().run.perform()
+
+AppEnvironment.current.database
+  .fetchUserById(Database.User.Id(unwrap: UUID(uuidString: "df73ae7c-e12f-11e7-82c0-afa1915eb872")!))
+  .run
+  .perform()
+
+AppEnvironment.current.database
+  .upsertUser(.init(accessToken: .init(accessToken: "deadbeef"), gitHubUser: .init(email: .init(unwrap: "me@pointfree.co"), id: .init(unwrap: 123), name: "Blobg")))
+  .run
+  .perform()
+
+AppEnvironment.current.database
+  .insertTeamInvite(
+    EmailAddress(unwrap: "mcclane@pointfree.co"),
+    Database.User.Id(unwrap: UUID(uuidString: "df73ae7c-e12f-11e7-82c0-afa1915eb872")!)
+    )
+  .run
+  .perform()
+
+AppEnvironment.current.database
+  .fetchTeamInvite(.init(unwrap: UUID(uuidString: "5ba328c8-e131-11e7-a5f1-fbef0b8d9eca")!))
+  .run
+  .perform()
 
 1
 
