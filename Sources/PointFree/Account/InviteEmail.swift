84334d1649ddf95d3803f2e4a33c8a9b2571c2c6
Brandon Williams <mbw234@gmail.com>
2017-12-22 18:08:09 -0500

    Simple email layout (#71)
    * email fun
    
    * more email template stuff
    
    * wip
    
    * emails wip
    
    * clean up
    
    * lots of email work
    
    * update snaps
    
    * clean up
    
    * clean up
    
    * typo fix

--- Sources/PointFree/Account/InviteEmail.swift
+++ Sources/PointFree/Account/InviteEmail.swift
@@ -4,30 +4,35 @@ import HtmlCssSupport
 import Prelude
 import Styleguide
 
-let teamInviteEmailView = View<(Database.User, Database.TeamInvite)> { inviter, invite in
-  document([
-    html([
-      head([
-        style(styleguide),
-        ]),
+let teamInviteEmailView = simpleEmailLayout(teamInviteEmailBodyView)
+  .contramap { inviter, invite in
+    SimpleEmailLayoutData(
+      title: "You’re invited to join \(inviter.name)’s team on Point-Free",
+      preheader: "Your colleague \(inviter.name) has invited you to join their team account on Point-Free.",
+      data: (inviter, invite)
+    )
+}
 
-      body([
-        gridRow([
-          gridColumn(sizes: [:], [
-            div([`class`([Class.padding([.mobile: [.all: 2]])])], [
-              h3([`class`([Class.h3])], ["You've been invited!"]),
-              p([`class`([Class.padding([.mobile: [.topBottom: 2]])])], [
-                "Your colleague ",
-                .text(encode(inviter.name)),
-                """
-                 has invited you to join their team account on Point-Free, a weekly video series discussing
-                functional programming and the Swift language. To accept, simply click the link below!
-                """
-                ]),
-              p([], [
-                a([href(url(to: .invite(.show(invite.id))))], ["Click here!"])
-                ])
-              ])
+private let teamInviteEmailBodyView = View<(Database.User, Database.TeamInvite)> { inviter, invite in
+  emailTable([style(contentTableStyles)], [
+    tr([
+      td([valign(.top)], [
+        div([`class`([Class.padding([.mobile: [.all: 2]])])], [
+          h3([`class`([Class.pf.type.title3])], ["You’re invited!"]),
+          p([`class`([Class.padding([.mobile: [.topBottom: 2]])])], [
+            "Your colleague ",
+            .text(encode(inviter.name)),
+            """
+             has invited you to join their team account on Point-Free, a weekly video series discussing
+            functional programming and the Swift programming language. To accept, simply click the link below!
+            """
+            ]),
+          p([`class`([Class.padding([.mobile: [.topBottom: 2]])])], [
+            a([
+              href(url(to: .invite(.show(invite.id)))),
+              `class`([Class.pf.components.button(color: .purple)])
+              ],
+              ["Click here to accept!"])
             ])
           ])
         ])
@@ -35,28 +40,35 @@ let teamInviteEmailView = View<(Database.User, Database.TeamInvite)> { inviter, 
     ])
 }
 
-let inviteeAcceptedEmailView = View<(Database.User, Database.User)> { inviter, invitee in
-  document([
-    html([
-      head([
-        style(styleguide),
-        ]),
+let inviteeAcceptedEmailView = simpleEmailLayout(inviteeAcceptedEmailBodyView)
+  .contramap { inviter, invitee in
+    SimpleEmailLayoutData(
+      title: "\(invitee.name) has accepted your invitation!",
+      preheader: "",
+      data: (inviter, invitee)
+    )
+}
 
-      body([
-        gridRow([
-          gridColumn(sizes: [:], [
-            div([`class`([Class.padding([.mobile: [.all: 2]])])], [
-              h3([`class`([Class.h3])], ["You’re invitation was accepted!"]),
-              p([`class`([Class.padding([.mobile: [.topBottom: 2]])])], [
-                "Your colleague ",
-                .text(encode(invitee.name)),
-                """
-                 has accepted your invitation! They now have full access to everything Point-Free has to
-                offer.
-                """
-                ]),
-              ])
-            ])
+private let inviteeAcceptedEmailBodyView = View<(Database.User, Database.User)> { inviter, invitee in
+  emailTable([style(contentTableStyles)], [
+    tr([
+      td([valign(.top)], [
+        h3([`class`([Class.pf.type.title3]), `class`([Class.padding([.mobile: [.bottom: 2]])])], [
+          "Your invitation was accepted!"
+          ]),
+        p([
+          "Hey ", .text(encode(inviter.name)), "!"
+          ]),
+        p([
+          "Your colleague ",
+          .text(encode(invitee.name)),
+          " has accepted your invitation! They now have full access to everything Point-Free has to offer. "
+          ]),
+
+        p([
+          "To review your who is on your team, ",
+          a([href(url(to: .account))], ["click here"]),
+          "."
           ])
         ])
       ])
---
+++ pointfreeco.playground/Pages/Email Tester.xcplaygroundpage/Contents.swift
@@ -0,0 +1,27 @@
+import Either
+import Html
+import PlaygroundSupport
+@testable import PointFree
+@testable import PointFreeTestSupport
+import Prelude
+import WebKit
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+let htmlNodes = teamInviteEmailView.view((.mock, .mock))
+let htmlString = render(htmlNodes, config: pretty)
+
+let webView = WKWebView(frame: .init(x: 0, y: 0, width: 400, height: 750))
+webView.loadHTMLString(htmlString, baseURL: nil)
+print(htmlString)
+
+PlaygroundPage.current.liveView = webView
+
+//sendEmail(
+//  to: [.init(unwrap: "mbw234@gmail.com")],
+//  subject: "Invite email with image \(arc4random())",
+//  content: inj2(htmlNodes)
+//  )
+//  .run
+//  .perform()
+
