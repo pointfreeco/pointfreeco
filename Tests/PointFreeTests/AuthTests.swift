e3cc97a21f6a43b307f237306bb9aec6bb1c435f
Stephen Celis <stephen@stephencelis.com>
2017-12-29 13:57:46 -0500

    Fix tests

--- Tests/PointFreeTests/AuthTests.swift
+++ Tests/PointFreeTests/AuthTests.swift
@@ -112,19 +112,7 @@ class AuthTests: TestCase {
   }
 
   func testSecretHome_LoggedIn() {
-    let sessionCookie = """
-    e78e522838eb96eb96253b60dca1f4afc46adfc6f2e8f6700f03b2b7df656c6d\
-    bacd144a80672f96a0a077faf1b63f0de0ba20e2c6bfaaa321853ab5bfb20e8d\
-    e3212f5827ef1f51d4542ee4e6920dbcc991459f02f1bdb778782d4a88a7abce
-    """
-
-    let request = URLRequest(url: URL(string: "http://localhost:8080/home")!)
-      |> \.allHTTPHeaderFields .~ [
-        "Cookie": "pf_session=\(sessionCookie)",
-        "Authorization": "Basic " + Data("hello:world".utf8).base64EncodedString()
-    ]
-
-    let conn = connection(from: request)
+    let conn = connection(from: request(to: "http://localhost:8080/home"))
     let result = conn |> siteMiddleware 
 
     assertSnapshot(matching: result.perform())
