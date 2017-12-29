1fbe77f7c5ceaf765491f09b442da61f30f06585
Stephen Celis <stephen@stephencelis.com>
2017-12-28 20:14:03 -0500

    tentative session middlewares

--- Sources/PointFree/Session.swift
+++ Sources/PointFree/Session.swift
@@ -1,54 +1,53 @@
-//import Foundation
-//import HttpPipeline
-//import Prelude
-//import Tuple
-//
-//public struct Session: Codable {
-//  let userId: Database.User.Id?
-//  let flash: Flash?
-//
-//  private enum CodingKeys: String, CodingKey {
-//    case userId = "user_id"
-//    case flash
-//  }
-//}
-//
-//public func readSessionCookieMiddleware<I, A>(
-//  _ conn: Conn<I, A>)
-//  -> IO<Conn<I, T2<Session, A>>> {
-//
-//    let session = conn.request.cookies[pointFreeUserSession]
-//      .flatMap {
-//        Response.Header
-//          .verifiedString(signedCookieValue: $0, secret: AppEnvironment.current.envVars.appSecret)
-//      }
-//      .flatMap { try? jsonDecoder.decode(Session.self, from: Data($0.utf8)) }
-//      ?? pure(Session(userId: nil, flash: nil))
-//
-//    return session
-//      .map { conn.map(const($0 .*. conn.data)) }
-//}
-//
-//private func writeSessionCookieMiddleware(
-//  _ conn: Conn<HeadersOpen, Database.User>
-//  )
-//  -> IO<Conn<HeadersOpen, Database.User>> {
-//
-//    return conn |> writeHeaders(
-//      [
-//        Response.Header.setSignedCookie(
-//          key: pointFreeUserSession,
-//          value: conn.data.id.unwrap.uuidString,
-//          options: [],
-//          secret: AppEnvironment.current.envVars.appSecret,
-//          encrypt: true
-//        )
-//        ]
-//        |> catOptionals
-//    )
-//}
-//
-//private let jsonDecoder = JSONDecoder()
-//private let jsonEncoder = JSONEncoder()
-//private let pointFreeUserSession = "pf_session"
+import Foundation
+import HttpPipeline
+import Prelude
+import Tuple
 
+public struct Session: Codable {
+  let flash: Flash?
+  let userId: Database.User.Id?
+
+  public static let empty = Session(flash: nil, userId: nil)
+
+  private enum CodingKeys: String, CodingKey {
+    case userId = "user_id"
+    case flash
+  }
+}
+
+public func _readSessionCookieMiddleware<I, A>(_ conn: Conn<I, A>) -> IO<Conn<I, T2<Session, A>>> {
+
+  return pure(conn.request.session)
+    .map { conn.map(const($0 .*. conn.data)) }
+}
+
+private func _writeSessionCookieMiddleware<A>(_ update: @escaping (Session) -> Session)
+  -> (Conn<HeadersOpen, A>)
+  -> IO<Conn<HeadersOpen, A>> {
+
+    return { conn in
+      Response.Header
+        .setSignedCookie(
+          key: pointFreeUserSession,
+          value: update(conn.request.session),
+          options: [],
+          secret: AppEnvironment.current.envVars.appSecret,
+          encrypt: true
+        )
+        .map { conn |> writeHeader($0) }
+        ?? pure(conn)
+    }
+}
+
+private extension URLRequest {
+  var session: Session {
+    return self.cookies[pointFreeUserSession]
+      .flatMap {
+        Response.Header
+          .verifiedValue(signedCookieValue: $0, secret: AppEnvironment.current.envVars.appSecret)
+      }
+      ?? .empty
+  }
+}
+
+//private let pointFreeUserSession = "pf_session"
