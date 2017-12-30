ccda697707d72982ecb9c837342a9cc2210c1af1
Stephen Celis <stephen@stephencelis.com>
2017-12-30 13:59:30 -0500

    Tests

--- Tests/PointFreeTests/AccountTests.swift
+++ Tests/PointFreeTests/AccountTests.swift
@@ -30,7 +30,32 @@ class AccountTests: TestCase {
       |> \.plan.interval .~ .year
 
     AppEnvironment.with(\.stripe.fetchSubscription .~ const(pure(subscription))) {
-      let conn = connection(from: authedRequest(to: url(to: .account)))
+      let conn = connection(from: authedRequest(to: .account))
+      let result = conn |> siteMiddleware
+
+      assertSnapshot(matching: result.perform())
+
+      #if !os(Linux)
+        if #available(OSX 10.13, *) {
+          let webView = WKWebView(frame: .init(x: 0, y: 0, width: 1080, height: 2000))
+          webView.loadHTMLString(String(data: result.perform().data, encoding: .utf8)!, baseURL: nil)
+          assertSnapshot(matching: webView, named: "desktop")
+
+          webView.frame.size.width = 400
+          assertSnapshot(matching: webView, named: "mobile")
+
+        }
+      #endif
+    }
+  }
+
+  func testAccountWithFlash() {
+    let notice = Flash(priority: .notice, message: "You’ve subscribed!")
+    let warning = Flash(priority: .warning, message: "Your subscription is past-due!")
+    let error = Flash(priority: .error, message: "An error has occurred!")
+
+    [notice, warning, error].forEach { flash in
+      let conn = connection(from: authedRequest(to: .account, session: .mock |> \.flash .~ flash))
       let result = conn |> siteMiddleware
 
       assertSnapshot(matching: result.perform())
@@ -53,7 +78,7 @@ class AccountTests: TestCase {
     let subscription = Stripe.Subscription.canceling
 
     AppEnvironment.with(\.stripe.fetchSubscription .~ const(pure(subscription))) {
-      let conn = connection(from: authedRequest(to: url(to: .account)))
+      let conn = connection(from: authedRequest(to: .account))
       let result = conn |> siteMiddleware
 
       assertSnapshot(matching: result.perform())
@@ -76,7 +101,7 @@ class AccountTests: TestCase {
     let subscription = Stripe.Subscription.canceled
 
     AppEnvironment.with(\.stripe.fetchSubscription .~ const(pure(subscription))) {
-      let conn = connection(from: authedRequest(to: url(to: .account)))
+      let conn = connection(from: authedRequest(to: .account))
       let result = conn |> siteMiddleware
 
       assertSnapshot(matching: result.perform())
