e074ae144b49369ab8a3ddc6c2a2d047f330fea7
Stephen Celis <stephen.celis@gmail.com>
2017-12-31 11:03:55 -0500

    Layouts and flash messages (#79)
    * Cleanup
    
    * More cleanup
    
    * Basic working flash!
    
    * Add flashes
    
    * Flash styles
    
    * Add flash for profile update
    
    * Tests
    
    * Linux test fix
    
    
    Former-commit-id: 75a145d0529ee2df9276c60cf3b70988cc564231

--- Sources/PointFree/Session.swift
+++ Sources/PointFree/Session.swift
@@ -1,31 +1,31 @@
 import Foundation
 import HttpPipeline
+import Optics
 import Prelude
 import Tuple
-
-public struct Session: Codable {
-  public var flash: Flash?
-  public var userId: Database.User.Id?
-
-  public static let empty = Session(flash: nil, userId: nil)
-}
 
 public func writeSessionCookieMiddleware<A>(_ update: @escaping (Session) -> Session)
   -> (Conn<HeadersOpen, A>)
   -> IO<Conn<HeadersOpen, A>> {
 
     return { conn in
-      Response.Header
+      let value = update(conn.request.session)
+      guard value != conn.request.session else { return pure(conn) }
+      return Response.Header
         .setSignedCookie(
           key: pointFreeUserSession,
-          value: update(conn.request.session),
-          options: [],
+          value: value,
+          options: [.path("/")],
           secret: AppEnvironment.current.envVars.appSecret,
           encrypt: true
         )
         .map { conn |> writeHeader($0) }
         ?? pure(conn)
     }
+}
+
+public func flash<A>(_ priority: Flash.Priority, _ message: String) -> Middleware<HeadersOpen, HeadersOpen, A, A> {
+  return writeSessionCookieMiddleware(\.flash .~ Flash(priority: priority, message: message))
 }
 
 extension URLRequest {
@@ -36,6 +36,36 @@ extension URLRequest {
           .verifiedValue(signedCookieValue: $0, secret: AppEnvironment.current.envVars.appSecret)
       }
       ?? .empty
+  }
+}
+
+public struct Session: Codable {
+  public var flash: Flash?
+  public var userId: Database.User.Id?
+
+  public static let empty = Session(flash: nil, userId: nil)
+}
+
+public struct Flash: Codable {
+  public enum Priority: String, Codable {
+    case error
+    case notice
+    case warning
+  }
+
+  public let priority: Priority
+  public let message: String
+}
+
+extension Session: Equatable {
+  public static func ==(lhs: Session, rhs: Session) -> Bool {
+    return lhs.flash == rhs.flash && lhs.userId?.rawValue == rhs.userId?.rawValue
+  }
+}
+
+extension Flash: Equatable {
+  public static func ==(lhs: Flash, rhs: Flash) -> Bool {
+    return lhs.priority == rhs.priority && lhs.message == rhs.message
   }
 }
 
