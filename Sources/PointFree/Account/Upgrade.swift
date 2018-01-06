b83a34cb81daf7258344cb6127b8cea481395386
Stephen Celis <stephen.celis@gmail.com>
2018-01-06 16:11:04 -0500

    Subscription changes (#91)
    * Stub out endpoints
    
    * Clean up cancellation, add downgrading
    
    * Upgrade endpoints
    
    * Hook up upgrade/downgrade
    
    * Basic upgrade/downgrade confirmation tests
    
    * Fix snap locations
    
    * Add upgrade/downgrade copy
    
    * Add upgrade/downgrade tests
    
    * Add test for cancel
    
    * Update snaps
    
    * Preliminary seat change middleware
    
    * Changes seats
    
    * flatmap the effect
    
    * Fix warning
    
    * Add failure cases to seat change
    
    * Add failure tests for upgrade/downgrade
    
    * Test cancel failure states
    
    
    Former-commit-id: 82afb0466cb885c25f61d2ea87e17452527a7d00

---
+++ Sources/PointFree/Account/Upgrade.swift
@@ -0,0 +1,116 @@
+import Css
+import Foundation
+import Html
+import HtmlCssSupport
+import HttpPipeline
+import HttpPipelineHtmlSupport
+import Optics
+import Prelude
+import Styleguide
+import Tuple
+
+// MARK: Middleware
+
+let confirmUpgradeResponse =
+  filterMap(require1 >>> pure, or: loginAndRedirect)
+    <<< requireStripeSubscription
+    <<< requireActiveSubscription
+    <<< requireIndividualMonthlySubscription
+    <| writeStatus(.ok)
+    >-> map(lower)
+    >>> respond(
+      view: confirmUpgradeView,
+      layoutData: { subscription, currentUser in
+        SimplePageLayoutData(
+          currentUser: currentUser,
+          data: (subscription, currentUser),
+          title: "Upgrade to yearly billing?"
+        )
+    }
+)
+
+let upgradeMiddleware =
+  filterMap(require1 >>> pure, or: loginAndRedirect)
+    <<< requireStripeSubscription
+    <<< requireActiveSubscription
+    <<< requireIndividualMonthlySubscription
+    <| upgrade
+    >-> redirect(to: .account(.index), headersMiddleware: flash(.notice, "We’ll start billing you yearly!"))
+
+// MARK: -
+
+private func upgrade(_ conn: Conn<StatusLineOpen, Tuple2<Stripe.Subscription, Database.User>>)
+  -> IO<Conn<StatusLineOpen, Prelude.Unit>> {
+
+    // TODO: send emails
+    return AppEnvironment.current.stripe.updateSubscription(get1(conn.data), .individualYearly, 1)
+      .run
+      .map(const(conn.map(const(unit))))
+}
+
+// MARK: - Transformers
+
+private func requireIndividualMonthlySubscription<A>(
+  _ middleware: @escaping Middleware<StatusLineOpen, ResponseEnded, T3<Stripe.Subscription, Database.User, A>, Data>
+  )
+  -> Middleware<StatusLineOpen, ResponseEnded, T3<Stripe.Subscription, Database.User, A>, Data> {
+
+    return filter(
+      get1 >>> (^\.plan.id.unwrap == Stripe.Plan.Id.individualMonthly.unwrap),
+      or: redirect(
+        to: .account(.index),
+        headersMiddleware: flash(.error, "Your subscription can’t be upgraded.")
+      )
+      )
+      <| middleware
+}
+
+// MARK: - Views
+
+let confirmUpgradeView = View<(Stripe.Subscription, Database.User)> { subscription, currentUser in
+  gridRow([
+    gridColumn(sizes: [.mobile: 12, .desktop: 8], [style(margin(leftRight: .auto))], [
+      div(
+        [`class`([Class.padding([.mobile: [.all: 3], .desktop: [.all: 4]])])],
+        titleRowView.view(unit)
+          <> formRowView.view(subscription)
+      )
+      ])
+    ])
+}
+
+private let titleRowView = View<Prelude.Unit> { _ in
+  gridRow([`class`([Class.padding([.mobile: [.bottom: 2]])])], [
+    gridColumn(sizes: [.mobile: 12], [
+      div([
+        h1([`class`([Class.pf.type.title2])], ["Upgrade Subscription?"])
+        ])
+      ])
+    ])
+}
+
+private let formRowView = View<Stripe.Subscription> { subscription in
+  gridRow([`class`([Class.padding([.mobile: [.bottom: 4]])])], [
+    gridColumn(sizes: [.mobile: 12], [
+      p([
+        "You are currently enrolled in the ", text(subscription.plan.name), " plan. If you choose to ",
+        "upgrade your subscription, you will begin to be billed yearly at the end of the current billing ",
+        "cycle", text(subscription.currentPeriodEnd.map { ": " + dateFormatter.string(from: $0) } ?? ""),
+        "."
+        ]),
+      form([action(path(to: .account(.subscription(.upgrade(.update))))), method(.post)], [
+        button(
+          [`class`([Class.pf.components.button(color: .purple), Class.margin([.mobile: [.top: 3]])])],
+          ["Bill me yearly"]
+        ),
+        a(
+          [
+            href(path(to: .account(.index))),
+            `class`([Class.pf.components.button(color: .black, style: .underline)])
+          ],
+          ["Never mind"]
+        )
+        ])
+      ])
+    ])
+}
---
+++ Tests/PointFreeTests/AccountTests/CancelTests.swift
@@ -0,0 +1,118 @@
+import Either
+import Html
+import HtmlPrettyPrint
+import HttpPipeline
+@testable import PointFree
+import PointFreeTestSupport
+import Prelude
+import Optics
+import SnapshotTesting
+import XCTest
+#if !os(Linux)
+  import WebKit
+#endif
+
+final class CancelTests: TestCase {
+  override func setUp() {
+    super.setUp()
+    AppEnvironment.push(\.database .~ .mock)
+  }
+
+  override func tearDown() {
+    super.tearDown()
+    AppEnvironment.pop()
+  }
+
+  func testConfirmCancel() {
+    let conn = connection(from: request(to: .account(.subscription(.cancel(.show))), session: .loggedIn))
+    let result = conn |> siteMiddleware
+
+    assertSnapshot(matching: result.perform())
+
+    #if !os(Linux)
+      if #available(OSX 10.13, *) {
+        let webView = WKWebView(frame: .init(x: 0, y: 0, width: 1080, height: 1000))
+        webView.loadHTMLString(String(data: result.perform().data, encoding: .utf8)!, baseURL: nil)
+        assertSnapshot(matching: webView, named: "desktop")
+
+        webView.frame.size.width = 400
+        assertSnapshot(matching: webView, named: "mobile")
+      }
+    #endif
+  }
+
+  func testConfirmCancelLoggedOut() {
+    let conn = connection(from: request(to: .account(.subscription(.cancel(.show)))))
+    let result = conn |> siteMiddleware
+
+    assertSnapshot(matching: result.perform())
+  }
+
+  func testConfirmCancelNoSubscription() {
+    AppEnvironment.with(\.stripe.fetchSubscription .~ const(throwE(unit))) {
+      let conn = connection(from: request(to: .account(.subscription(.cancel(.show))), session: .loggedIn))
+      let result = conn |> siteMiddleware
+
+      assertSnapshot(matching: result.perform())
+    }
+  }
+
+  func testConfirmCancelCancelingSubscription() {
+    AppEnvironment.with(\.stripe.fetchSubscription .~ const(pure(.canceling))) {
+      let conn = connection(from: request(to: .account(.subscription(.cancel(.show))), session: .loggedIn))
+      let result = conn |> siteMiddleware
+
+      assertSnapshot(matching: result.perform())
+    }
+  }
+
+  func testConfirmCancelCanceledSubscription() {
+    AppEnvironment.with(\.stripe.fetchSubscription .~ const(pure(.canceled))) {
+      let conn = connection(from: request(to: .account(.subscription(.cancel(.show))), session: .loggedIn))
+      let result = conn |> siteMiddleware
+
+      assertSnapshot(matching: result.perform())
+    }
+  }
+
+  func testCancel() {
+    let conn = connection(from: request(to: .account(.subscription(.cancel(.update))), session: .loggedIn))
+    let result = conn |> siteMiddleware
+
+    assertSnapshot(matching: result.perform())
+  }
+
+  func testCancelLoggedOut() {
+    let conn = connection(from: request(to: .account(.subscription(.cancel(.update)))))
+    let result = conn |> siteMiddleware
+
+    assertSnapshot(matching: result.perform())
+  }
+
+  func testCancelNoSubscription() {
+    AppEnvironment.with(\.stripe.fetchSubscription .~ const(throwE(unit))) {
+      let conn = connection(from: request(to: .account(.subscription(.cancel(.update))), session: .loggedIn))
+      let result = conn |> siteMiddleware
+
+      assertSnapshot(matching: result.perform())
+    }
+  }
+
+  func testCancelCancelingSubscription() {
+    AppEnvironment.with(\.stripe.fetchSubscription .~ const(pure(.canceling))) {
+      let conn = connection(from: request(to: .account(.subscription(.cancel(.update))), session: .loggedIn))
+      let result = conn |> siteMiddleware
+
+      assertSnapshot(matching: result.perform())
+    }
+  }
+
+  func testCancelCanceledSubscription() {
+    AppEnvironment.with(\.stripe.fetchSubscription .~ const(pure(.canceled))) {
+      let conn = connection(from: request(to: .account(.subscription(.cancel(.update))), session: .loggedIn))
+      let result = conn |> siteMiddleware
+
+      assertSnapshot(matching: result.perform())
+    }
+  }
+}
