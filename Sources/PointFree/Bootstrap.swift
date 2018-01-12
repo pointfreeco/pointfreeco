09989fd56aab99fbf1f28b1140eb4676fee11ab8
Brandon Williams <mbw234@gmail.com>
2018-01-12 18:31:02 -0500

    Bootstrap function for starting up the site. (#108)
    * Bootstrap function for starting up the site.
    
    * fix
    
    * clean
    
    * fix retry helper
    
    
    Former-commit-id: 5bcf4680999dc8a971ff7d06aff38de9dab2f771

---
+++ Sources/PointFree/Bootstrap.swift
@@ -0,0 +1,22 @@
+import Either
+import Prelude
+
+public func bootstrap() -> EitherIO<Error, Prelude.Unit> {
+
+  return print(message: "Bootstrapping PointFree...")
+    .flatMap(const(connectToPostgres))
+    .flatMap(const(print(message: "PointFree Bootstrapped!")))
+}
+
+private func print(message: String) -> EitherIO<Error, Prelude.Unit> {
+  return EitherIO<Error, Prelude.Unit>(run: IO {
+    print(message)
+    return .right(unit)
+  })
+}
+
+private let connectToPostgres =
+  print(message: "Connecting to PostgreSQL...")
+    .flatMap { _ in AppEnvironment.current.database.migrate() }
+    .flatMap(const(print(message: "Connected to PostgreSQL!")))
+    .retry(maxRetries: 999_999, backoff: const(.seconds(1)))
