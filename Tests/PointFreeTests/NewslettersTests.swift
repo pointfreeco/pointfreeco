2edfb78523df67634997aa9c4c4716a9e4bc23f5
Brandon Williams <mbw234@gmail.com>
2018-01-13 16:24:17 -0500

    dont run on linux

--- Tests/PointFreeTests/NewslettersTests.swift
+++ Tests/PointFreeTests/NewslettersTests.swift
@@ -45,46 +45,48 @@ class NewslettersTests: TestCase {
   }
 
   func testExpressUnsubscribeReply() {
-    let user = AppEnvironment.current.database.registerUser(.mock)
-      .run
-      .perform()
-      .right!!
-
-    let unsubEmail = unsubscribeEmail(fromUserId: user.id, andNewsletter: .announcements)!
-
-    let unsubscribe = request(
-      to: .expressUnsubscribeReply(
-        .init(
-          recipient: unsubEmail,
-          timestamp: Int(AppEnvironment.current.date().timeIntervalSince1970),
-          token: "deadbeef",
-          sender: user.email,
-          signature: "ab77648a3a922e2aab8b0e309e898a6606d071438b6f2490d381c6ca4aa6d8c9"
-        )
-      ),
-      session: .loggedOut
-    )
-
-    assertSnapshot(
-      matching: AppEnvironment.current.database.fetchEmailSettingsForUserId(user.id)
+    #if !os(Linux)
+      let user = AppEnvironment.current.database.registerUser(.mock)
         .run
         .perform()
-        .right!,
-      named: "email_settings_before_unsubscribe"
-    )
+        .right!!
 
-    let output = connection(from: unsubscribe)
-      |> siteMiddleware
-      |> Prelude.perform
-    assertSnapshot(matching: output)
+      let unsubEmail = unsubscribeEmail(fromUserId: user.id, andNewsletter: .announcements)!
 
-    assertSnapshot(
-      matching: AppEnvironment.current.database.fetchEmailSettingsForUserId(user.id)
-        .run
-        .perform()
-        .right!,
-      named: "email_settings_after_unsubscribe"
-    )
+      let unsubscribe = request(
+        to: .expressUnsubscribeReply(
+          .init(
+            recipient: unsubEmail,
+            timestamp: Int(AppEnvironment.current.date().timeIntervalSince1970),
+            token: "deadbeef",
+            sender: user.email,
+            signature: "ab77648a3a922e2aab8b0e309e898a6606d071438b6f2490d381c6ca4aa6d8c9"
+          )
+        ),
+        session: .loggedOut
+      )
+
+      assertSnapshot(
+        matching: AppEnvironment.current.database.fetchEmailSettingsForUserId(user.id)
+          .run
+          .perform()
+          .right!,
+        named: "email_settings_before_unsubscribe"
+      )
+
+      let output = connection(from: unsubscribe)
+        |> siteMiddleware
+        |> Prelude.perform
+      assertSnapshot(matching: output)
+
+      assertSnapshot(
+        matching: AppEnvironment.current.database.fetchEmailSettingsForUserId(user.id)
+          .run
+          .perform()
+          .right!,
+        named: "email_settings_after_unsubscribe"
+      )
+    #endif
   }
 
   func testExpressUnsubscribeReply_IncorrectSignature() {
