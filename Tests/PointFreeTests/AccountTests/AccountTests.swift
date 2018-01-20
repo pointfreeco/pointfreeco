b2cbf50efa9dcf532cf7c91a1f6f8a164fa7d704
Brandon Williams <mbw234@gmail.com>
2018-01-20 17:56:54 -0500

    lots of tests

--- Tests/PointFreeTests/AccountTests/AccountTests.swift
+++ Tests/PointFreeTests/AccountTests/AccountTests.swift
@@ -8,208 +8,146 @@ import Prelude
 import Optics
 import SnapshotTesting
 import XCTest
+#if !os(Linux)
+  import WebKit
+#endif
 
-final class SubscribeTests: TestCase {
+final class AccountTests: TestCase {
   override func setUp() {
     super.setUp()
-    record = true
+    AppEnvironment.push(\.database .~ .mock)
   }
 
-  func testNotLoggedIn() {
+  override func tearDown() {
+    super.tearDown()
+    AppEnvironment.pop()
+  }
+  
+  func testAccount() {
+    let subscription = Stripe.Subscription.mock
+      |> \.quantity .~ 4
+      |> \.plan .~ .teamYearly
 
-    let conn = connection(from: request(to: .subscribe(.some(.individualMonthly))))
-      |> siteMiddleware
+    AppEnvironment.with(const(.teamYearly)) {
+      let conn = connection(from: request(to: .account(.index), session: .loggedIn))
+      let result = conn |> siteMiddleware
 
-    assertSnapshot(matching: conn)
+      assertSnapshot(matching: result.perform())
 
+      #if !os(Linux)
+        if #available(OSX 10.13, *) {
+          let webView = WKWebView(frame: .init(x: 0, y: 0, width: 1080, height: 2000))
+          webView.loadHTMLString(String(data: result.perform().data, encoding: .utf8)!, baseURL: nil)
+          assertSnapshot(matching: webView, named: "desktop")
+
+          webView.frame.size.width = 400
+          assertSnapshot(matching: webView, named: "mobile")
+        }
+      #endif
+    }
   }
 
-  func testCurrentSubscribers() {
+  func testAccountWithFlashNotice() {
+    let flash = Flash(priority: .notice, message: "You’ve subscribed!")
+
+    let conn = connection(from: request(to: .account(.index), session: .loggedIn |> \.flash .~ flash))
+    let result = conn |> siteMiddleware
+
+    assertSnapshot(matching: result.perform())
+
+    #if !os(Linux)
+      if #available(OSX 10.13, *) {
+        let webView = WKWebView(frame: .init(x: 0, y: 0, width: 1080, height: 2000))
+        webView.loadHTMLString(String(data: result.perform().data, encoding: .utf8)!, baseURL: nil)
+        assertSnapshot(matching: webView, named: "desktop")
+
+        webView.frame.size.width = 400
+        assertSnapshot(matching: webView, named: "mobile")
+      }
+    #endif
   }
 
-  func testMissingSubscriberData() {
+  func testAccountWithFlashWarning() {
+    let flash = Flash(priority: .warning, message: "Your subscription is past-due!")
 
+    let conn = connection(from: request(to: .account(.index), session: .loggedIn |> \.flash .~ flash))
+    let result = conn |> siteMiddleware
+
+    assertSnapshot(matching: result.perform())
+
+    #if !os(Linux)
+      if #available(OSX 10.13, *) {
+        let webView = WKWebView(frame: .init(x: 0, y: 0, width: 1080, height: 2000))
+        webView.loadHTMLString(String(data: result.perform().data, encoding: .utf8)!, baseURL: nil)
+        assertSnapshot(matching: webView, named: "desktop")
+
+        webView.frame.size.width = 400
+        assertSnapshot(matching: webView, named: "mobile")
+      }
+    #endif
   }
 
-  func testInvalidQuantity() {
+  func testAccountWithFlashError() {
+    let flash = Flash(priority: .error, message: "An error has occurred!")
 
+    let conn = connection(from: request(to: .account(.index), session: .loggedIn |> \.flash .~ flash))
+    let result = conn |> siteMiddleware
+
+    assertSnapshot(matching: result.perform())
+
+    #if !os(Linux)
+      if #available(OSX 10.13, *) {
+        let webView = WKWebView(frame: .init(x: 0, y: 0, width: 1080, height: 2000))
+        webView.loadHTMLString(String(data: result.perform().data, encoding: .utf8)!, baseURL: nil)
+        assertSnapshot(matching: webView, named: "desktop")
+
+        webView.frame.size.width = 400
+        assertSnapshot(matching: webView, named: "mobile")
+      }
+    #endif
   }
 
-  func testHappyPath() {
+  func testAccountCancelingSubscription() {
+    let subscription = Stripe.Subscription.canceling
 
+    AppEnvironment.with(\.stripe.fetchSubscription .~ const(pure(subscription))) {
+      let conn = connection(from: request(to: .account(.index), session: .loggedIn))
+      let result = conn |> siteMiddleware
+
+      assertSnapshot(matching: result.perform())
+
+      #if !os(Linux)
+        if #available(OSX 10.13, *) {
+          let webView = WKWebView(frame: .init(x: 0, y: 0, width: 1080, height: 2000))
+          webView.loadHTMLString(String(data: result.perform().data, encoding: .utf8)!, baseURL: nil)
+          assertSnapshot(matching: webView, named: "desktop")
+
+          webView.frame.size.width = 400
+          assertSnapshot(matching: webView, named: "mobile")
+        }
+      #endif
+    }
   }
+  
+  func testAccountCanceledSubscription() {
+    let subscription = Stripe.Subscription.canceled
 
-  func testCreateCustomerFailure() {
+    AppEnvironment.with(\.stripe.fetchSubscription .~ const(pure(subscription))) {
+      let conn = connection(from: request(to: .account(.index), session: .loggedIn))
+      let result = conn |> siteMiddleware
 
+      assertSnapshot(matching: result.perform())
+
+      #if !os(Linux)
+        if #available(OSX 10.13, *) {
+          let webView = WKWebView(frame: .init(x: 0, y: 0, width: 1080, height: 2000))
+          webView.loadHTMLString(String(data: result.perform().data, encoding: .utf8)!, baseURL: nil)
+          assertSnapshot(matching: webView, named: "desktop")
+
+          webView.frame.size.width = 400
+          assertSnapshot(matching: webView, named: "mobile")
+        }
+      #endif
+    }
   }
-
-  func testCreateStripeSubscriptionFailure() {
-
-  }
-
-  func testCreateDatabaseSubscriptionFailure() {
-
-  }
-
-
-
-//  override func setUp() {
-//    super.setUp()
-//    AppEnvironment.push(\.database .~ .mock)
-//  }
-//
-//  override func tearDown() {
-//    super.tearDown()
-//    AppEnvironment.pop()
-//  }
-//
-//  func testConfirmUpgrade() {
-//    AppEnvironment.with(
-//      \.stripe.fetchSubscription .~ const(pure(.mock |> \.plan .~ .individualMonthly))
-//    ) {
-//      let conn = connection(from: request(to: .account(.subscription(.upgrade(.show))), session: .loggedIn))
-//      let result = conn |> siteMiddleware
-//
-//      assertSnapshot(matching: result.perform())
-//
-//      #if !os(Linux)
-//        if #available(OSX 10.13, *) {
-//          let webView = WKWebView(frame: .init(x: 0, y: 0, width: 1080, height: 1000))
-//          webView.loadHTMLString(String(data: result.perform().data, encoding: .utf8)!, baseURL: nil)
-//          assertSnapshot(matching: webView, named: "desktop")
-//
-//          webView.frame.size.width = 400
-//          assertSnapshot(matching: webView, named: "mobile")
-//        }
-//      #endif
-//    }
-//  }
-//
-//  func testConfirmUpgradeLoggedOut() {
-//    let conn = connection(from: request(to: .account(.subscription(.upgrade(.show)))))
-//    let result = conn |> siteMiddleware
-//
-//    assertSnapshot(matching: result.perform())
-//  }
-//
-//  func testConfirmUpgradeNoSubscription() {
-//    AppEnvironment.with(\.stripe.fetchSubscription .~ const(throwE(unit))) {
-//      let conn = connection(from: request(to: .account(.subscription(.upgrade(.show))), session: .loggedIn))
-//      let result = conn |> siteMiddleware
-//
-//      assertSnapshot(matching: result.perform())
-//    }
-//  }
-//
-//  func testConfirmUpgradeInvalidSubscription() {
-//    AppEnvironment.with(
-//      \.stripe.fetchSubscription .~ const(pure(.mock |> \.plan .~ .individualYearly))
-//    ) {
-//      let conn = connection(from: request(to: .account(.subscription(.upgrade(.show))), session: .loggedIn))
-//      let result = conn |> siteMiddleware
-//
-//      assertSnapshot(matching: result.perform())
-//    }
-//  }
-//
-//  func testConfirmUpgradeCanceledSubscription() {
-//    let subscription = Stripe.Subscription.canceled
-//      |> \.plan .~ .individualMonthly
-//
-//    AppEnvironment.with(\.stripe.fetchSubscription .~ const(pure(subscription))) {
-//      let conn = connection(from: request(to: .account(.subscription(.upgrade(.show))), session: .loggedIn))
-//      let result = conn |> siteMiddleware
-//
-//      assertSnapshot(matching: result.perform())
-//    }
-//  }
-//
-//  func testUpgrade() {
-//    AppEnvironment.with(
-//      (\.stripe.fetchSubscription .~ const(pure(.mock |> \.plan .~ .individualMonthly)))
-//        >>> (\.stripe.updateSubscription .~ { _, _, _ in pure(.mock |> \.plan .~ .individualYearly) })
-//    ) {
-//      let conn = connection(
-//        from: request(to: .account(.subscription(.upgrade(.update))), session: .loggedIn)
-//      )
-//      let result = conn |> siteMiddleware
-//
-//      assertSnapshot(matching: result.perform())
-//    }
-//  }
-//
-//  func testUpgradeLoggedOut() {
-//    let conn = connection(from: request(to: .account(.subscription(.upgrade(.update)))))
-//    let result = conn |> siteMiddleware
-//
-//    assertSnapshot(matching: result.perform())
-//  }
-//
-//  func testUpgradeNoSubscription() {
-//    AppEnvironment.with(\.stripe.fetchSubscription .~ const(throwE(unit))) {
-//      let conn = connection(
-//        from: request(to: .account(.subscription(.upgrade(.update))), session: .loggedIn)
-//      )
-//      let result = conn |> siteMiddleware
-//
-//      assertSnapshot(matching: result.perform())
-//    }
-//  }
-//
-//  func testUpgradeInvalidSubscription() {
-//    AppEnvironment.with(
-//      \.stripe.fetchSubscription .~ const(pure(.mock |> \.plan .~ .individualYearly))
-//    ) {
-//      let conn = connection(
-//        from: request(to: .account(.subscription(.upgrade(.update))), session: .loggedIn)
-//      )
-//      let result = conn |> siteMiddleware
-//
-//      assertSnapshot(matching: result.perform())
-//    }
-//  }
-//
-//  func testUpgradeCanceledSubscription() {
-//    let subscription = Stripe.Subscription.canceled
-//      |> \.plan .~ .individualMonthly
-//
-//    AppEnvironment.with(\.stripe.fetchSubscription .~ const(pure(subscription))) {
-//      let conn = connection(from: request(to: .account(.subscription(.upgrade(.update))), session: .loggedIn))
-//      let result = conn |> siteMiddleware
-//
-//      assertSnapshot(matching: result.perform())
-//    }
-//  }
-//
-//  func testUpgradeStripeError() {
-//    AppEnvironment.with(
-//      (\.stripe.fetchSubscription .~ const(pure(.mock |> \.plan .~ .individualMonthly)))
-//        >>> (\.stripe.updateSubscription .~ { _, _, _ in throwE(unit) })
-//    ) {
-//      let conn = connection(
-//        from: request(to: .account(.subscription(.upgrade(.update))), session: .loggedIn)
-//      )
-//      let result = conn |> siteMiddleware
-//
-//      assertSnapshot(matching: result.perform())
-//    }
-//  }
-//
-//  func testUpgradeEmail() {
-//    let doc = upgradeEmailView.view((.mock, .mock)).first!
-//
-//    assertSnapshot(matching: render(doc, config: pretty), pathExtension: "html")
-//    assertSnapshot(matching: plainText(for: doc))
-//
-//    #if !os(Linux)
-//      if #available(OSX 10.13, *) {
-//        let webView = WKWebView(frame: .init(x: 0, y: 0, width: 800, height: 800))
-//        webView.loadHTMLString(render(doc), baseURL: nil)
-//        assertSnapshot(matching: webView)
-//
-//        webView.frame.size = .init(width: 400, height: 700)
-//        assertSnapshot(matching: webView)
-//      }
-//    #endif
-//  }
 }
--- Tests/PointFreeTests/SubscribeTests.swift
+++ Tests/PointFreeTests/SubscribeTests.swift
@@ -1,8 +1,142 @@
-//
-//  SubscribeTests.swift
-//  PointFreePackageDescription
-//
-//  Created by Brandon Williams on 1/20/18.
-//
+import Either
+import Html
+import HtmlPrettyPrint
+import HttpPipeline
+@testable import PointFree
+import PointFreeTestSupport
+import Prelude
+import Optics
+import SnapshotTesting
+import XCTest
 
-import Foundation
+final class SubscribeTests: TestCase {
+  override func setUp() {
+    super.setUp()
+    AppEnvironment.push(\.database .~ .mock)
+  }
+
+  override func tearDown() {
+    super.tearDown()
+    AppEnvironment.pop()
+  }
+
+  func testNotLoggedIn() {
+    let conn = connection(from: request(to: .subscribe(.some(.individualMonthly))))
+      |> siteMiddleware
+      |> Prelude.perform
+
+    assertSnapshot(matching: conn)
+  }
+
+  func testCurrentSubscribers() {
+    let conn = connection(
+      from: request(to: .subscribe(.some(.individualMonthly)), session: .loggedIn)
+      )
+      |> siteMiddleware
+      |> Prelude.perform
+
+    assertSnapshot(matching: conn)
+  }
+
+  // TODO: dont know how to get this route to recognize.
+  //  func testMissingSubscriberData() {
+  //    let req = request(to: .subscribe(nil), session: .loggedIn)
+  //    let conn = connection(from: req)
+  //      |> siteMiddleware
+  //      |> Prelude.perform
+  //
+  //    assertSnapshot(matching: conn)
+  //  }
+
+  func testInvalidQuantity() {
+    AppEnvironment.with(\.database.fetchSubscriptionById .~ const(pure(nil))) {
+      let conn = connection(
+        from: request(to: .subscribe(.some(.teamYearly(quantity: 200))), session: .loggedIn)
+        )
+        |> siteMiddleware
+        |> Prelude.perform
+
+      assertSnapshot(matching: conn, named: "too_high")
+    }
+
+    AppEnvironment.with(\.database.fetchSubscriptionById .~ const(pure(nil))) {
+      let conn = connection(
+        from: request(to: .subscribe(.some(.teamYearly(quantity: 1))), session: .loggedIn)
+        )
+        |> siteMiddleware
+        |> Prelude.perform
+
+      assertSnapshot(matching: conn, named: "too_low")
+    }
+  }
+
+  func testHappyPath() {
+    AppEnvironment.with(\.database .~ .live) {
+      let user = AppEnvironment.current.database.upsertUser(.mock)
+        .run
+        .perform()
+        .right!!
+      let session = Session.loggedIn |> \.userId .~ user.id
+
+      let conn = connection(
+        from: request(to: .subscribe(.some(.individualMonthly)), session: session)
+        )
+        |> siteMiddleware
+        |> Prelude.perform
+
+      assertSnapshot(matching: conn)
+
+      let subscription = AppEnvironment.current.database.fetchSubscriptionByOwnerId(user.id)
+        .run
+        .perform()
+        .right!!
+
+      assertSnapshot(matching: subscription)
+    }
+  }
+
+  func testCreateCustomerFailure() {
+    AppEnvironment.with(
+      (\.stripe.createCustomer .~ { _, _ in throwE(unit) })
+        <> (\.database.fetchSubscriptionById .~ const(pure(nil)))
+    ) {
+      let conn = connection(
+        from: request(to: .subscribe(.some(.individualMonthly)), session: .loggedIn)
+        )
+        |> siteMiddleware
+        |> Prelude.perform
+
+      assertSnapshot(matching: conn)
+    }
+  }
+
+  func testCreateStripeSubscriptionFailure() {
+    AppEnvironment.with(
+      (\.stripe.createSubscription .~ { _, _, _ in throwE(unit) })
+        <> (\.database.fetchSubscriptionById .~ const(pure(nil)))
+    ) {
+      let conn = connection(
+        from: request(to: .subscribe(.some(.individualMonthly)), session: .loggedIn)
+        )
+        |> siteMiddleware
+        |> Prelude.perform
+
+      assertSnapshot(matching: conn)
+    }
+  }
+
+  func testCreateDatabaseSubscriptionFailure() {
+    AppEnvironment.with(
+      (\.database.createSubscription .~ { _, _ in throwE(unit as Error) })
+        <> (\.database.fetchSubscriptionById .~ const(pure(nil)))
+    ) {
+      let conn = connection(
+        from: request(to: .subscribe(.some(.individualMonthly)), session: .loggedIn)
+        )
+        |> siteMiddleware
+        |> Prelude.perform
+
+      assertSnapshot(matching: conn)
+    }
+  }
+}
