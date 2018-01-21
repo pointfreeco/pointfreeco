d61ef7b587e3af2d7c8dab3bfeb0964d388c4c8a
Brandon Williams <mbw234@gmail.com>
2018-01-21 10:12:49 -0500

    fix tests

--- Sources/PointFree/RouteNotFoundMiddleware.swift
+++ Sources/PointFree/RouteNotFoundMiddleware.swift
@@ -29,7 +29,7 @@ private let routeNotFoundView = View<Prelude.Unit> { _ in
   gridRow([`class`([Class.grid.center(.mobile)])], [
     gridColumn(sizes: [.mobile: 6], [
       div([style(padding(topBottom: .rem(12)))], [
-        h5([`class`([Class.h5])], ["Page not found :("]),
+        h5([`class`([Class.pf.type.responsiveTitle5])], ["Page not found :("]),
         pre([
           code([`class`([Class.pf.components.code(lang: "swift")])], [
             "f: (Page) -> Never"
