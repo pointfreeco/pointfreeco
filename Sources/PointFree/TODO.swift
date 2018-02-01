41245ab8c5016adf045e93d48f159ebab7b01cb4
Brandon Williams <mbw234@gmail.com>
2018-02-01 09:56:17 -0500

    Better chapter anchors (#141)
    * wip
    
    * snaps
    
    * unsafe javascript
    
    * update snaps
    
    * move imports down
    
    
    Former-commit-id: 58c55cb206a850aa5876fb96d2ec6a857b144a22

--- Sources/PointFree/TODO.swift
+++ Sources/PointFree/TODO.swift
@@ -309,6 +309,10 @@ public func onclick<T>(javascript: StaticString) -> Attribute<T> {
   return .init("onclick", "javascript:\(javascript)")
 }
 
+public func onclick<T>(unsafeJavascript: String) -> Attribute<T> {
+  return .init("onclick", "javascript:\(unsafeJavascript)")
+}
+
 // FIXME: Move to swift-web
 public func data<T>(_ name: StaticString, _ value: String) -> Attribute<T> {
   return .init("data-\(name)", value)
