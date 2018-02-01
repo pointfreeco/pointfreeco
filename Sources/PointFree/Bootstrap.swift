601758184c947980bb230e7a6c56edbf9eacde1c
Stephen Celis <stephen.celis@gmail.com>
2018-01-31 22:45:13 -0500

    More JavaScript (#140)
    * More JS
    
    * Make double-submit less likely
    
    * Make tabs pointer-able

--- Sources/PointFree/Bootstrap.swift
+++ Sources/PointFree/Bootstrap.swift
@@ -20,6 +20,7 @@ private let stepDivider = print(message: "  -----------------------------")
 private let connectToPostgres =
   print(message: "  ⚠️ Connecting to PostgreSQL...")
     .flatMap { _ in AppEnvironment.current.database.migrate() }
+    .catch { print(message: "  ❌ Error! \($0)").flatMap(const(throwE($0))) }
     .retry(maxRetries: 999_999, backoff: const(.seconds(1)))
     .flatMap(const(print(message: "  ✅ Connected to PostgreSQL!")))
     .flatMap(const(stepDivider))
