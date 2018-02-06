12957e9ad015ecc083f7525467e7376b69ce0fda
Stephen Celis <stephen@stephencelis.com>
2018-02-05 22:37:13 -0500

    Add subscribe callout to free episodes

--- Tests/PointFreeTests/EpisodeTests.swift
+++ Tests/PointFreeTests/EpisodeTests.swift
@@ -60,6 +60,56 @@ class EpisodeTests: TestCase {
     #endif
   }
 
+  func testFreeEpisodePage() {
+    let freeEpisode = AppEnvironment.current.episodes().first!
+      |> \.subscriberOnly .~ false
+
+    AppEnvironment.with(\.episodes .~ { [freeEpisode] }) {
+      let episode = request(to: .episode(.left(AppEnvironment.current.episodes().first!.slug)), session: .loggedOut)
+
+      let conn = connection(from: episode)
+      let result = conn |> siteMiddleware
+
+      assertSnapshot(matching: result.perform())
+
+      #if !os(Linux)
+        if #available(OSX 10.13, *) {
+          let webView = WKWebView(frame: .init(x: 0, y: 0, width: 1100, height: 1800))
+          webView.loadHTMLString(String(data: result.perform().data, encoding: .utf8)!, baseURL: nil)
+          assertSnapshot(matching: webView, named: "desktop")
+
+          webView.frame.size.width = 500
+          assertSnapshot(matching: webView, named: "mobile")
+        }
+      #endif
+    }
+  }
+
+  func testFreeEpisodePageSubscriber() {
+    let freeEpisode = AppEnvironment.current.episodes().first!
+      |> \.subscriberOnly .~ false
+
+    AppEnvironment.with(\.episodes .~ { [freeEpisode] }) {
+      let episode = request(to: .episode(.left(AppEnvironment.current.episodes().first!.slug)), session: .loggedIn)
+
+      let conn = connection(from: episode)
+      let result = conn |> siteMiddleware
+
+      assertSnapshot(matching: result.perform())
+
+      #if !os(Linux)
+        if #available(OSX 10.13, *) {
+          let webView = WKWebView(frame: .init(x: 0, y: 0, width: 1100, height: 1800))
+          webView.loadHTMLString(String(data: result.perform().data, encoding: .utf8)!, baseURL: nil)
+          assertSnapshot(matching: webView, named: "desktop")
+
+          webView.frame.size.width = 500
+          assertSnapshot(matching: webView, named: "mobile")
+        }
+      #endif
+    }
+  }
+
   func testEpisodeNotFound() {
     let request = URLRequest(url: URL(string: url(to: .episode(.left("object-oriented-programming"))))!)
       |> \.allHTTPHeaderFields .~ [
