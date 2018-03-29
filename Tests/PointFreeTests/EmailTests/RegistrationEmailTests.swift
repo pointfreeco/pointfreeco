d93fdfe428e4a8a1329dcec6cdac7b790f6d933b
Stephen Celis <stephen.celis@gmail.com>
2018-03-29 10:47:58 -0400

    Misc email stuff (#196)
    * Remove launch email
    
    * Fix registration tests
    
    
    Former-commit-id: e5bdfca60e28cf980d3c30712690d50a71a00c16

--- Tests/PointFreeTests/EmailTests/RegistrationEmailTests.swift
+++ Tests/PointFreeTests/EmailTests/RegistrationEmailTests.swift
@@ -12,15 +12,18 @@ import Optics
 
 class RegistrationEmailTests: TestCase {
   func testRegistrationEmail() {
-    let emailNodes = registrationEmailView.view(.mock)
+    let doc = registrationEmailView.view(.mock)
 
-    assertSnapshot(matching: render(emailNodes, config: pretty), pathExtension: "html")
+    assertSnapshot(matching: render(doc, config: pretty), pathExtension: "html")
+    assertSnapshot(matching: plainText(for: doc))
 
     #if !os(Linux)
       if #available(OSX 10.13, *) {
-        let webView = WKWebView(frame: NSRect(x: 0, y: 0, width: 600, height: 400))
-        webView.loadHTMLString(render(emailNodes), baseURL: nil)
+        let webView = WKWebView(frame: .init(x: 0, y: 0, width: 900, height: 1200))
+        webView.loadHTMLString(render(doc), baseURL: nil)
+        assertSnapshot(matching: webView)
 
+        webView.frame.size = .init(width: 400, height: 1100)
         assertSnapshot(matching: webView)
       }
     #endif
