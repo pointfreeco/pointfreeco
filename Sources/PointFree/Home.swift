7ef8c552660c040e5bb85614537087e36221d1d0
Brandon Williams <mbw234@gmail.com>
2018-04-02 08:05:57 -0400

    wip

--- Sources/PointFree/Home.swift
+++ Sources/PointFree/Home.swift
@@ -22,7 +22,7 @@ let homeMiddleware: (Conn<StatusLineOpen, Tuple3<Database.User?, Stripe.Subscrip
           currentUser: currentUser,
           data: (currentUser, currentSubscriptionStatus),
           description: "Point-Free is a video series exploring functional programming and Swift.",
-          extraStyles: pricingExtraStyles,
+          extraStyles: markdownBlockStyles <> pricingExtraStyles,
           image: "https://d3rccdn33rt8ze.cloudfront.net/social-assets/twitter-card-large.png",
           navStyle: .mountains,
           openGraphType: .website,
