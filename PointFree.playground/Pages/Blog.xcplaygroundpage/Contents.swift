3e48cc4a35493853e081303c93a2c62b86ffcc84
Brandon Williams <mbw234@gmail.com>
2018-04-16 14:47:57 -0400

    clean up playground

--- PointFree.playground/Pages/Blog.xcplaygroundpage/Contents.swift
+++ PointFree.playground/Pages/Blog.xcplaygroundpage/Contents.swift
@@ -14,11 +14,11 @@ import SnapshotTesting
 
 AppEnvironment.push(const(.mock))
 
-var request = URLRequest(url: URL(string: "http://localhost:8080/blog")!)
-
-let conn = connection(from: request)
-let result = (conn |> siteMiddleware).perform()
-let htmlStr = String(data: result.response.body, encoding: .utf8) ?? ""
+let result = connection(from: request(to: .blog(.index), basicAuth: true))
+  |> siteMiddleware
+  |> Prelude.perform
+  |> ^\.response.body
+let htmlStr = String(data: result, encoding: .utf8) ?? ""
 
 let webView = WKWebView(frame: .init(x: 0, y: 0, width: 832, height: 750))
 webView.loadHTMLString(htmlStr, baseURL: nil)
