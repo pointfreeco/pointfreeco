bee01a85f2296622cecee9c9873e46fc94867ae3
Brandon Williams <mbw234@gmail.com>
2018-05-22 12:20:02 -0400

    Environment.swift clean up (#250)
    * Small clean up
    
    * fix tests
    
    * Update FeatureFlags.swift
    
    
    Former-commit-id: bcdb3d86d2aa97fc65e097636e502d510e4150c4

--- Sources/PointFree/About.swift
+++ Sources/PointFree/About.swift
@@ -279,6 +279,13 @@ functional convert and believer after years of objects.
   }
 }
 
+public struct Assets {
+  public var brandonImgSrc = "https://d3rccdn33rt8ze.cloudfront.net/about-us/brando.jpg"
+  public var stephenImgSrc = "https://d3rccdn33rt8ze.cloudfront.net/about-us/stephen.jpg"
+  public var emailHeaderImgSrc = "https://d3rccdn33rt8ze.cloudfront.net/email-assets/pf-email-header.png"
+  public var pointersEmailHeaderImgSrc = "https://d3rccdn33rt8ze.cloudfront.net/email-assets/pf-pointers-header.jpg"
+}
+
 private let hostImgClass = CssSelector.class("host-img")
 private let hostBioClass = CssSelector.class("host-bio")
 private let hostImgStyles =
