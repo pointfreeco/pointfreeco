f8601c66353dc20b37ba0cf9661a58d99257ccda
Brandon Williams <mbw234@gmail.com>
2018-05-23 08:25:10 -0400

    Remove unneeded type annotations.
    Former-commit-id: 652076edd053e91bf5aad7dc0b632b73254c8d2b

--- Sources/PointFree/Environment.swift
+++ Sources/PointFree/Environment.swift
@@ -6,16 +6,16 @@ import Prelude
 public var Current = Environment()
 
 public struct Environment {
-  public private(set) var assets: Assets = .init()
-  public private(set) var blogPosts: () -> [BlogPost] = allBlogPosts
-  public private(set) var cookieTransform: CookieTransform = .encrypted
-  public private(set) var database: Database = .live
+  public private(set) var assets = Assets()
+  public private(set) var blogPosts = allBlogPosts
+  public private(set) var cookieTransform = CookieTransform.encrypted
+  public private(set) var database = Database.live
   public private(set) var date: () -> Date = Date.init
-  public private(set) var envVars: EnvVars = EnvVars()
-  public private(set) var episodes: () -> [Episode] = { [] }
-  public private(set) var features: [Feature] = .allFeatures
-  public private(set) var gitHub: GitHub = .live
-  public private(set) var logger: Logger = Logger()
-  public private(set) var mailgun: Mailgun = .live
-  public private(set) var stripe: Stripe = .live
+  public private(set) var envVars = EnvVars()
+  public private(set) var episodes = { [Episode]() }
+  public private(set) var features = [Feature].allFeatures
+  public private(set) var gitHub = GitHub.live
+  public private(set) var logger = Logger()
+  public private(set) var mailgun = Mailgun.live
+  public private(set) var stripe = Stripe.live
 }
