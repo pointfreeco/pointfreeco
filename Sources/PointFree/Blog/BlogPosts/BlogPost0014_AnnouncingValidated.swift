de1b2894fdc3c20740c930e5748eeb0bef7e8fb2
Brandon Williams <mbw234@gmail.com>
2018-08-17 11:26:37 -0400

    some validated edits
    Former-commit-id: c10574f3b7466363bfdd1a9a5239d5e312fa7b2b

--- Sources/PointFree/Blog/BlogPosts/BlogPost0014_AnnouncingValidated.swift
+++ Sources/PointFree/Blog/BlogPosts/BlogPost0014_AnnouncingValidated.swift
@@ -17,7 +17,8 @@ Today we are open sourcing Validated, a tiny functional Swift library for handli
       content: """
 ---
 
-> Today we are open sourcing [Validated](\(gitHubUrl(to: .repo(.validated)))), a Swift library for handling multiple errors: functionality that you don't get from throwing functions and the Result type.
+> Today we are open sourcing [Validated](\(gitHubUrl(to: .repo(.validated)))), a Swift library for handling
+multiple errors: functionality that you don't get from throwing functions and the `Result` type.
 
 ---
 """,
@@ -27,7 +28,23 @@ Today we are open sourcing Validated, a tiny functional Swift library for handli
 
     .init(
       content: """
-Swift error handling short-circuits on the first failure. Because of this, it's not the greatest option for handling things like form data, where multiple inputs may result in multiple errors.
+Error handling in Swift is done through a first-class language feature known as `throw` and `catch`. It
+allows you to express the concept of an operation that can fail and provide a data value to represent
+what went wrong. It is designed to be a short-circuiting mechanism, which means the moment you `throw`
+an error, the execution of your function is immediately stopped.
+
+Having first-class support for error handling in Swift is nice, but its short-circuiting behavior can
+be limiting. There are times that we may want to throw multiple times in a function, and have all of those
+errors accumulated.
+
+Today we are open sourcing [Validated](\(gitHubUrl(to: .repo(.validated)))), a Swift library for handling
+multiple errors: functionality that you don't get from throwing functions and the `Result` type.
+
+## Short-circuiting `throws`
+
+Let's start by looking at the short-circuiting nature of `throws`. Imagine we have a `User` type that we
+are trying to populate with data submitted in a form. Before creating the user we want to perform some
+validations, and so we cook up some throwing functions to do those validations:
 """,
       timestamp: nil,
       type: .paragraph
@@ -77,6 +94,10 @@ func validateUser(id: Int, email: String, name: String) throws -> User {
     .init(
       content: """
 Here we've combined a few throwing functions into a single throwing function that may return a `User`.
+It's important to note that each of these validations are _independent_ of each other: we don't need to
+validate one field in order to validate the next field.
+
+Let's see what it looks like to use this `validateUser` function:
 """,
       timestamp: nil,
       type: .paragraph
@@ -93,7 +114,8 @@ let user = try validateUser(id: 1, email: "blob@pointfree.co", name: "Blob")
 
     .init(
       content: """
-If the `id`, `email`, or `name` are invalid, an error is thrown.
+Here we have provided all valid values, and so we get a `User` value back. However, if the `id`, `email`, or
+`name` are invalid, an error is thrown:
 """,
       timestamp: nil,
       type: .paragraph
@@ -126,14 +148,15 @@ let user = try validateUser(id: -1, email: "blobpointfree.co", name: "")
     ),
 
     .init(
-      content: "Handling multiple errors with Validated",
-      timestamp: nil,
-      type: .title
-    ),
-
-    .init(
       content: """
-`Validated` is a [`Result`](https://github.com/antitypical/Result)-like type that can accumulate multiple errors. Let's redefine our validation functions using `Validated`.
+This is unfortunate because it creates a subpar user experience. The user can submit a form with invalid
+data, but then only be notified of the first error we discover. They will have no choice but to fix that
+one error and submit again to find out if there is anything else wrong with their data.
+
+## Handling multiple errors with `Validated`
+
+`Validated` is a [`Result`](https://github.com/antitypical/Result)-like type that can accumulate multiple
+errors. Let's start over by redefining our validation functions to return `Validated` values:
 """,
       timestamp: nil,
       type: .paragraph
@@ -165,47 +188,10 @@ func validate(name: String) -> Validated<String, String> {
 
     .init(
       content: """
-To accumulate errors, we use a function that we may already be familiar with: `zip`.
-""",
-      timestamp: nil,
-      type: .paragraph
-    ),
-
-    .init(
-      content: """
-let validInputs = zip(
-  validate(id: 1),
-  validate(email: "blob@pointfree.co"),
-  validate(name: "Blob")
-)
-// Validated<(Int, String, String), String>
-""",
-      timestamp: nil,
-      type: .code(lang: .swift)
-    ),
-
-    .init(
-      content: """
-The `zip` function on `Validated` works much the same way it works on sequences, but rather than zipping a pair of sequences into a sequence pairs, it zips up a group of single `Validated` values into single `Validated` value of a group.
-
-From here, we can use another function that we may already be familiar with, `map`, which takes a transform function and produces a new `Validated` value with its valid case transformed.
-""",
-      timestamp: nil,
-      type: .paragraph
-    ),
-
-    .init(
-      content: """
-let validUser = validInputs.map(User.init)
-// valid(User(id: 1, email: "blob@pointfree.co", name: "Blob"))
-""",
-      timestamp: nil,
-      type: .code(lang: .swift)
-    ),
-
-    .init(
-      content: """
-For ergonomics, a `zip(with:)` function is provided that takes both a transform and `Validated` inputs at once.
+So, how do we create a `User` value from the form data and these functions? Previously we had to create a
+custom `validateUser` function to accomodate for the `throw`ing functions, but the `Validated` library
+comes with a special function that allows us to instantiate _any_ date type from validated values. It's
+our [old friend](/episodes/ep23-the-many-faces-of-zip-part-1) `zip(with:)`!
 """,
       timestamp: nil,
       type: .paragraph
@@ -218,7 +204,7 @@ zip(with: User.init)(
   validate(email: "blob@pointfree.co"),
   validate(name: "Blob")
 )
-// valid(User(id: 1, email: "blob@pointfree.co", name: "Blob"))
+// .valid(User(id: 1, email: "blob@pointfree.co", name: "Blob"))
 """,
       timestamp: nil,
       type: .code(lang: .swift)
@@ -226,9 +212,12 @@ zip(with: User.init)(
 
     .init(
       content: """
-Valid inputs yield a user wrapped in the `valid` case.
+You can think of `zip(with: User.init)` as a lifting the `User` initializer up into the world of validated
+values. Here we've provided valid values for the fields of the initializer, and so we get a valid user
+out the other side.
 
-Meanwhile, an invalid input yields an error in the `invalid` case.
+On the other hand, if we provide an invalid value for a field, say a blank name, then we get an invalid
+user:
 """,
       timestamp: nil,
       type: .paragraph
@@ -241,7 +230,7 @@ zip(with: User.init)(
   validate(email: "blob@pointfree.co"),
   validate(name: "")
 )
-// invalid(["name can't be blank"])
+// .invalid(["name can't be blank"])
 """,
       timestamp: nil,
       type: .code(lang: .swift)
@@ -262,7 +251,7 @@ zip(with: User.init)(
   validate(email: "blobpointfree.co"),
   validate(name: "")
 )
-// invalid([
+// .invalid([
 //   "id must be greater than zero",
 //   "email must be valid",
 //   "name can't be blank"
@@ -274,7 +263,8 @@ zip(with: User.init)(
 
     .init(
       content: """
-Invalid errors are held in a [non-empty array](https://github.com/pointfreeco/swift-nonempty.git) to provide a compile-time guarantee that you will never encounter an empty `invalid` case.
+Invalid errors are held in a [non-empty array](https://github.com/pointfreeco/swift-nonempty.git) to provide
+a compile-time guarantee that you will never encounter an empty `invalid` case.
 """,
       timestamp: nil,
       type: .paragraph
@@ -288,7 +278,11 @@ Invalid errors are held in a [non-empty array](https://github.com/pointfreeco/sw
 
     .init(
       content: """
-That's about all there is to Validated! It's a tiny API that mostly mirrors one you may already be familiar with, `zip` and `map`, and are precisely what we need to describe the notion of error accumulation.
+## `Validated` on GitHub
+
+That's about all there is to `Validated`! It's a tiny API that mostly mirrors one you may already be
+familiar with, `zip` and `map`, and are precisely what we need to describe the notion of error
+accumulation.
 
 If you want to give it a spin, check out our
 [open source repo](\(gitHubUrl(to: .repo(.validated)))).
