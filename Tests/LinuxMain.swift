8139c80c24749b08d9ef23cadd30178dff2a48c8
Brandon Williams <mbw234@gmail.com>
2018-08-19 09:51:22 -0400

    Beginnings of Fika support.
    Former-commit-id: 8fa9f4939c5de4a68b0944a5f455e348d1c74d1c

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -20,7 +20,8 @@ extension AccountTests {
     ("testAccountCancelingSubscription", testAccountCancelingSubscription),
     ("testAccountCanceledSubscription", testAccountCanceledSubscription),
     ("testEpisodeCredits_1Credit_NoneChosen", testEpisodeCredits_1Credit_NoneChosen),
-    ("testEpisodeCredits_1Credit_1Chosen", testEpisodeCredits_1Credit_1Chosen)
+    ("testEpisodeCredits_1Credit_1Chosen", testEpisodeCredits_1Credit_1Chosen),
+    ("testAccountWithDiscount", testAccountWithDiscount)
   ]
 }
 extension AppleDeveloperMerchantIdDomainAssociationTests {
@@ -183,7 +184,8 @@ extension InviteTests {
 extension InvoicesTests {
   static var allTests: [(String, (InvoicesTests) -> () throws -> Void)] = [
     ("testInvoices", testInvoices),
-    ("testInvoice", testInvoice)
+    ("testInvoice", testInvoice),
+    ("testInvoiceWithDiscount", testInvoiceWithDiscount)
   ]
 }
 extension MetaLayoutTests {
@@ -236,6 +238,7 @@ extension PricingTests {
   static var allTests: [(String, (PricingTests) -> () throws -> Void)] = [
     ("testPricing", testPricing),
     ("testPricingLoggedIn_NonSubscriber", testPricingLoggedIn_NonSubscriber),
+    ("testPricingLoggedIn_NonSubscriber_Expanded", testPricingLoggedIn_NonSubscriber_Expanded),
     ("testPricingLoggedIn_Subscriber", testPricingLoggedIn_Subscriber),
     ("testPricingLoggedIn_CanceledSubscriber", testPricingLoggedIn_CanceledSubscriber),
     ("testPricingLoggedIn_PastDueSubscriber", testPricingLoggedIn_PastDueSubscriber)
@@ -262,7 +265,9 @@ extension SiteMiddlewareTests {
 extension StripeTests {
   static var allTests: [(String, (StripeTests) -> () throws -> Void)] = [
     ("testDecodingCustomer", testDecodingCustomer),
-    ("testDecodingCustomer_Metadata", testDecodingCustomer_Metadata)
+    ("testDecodingCustomer_Metadata", testDecodingCustomer_Metadata),
+    ("testDecodingSubscriptionWithDiscount", testDecodingSubscriptionWithDiscount),
+    ("testDecodingDiscountJson", testDecodingDiscountJson)
   ]
 }
 extension StripeWebhooksTests {
@@ -284,6 +289,8 @@ extension StyleguideTests {
 extension SubscribeTests {
   static var allTests: [(String, (SubscribeTests) -> () throws -> Void)] = [
     ("testNotLoggedIn_IndividualMonthly", testNotLoggedIn_IndividualMonthly),
+    ("testCoupon_Individual", testCoupon_Individual),
+    ("testCoupon_Team", testCoupon_Team),
     ("testNotLoggedIn_IndividualYearly", testNotLoggedIn_IndividualYearly),
     ("testNotLoggedIn_Team", testNotLoggedIn_Team),
     ("testCurrentSubscribers", testCurrentSubscribers),
