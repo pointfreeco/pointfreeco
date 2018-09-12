430123e511fa0a371951e682b2bb9e08adaeb8a7
Brandon Williams <mbw234@gmail.com>
2018-09-12 08:55:43 -0400

    Update NIO

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/pointfreeco/swift-prelude.git", .revision("5d5005d")),
     .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", .revision("69b48c8")),
-    .package(url: "https://github.com/pointfreeco/swift-web.git", .revision("507379f")),
+    .package(url: "https://github.com/pointfreeco/swift-web.git", .branch("update-nio")),
     .package(url: "https://github.com/pointfreeco/Ccmark.git", .branch("master")),
     .package(url: "https://github.com/vapor-community/postgresql.git", .exact("2.1.2")),
     ],
