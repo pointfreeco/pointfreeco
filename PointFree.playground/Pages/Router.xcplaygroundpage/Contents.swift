125b499d3770fe61b40564cd115d8ce6cb86fb06
Brandon Williams <mbw234@gmail.com>
2018-09-15 18:24:05 -0400

    wip

--- PointFree.playground/Pages/Router.xcplaygroundpage/Contents.swift
+++ PointFree.playground/Pages/Router.xcplaygroundpage/Contents.swift
@@ -1,11 +1,32 @@
+import ApplicativeRouter
 import Foundation
+import Prelude
 @testable import PointFree
 
-let urlString = "http://localhost:8080/account/subscription/change"
+//let urlString = "http://localhost:8080/account/subscription/change"
+//
+//router.match(string: urlString)!
+//
+//router.request(
+//  for: .account(.subscription(.change(.show))),
+//  base: URL(string: "https://www.pointfree.co")
+//)
 
-router.match(string: urlString)!
 
-router.request(
-  for: .account(.subscription(.change(.show))),
-  base: URL(string: "https://www.pointfree.co")
-)
+private let accountRouters: [Router<Route.Account>] = [
+  .confirmEmailChange
+    <¢> get %> lit("account") %> lit("confirm-email-change")
+    %> queryParam("payload", .appDecrypted >>> payload(.uuid >>> .tagged, .tagged))
+    <% end
+]
+let accountRouter = accountRouters.reduce(.empty, <|>)
+
+private let routers: [Router<Route>] = [
+  .account
+    <¢> lit("account") %> accountRouter
+]
+let _router = routers.reduce(.empty, <|>)
+
+_router.absoluteString(for: Route.account(Route.Account.invoices(Route.Account.Invoices.index)))
+
+//_router.absoluteString(for: Route.account(Route.Account.confirmEmailChange(userId: Database.User.Id.init(rawValue: UUID(uuidString: "deadbeef-dead-beef-dead-beefdeadbeef")!), emailAddress: "mbw234@gmail.com")))
