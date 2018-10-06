f819b0041d1a15ca22558ec6bfa159ce9b3be540
Brandon Williams <mbw234@gmail.com>
2018-10-06 16:49:20 -0400

    lil more

--- Sources/PointFree/Blog/BlogPosts/BlogPost00XY_PodcastRSS.swift
+++ Sources/PointFree/Blog/BlogPosts/BlogPost00XY_PodcastRSS.swift
@@ -43,11 +43,11 @@ best. We have even more features coming in the future for viewing our videos, bu
 
 > Hey, did you know that the entire codebase that runs this site is written in Swift _and_
 [open source](https://github.com/pointfreeco/pointfreeco)? That means you can actually see the work that
-went into building this feature. Everything from routing and rendering the RSS feed, to 
-
+went into building this feature ([here](https://github.com/pointfreeco/pointfreeco/pull/312) and
+[here](https://github.com/pointfreeco/pointfreeco/pull/317)). Everything from routing and rendering the RSS
+feed, to protecting the private feeds with encryption and unit testing the whole feature. We hope you find
+it interesting!
 """,
-      //https://github.com/pointfreeco/pointfreeco/pull/317
-      //https://github.com/pointfreeco/pointfreeco/pull/312
       timestamp: nil,
       type: .paragraph
     ),
