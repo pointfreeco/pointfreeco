a730d7d81ba865e656efd60977a1a9ed0e51cb3d
Stephen Celis <stephen.celis@gmail.com>
2018-10-10 11:03:48 -0400

    Hello World (#325)
    * draft
    
    * add nsspain video
    
    * edit
    
    * poster/id
    
    * Date
    
    * update transcript
    
    * private rss error reporting.
    
    * Fix time
    
    * Some fixes
    
    * Mailgun forces a email body.
    
    * wip
    
    * fix tests
    
    
    Former-commit-id: 35c7d20049bc70a55981ca0780d738da4a985de7

--- PointFree.playground/Pages/Router.xcplaygroundpage/Contents.swift
+++ PointFree.playground/Pages/Router.xcplaygroundpage/Contents.swift
@@ -1,5 +1,12 @@
 import Foundation
 @testable import PointFree
+import Either
+
+_ = try! PointFree
+  .bootstrap()
+  .run
+  .perform()
+  .unwrap()
 
 let urlString = "http://localhost:8080/account/subscription/change"
 
@@ -8,4 +15,18 @@ router.match(string: urlString)!
 router.request(
   for: .account(.subscription(.change(.show))),
   base: URL(string: "https://www.pointfree.co")
+)
+
+let userId = Database.User.Id.init(rawValue: UUID())
+let rssSalt = Database.User.RssSalt.init(rawValue: UUID())
+
+router.request(for: Route.account(Route.Account.rss(userId: userId, rssSalt: rssSalt)))?.url?.path
+
+
+print(
+sendEmail(
+  to: adminEmails,
+  subject: "[Private Rss Feed Error] TEST",
+  content: inj1("test")
+  ).run.perform()
 )
