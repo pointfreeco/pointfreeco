0c8d5bd046154a40fc88a20dc16330b262cbb9fc
Brandon Williams <mbw234@gmail.com>
2018-10-15 21:26:49 -0400

    Add AirPlay to Video.js (#327)
    * wip
    
    * wip
    
    * wip
    
    * fix tests
    
    * capitalization fix

--- Sources/PointFree/Episode/Show.swift
+++ Sources/PointFree/Episode/Show.swift
@@ -226,6 +226,7 @@ private let videoView = View<(Episode, isEpisodeViewable: Bool)> { episode, isEp
     [
       video(
         [
+          id("episode-video"),
           `class`([innerVideoContainerClass, videoJsClasses]),
           style(position(.absolute)),
           controls(true),
