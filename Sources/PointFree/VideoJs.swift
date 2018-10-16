785813a57c94732f101281b7e4cbd566db2b2e50
Brandon Williams <mbw234@gmail.com>
2018-10-15 21:26:49 -0400

    Add AirPlay to Video.js (#327)
    * wip
    
    * wip
    
    * wip
    
    * fix tests
    
    * capitalization fix
    
    
    Former-commit-id: 0c8d5bd046154a40fc88a20dc16330b262cbb9fc

--- Sources/PointFree/VideoJs.swift
+++ Sources/PointFree/VideoJs.swift
@@ -10,6 +10,7 @@ var videoJsHead: [ChildOf<Element.Head>] {
       rel(.stylesheet)
       ]),
     .init(script([
+      .init("onload", "videoJsLoaded()"),
       src("https://cdnjs.cloudflare.com/ajax/libs/video.js/7.2.4/video.min.js"),
       `defer`(true)
       ]))
@@ -17,10 +18,52 @@ var videoJsHead: [ChildOf<Element.Head>] {
 
   return [
     style(".vjs-subs-caps-button" % display(.none)),
-    script("window.HELP_IMPROVE_VIDEOJS = false;")
+    .init(node("script", [.text(unsafeUnencodedString("""
+window.HELP_IMPROVE_VIDEOJS = false
+
+function videoJsLoaded() {
+  video.addEventListener('webkitplaybacktargetavailabilitychanged', function(event) {
+    if (event.availability != "available") { return }
+
+    videojs("episode-video").ready(function() {
+      var existingAirplayControl = document.getElementsByClassName('vjs-airplay-control')
+      if (existingAirplayControl.length != 0) { return }
+
+      var controlBar = document.getElementsByClassName('vjs-control-bar')[0]
+      var video = document.getElementsByTagName('video')[0]
+
+      var template = document.createElement('div')
+      template.innerHTML = '\(render(airplayButton))'
+
+      controlBar.insertBefore(template.firstChild, controlBar.childNodes[controlBar.childNodes.length - 1])
+
+      var airplayControl = document.getElementsByClassName('vjs-airplay-control')[0]
+      airplayControl.addEventListener('click', function() {
+        video.webkitShowPlaybackTargetPicker()
+      })
+    })
+  })
+}
+"""))]))
     ]
     + (Current.envVars.appEnv == .testing ? [] : videoJsAssets)
 }
+
+let airplayButton = button(
+  [
+    `class`("vjs-airplay-control vjs-control vjs-button"),
+    type(.button),
+    title("AirPlay")
+  ],
+  [
+    img(
+      base64: airplaySvgBase64,
+      mediaType: .image(.svg),
+      alt: "AirPlay",
+      [style(verticalAlign(.middle))]
+    )
+  ]
+)
 
 let videoJsClasses: CssSelector =
   ".video-js"
