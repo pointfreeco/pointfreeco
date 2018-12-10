244a7b6a31efe640adb860dc349bc89d6bb7c257
Stephen Celis <stephen@stephencelis.com>
2018-12-10 11:20:13 -0500

    Update BlogPost0024_holidayDiscount.swift

--- Sources/PointFree/Blog/BlogPosts/BlogPost0024_holidayDiscount.swift
+++ Sources/PointFree/Blog/BlogPosts/BlogPost0024_holidayDiscount.swift
@@ -7,12 +7,6 @@ This holiday season we're offering a rare special: 50% savings on a Point-Free s
 """,
   contentBlocks: [
     .init(
-      content: "",
-      timestamp: nil,
-      type: .image(src: "") // TODO
-    ),
-
-    .init(
       content: """
 ---
 
@@ -20,18 +14,18 @@ This holiday season we're offering a rare special: 50% savings on a Point-Free s
 
 ---
 
-We've teamed up with [Paul Hudson](https://twitter.com/twostraws) of [Hacking with Swift](https://www.hackingwithswift.com) to offer a surprise gift on [day seven](https://www.hackingwithswift.com/articles/143/build-a-simon-game-for-watchos) of his [Advent of Swiftmas 2018](https://www.hackingwithswift.com/articles/137/advent-of-swiftmas-2018) series: 50%-off the first year of an individual Point-Free subscription!
+Act now and you can save 50% on the first year of an individual Point-Free subscription!
 
 New subscribers will get access to Point-Free for $8.50 per month, or $85 per year for the first year. This promotion won't be available for long, so (subscribe today)[\(url(to: .discounts(code: "advent-2018")))]!
 
-You can access this discount by [reading Paul's article](https://www.hackingwithswift.com/articles/143/build-a-simon-game-for-watchos) or by following (this festive link)[\(url(to: .discounts(code: "advent-2018")))].
+You can access this discount by following (this festive link)[\(url(to: .discounts(code: "advent-2018")))].
 """,
       timestamp: nil,
       type: .paragraph
     )
 
     ],
-  coverImage: "", // TODO
+  coverImage: "https://d3rccdn33rt8ze.cloudfront.net/social-assets/pf-avatar-square.",
   id: 24,
   publishedAt: .init(timeIntervalSince1970: 1544432400),
   title: "Holiday Special: Save 50% on Point-Free"
