ad3180a11666a6833ad96388963fd4d88fee909a
Brandon Williams <mbw234@gmail.com>
2018-12-11 20:20:37 -0500

    Upgrade swift-web for new NIO runner. (#363)
    Former-commit-id: d759f402082e17cd3d0d77e31a2886921f306141

--- Sources/Server/main.swift
+++ Sources/Server/main.swift
@@ -12,4 +12,4 @@ _ = try! PointFree
 
 // Server
 
-run(siteMiddleware, on: Current.envVars.port, gzip: true)
+run(siteMiddleware, on: Current.envVars.port, gzip: true, baseUrl: Current.envVars.baseUrl)
