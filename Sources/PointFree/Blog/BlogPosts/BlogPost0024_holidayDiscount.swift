bb44745bc6afa45cd7d96699f0961aca6e08fe0a
Brandon Williams <mbw234@gmail.com>
2018-12-13 09:41:50 -0500

    more blog

--- Sources/PointFree/Blog/BlogPosts/BlogPost0024_holidayDiscount.swift
+++ Sources/PointFree/Blog/BlogPosts/BlogPost0024_holidayDiscount.swift
@@ -11,8 +11,12 @@ on a Point-Free subscription.
       content: """
 We launched on January 29 of this year, and so we've been live for just under a year. In that time we have
 released 41 episodes with over 19 hours of [video](\(url(to: .home))), published 24
-[blog posts](\(url(to: .blog(.index)))), had 57 thousand unique visitors, more than 600 subscriptions, and
+[blog posts](\(url(to: .blog(.index)))), had 57 thousand unique visitors, and
 [open sourced](\(gitHubUrl(to: .organization))) 8 (!) libraries from the topics covered in our episodes.
+
+We're really proud of everything we produced for 2018, and hope that 2019 will be even better. And if you're
+not a subscriber, then keep reading and you'll find a rare special discount code for 30% off, good only for
+a short period of time!
 """,
       timestamp: nil,
       type: .paragraph
@@ -30,6 +34,8 @@ We open sourced many libraries from the content in our episodes, 8 libraries in 
 aims to solve a single problem in the simplest way possible in order to minimize the cost of bringing
 the dependency into your project.
 
+---
+
 ### `swift-snapshot-testing`
 
 Our [most recently](todo) open sourced library, [`swift-snapshot-testing`](todo), take snapshot testing to the
@@ -40,12 +46,12 @@ whole view hiearchy.
 
 The design of this library was covered in a whopping 9 episodes
 
-* Protocol Witnesses: Part 1, Part 2
-* Advanced Protocol Witnesses: Part 1, Part 2
-* Protocol-Oriented Library Design: Part 1, Part 2
-* Witness-Oriented Library Design
-* Async Snapshot Testing
-* A Tour of Snapshot Testing (free)
+* [Protocol Witnesses: Part 1](todo), [Part 2](todo)
+* [Advanced Protocol Witnesses: Part 1](todo), [Part 2](todo)
+* [Protocol-Oriented Library Design: Part 1](todo), [Part 2](todo)
+* [Witness-Oriented Library Design](todo)
+* [Async Snapshot Testing](todo)
+* [A Tour of Snapshot Testing](todo) (free)
 
 ---
 
@@ -55,22 +61,74 @@ Server-side Swift is in its nascent stages, but there have been some promising d
 such as the [Swift NIO](todo) project. Currently the most popular way to render HTML pages in server-side
 Swift is with templating languages, but there are a lot of [problems](todo) with templates. The
 [`swift-html`](todo) library aims to remedy these problems by providing a first-class data type to represent
+HTML and a way to render that data to an HTML string that can be sent to the browser.
 
+The design of this library was covered in 4 episodes:
 
+* [Domain Specific Languages: Part 1](todo), [Part 2](todo)
+* [An HTML DSL](todo)
+* [DSLs vs. Templating Languages](todo) (free)
 
- We open sourced [`swift-html`](todo)
+---
 
-### `swift-html-kitura`, `swift-html-vapure`
+### `swift-html-kitura`, `swift-html-vapor`
 
-### `swift-nonempty`
+The two most popular server-side Swift frameworks are Kitura and Vapor, but both use templating languages
+as the default way to render HTML. Luckily each framework gives a way to use your own view layer, and so
+both [`swift-html-kitura`](todo) and [`swift-html-vapor`](todo) are small libraries to help you use our
+`swift-html` library in either framework.
 
-### `swift-validated`
+---
 
 ### `swift-overture`
 
+Functional programming tends to make heavy use of custom operators, and this is because infix notation and
+associativity are a powerful way of reducing clutter in an expression and exposing some really interesting
+algebraic properties. But, it's not for everyone. So, we open sourced [`swift-overture`](todo) to be a
+simple library that gives you access to lots of interesting function composition tools, without the use
+of operators. We discussed this idea in the following episode:
+
+* [Composition without Operators](todo)
+
+---
+
 ### `swift-tagged`
 
+todo
+
+* [Tagged](todo)
+
+---
+
+### `swift-nonempty`
+
+An adage of functional programmers is "make invalid states unrepresentable". This means that states of data
+that shouldn't be allowed to happen should actually be provable by the compiler as being impossible. We
+achieve this by using concepts from algebraic data types in order to chisel away the invalid values from
+our types, and are hopefully only left with the valid states. Our [`swift-nonempty`](todo) library applies
+these ideas to model a "non-empty collection" type, which allows you to transform any collection type into
+a non-empty version of itself. We covered the design of this library in 4 episodes:
+
+* [Algebraic Data Types](todo) (free)
+* [Algebraic Data Types: Exponents](todo)
+* [Algebraic Data Types: Generics and Recursion](todo)
+* [NonEmpty](todo)
+
+---
+
+### `swift-validated`
+
+todo
+
+* [The Many Faces of Zip: Part 1](todo)
+* [The Many Faces of Zip: Part 2](todo)
+* [The Many Faces of Zip: Part 3](todo) (free)
+
+---
+
 ## Here's to 2019!
+
+todo: discount code
 """,
       timestamp: nil,
       type: .paragraph
@@ -81,15 +139,3 @@ Swift is with templating languages, but there are a lot of [problems](todo) with
   publishedAt: .init(timeIntervalSince1970: 1544432400),
   title: "2018 Year-in-Review"
 )
-
-//  ---
-//
-//  > This holiday season we're offering a rare special: 50% savings on a Point-Free subscription.
-//
-//    ---
-//
-//Act now and you can save 50% on the first year of an individual Point-Free subscription!
-//
-//New subscribers will get access to Point-Free for $8.50 per month, or $85 per year for the first year. This promotion won't be available for long, so (subscribe today)[\(url(to: .discounts(code: "advent-2018")))]!
-//
-//You can access this discount by following (this festive link)[\(url(to: .discounts(code: "advent-2018")))].
