e2f1c55d1aaeb26ca389651ef63fb8e96d0c5def
Stephen Celis <stephen@stephencelis.com>
2018-12-19 13:47:52 -0500

    wip

--- Sources/PointFree/Blog/BlogPosts/BlogPost0024_holidayDiscount.swift
+++ Sources/PointFree/Blog/BlogPosts/BlogPost0024_holidayDiscount.swift
@@ -14,7 +14,7 @@ We hope you'll join us for all of the great material we have planned for 2019.
 To end 2018 on a high note we're offering a 30% discount to new subscribers for their first year. Just
 [follow this link](/discounts/point-free-2018) and sign up for an individual subscription plan using the pre-filled code. The discount will be applied automatically.
 
-If you're interested in learning more about what you'll gain access too, see our [2018 Year In Review](/blog/25-2018-year-in-review)!
+If you're interested in learning more about what you'll gain access too, see our [2018 Year In Review](/blog/posts/25-2018-year-in-review)!
 
 We hope you'll [join us](/discounts/point-free-2018) for all of the great material we have planned for 2019.
 """,
--- Tests/PointFreeTests/EmailTests/NewBlogPostEmailTests.swift
+++ Tests/PointFreeTests/EmailTests/NewBlogPostEmailTests.swift
@@ -95,17 +95,6 @@ class NewBlogPostEmailTests: TestCase {
 
     assertSnapshot(matching: doc, as: .html)
     assertSnapshot(matching: plainText(for: doc), as: .lines)
-
-    #if !os(Linux)
-    if #available(OSX 10.13, *), ProcessInfo.processInfo.environment["CIRCLECI"] == nil {
-      let webView = WKWebView(frame: .init(x: 0, y: 0, width: 900, height: 1200))
-      webView.loadHTMLString(render(doc), baseURL: nil)
-      assertSnapshot(matching: webView, as: .image)
-
-      webView.frame.size = .init(width: 400, height: 1100)
-      assertSnapshot(matching: webView, as: .image)
-    }
-    #endif
   }
 
 }
