a5a026b8031825e9358795a4e705771d40331081
Brandon Williams <mbw234@gmail.com>
2018-12-19 10:01:29 -0500

    Make episode video column bigger.

--- PointFree.playground/Pages/Episode Page.xcplaygroundpage/Contents.swift
+++ PointFree.playground/Pages/Episode Page.xcplaygroundpage/Contents.swift
@@ -4,14 +4,32 @@ import PlaygroundSupport
 @testable import PointFreeTestSupport
 import WebKit
 
-Current = .mock
+func tocCount(episode: Episode) -> Int {
+  return episode.transcriptBlocks.reduce(0) { count, block in
+    if case .title = block.type { return count + 1 }
+    return count
+  }
+}
 
-let req = request(to: Route.episode(.right(1)))
-let result = siteMiddleware(connection(from: req)).perform()
-let htmlStr = String(decoding: result.response.body, as: UTF8.self)
+Current.episodes().count
 
-let webView = WKWebView(frame: .init(x: 0, y: 0, width: 832, height: 750))
-webView.loadHTMLString(htmlStr, baseURL: nil)
-print(htmlStr)
+(allPublicEpisodes + allPrivateEpisodes).sorted { lhs, rhs in
+  tocCount(episode: lhs) < tocCount(episode: rhs)
+}
+  .forEach { ep in
+    print(ep.title, tocCount(episode: ep))
+}
 
-PlaygroundPage.current.liveView = webView
+1
+
+//Current = .mock
+//
+//let req = request(to: Route.episode(.right(1)))
+//let result = siteMiddleware(connection(from: req)).perform()
+//let htmlStr = String(decoding: result.response.body, as: UTF8.self)
+//
+//let webView = WKWebView(frame: .init(x: 0, y: 0, width: 832, height: 750))
+//webView.loadHTMLString(htmlStr, baseURL: nil)
+//print(htmlStr)
+//
+//PlaygroundPage.current.liveView = webView
