14418fb614d581cb0eb7045d1d6d66c315d734e8
Brandon Williams <mbw234@gmail.com>
2019-01-24 12:37:49 -0500

    Drop timeout for now. (#389)

--- Sources/PointFree/SiteMiddleware.swift
+++ Sources/PointFree/SiteMiddleware.swift
@@ -9,7 +9,6 @@ import Tuple
 
 public let siteMiddleware: Middleware<StatusLineOpen, ResponseEnded, Prelude.Unit, Data> =
   requestLogger { Current.logger.info($0) }
-    <<< responseTimeout(25)
     <<< requireHerokuHttps(allowedInsecureHosts: allowedInsecureHosts)
     <<< redirectUnrelatedHosts(isAllowedHost: { isAllowed(host: $0) }, canonicalHost: canonicalHost)
     <<< route(router: router, notFound: routeNotFoundMiddleware)
