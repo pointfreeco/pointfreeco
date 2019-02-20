091de888845197bafe946f569fd03e58a22c51ae
Brandon Williams <mbw234@gmail.com>
2019-02-19 21:19:15 -0500

    update snapshot testing

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
   dependencies: [
     .package(url: "https://github.com/pointfreeco/swift-prelude.git", .revision("8cbc934")),
-    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.1.0"),
+    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", .revision("0ea1b40")),
     .package(url: "https://github.com/pointfreeco/swift-html.git", from: "0.2.1"),
     .package(url: "https://github.com/pointfreeco/swift-web.git", .revision("ad19b2d")),
     .package(url: "https://github.com/pointfreeco/Ccmark.git", .branch("master")),
