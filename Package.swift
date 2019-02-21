9599b7c21ca17d2f2e5cb0205707bc8d97a0b22e
Brandon Williams <mbw234@gmail.com>
2019-02-21 10:02:56 -0500

    update snapshot testing

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     ],
   dependencies: [
     .package(url: "https://github.com/pointfreeco/swift-prelude.git", .revision("8cbc934")),
-    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.1.0"),
+    .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.2.0"),
     .package(url: "https://github.com/pointfreeco/swift-html.git", from: "0.2.1"),
     .package(url: "https://github.com/pointfreeco/swift-tagged.git", from: "0.2.0"),
     .package(url: "https://github.com/pointfreeco/swift-web.git", .revision("ad19b2d")),
