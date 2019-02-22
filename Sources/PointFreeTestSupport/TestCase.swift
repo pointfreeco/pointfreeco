28f325272d6a03fc0a6db0ee7aa92b6e423675a6
Stephen Celis <stephen.celis@gmail.com>
2019-02-22 09:18:40 -0500

    Remove SiteTag (#401)
    * Remove SiteTag
    
    * Update PointFreeRoutes.swift
    
    * Update snapshot testing.
    
    * remove typealias

--- Sources/PointFreeTestSupport/TestCase.swift
+++ Sources/PointFreeTestSupport/TestCase.swift
@@ -4,10 +4,6 @@ import Prelude
 import SnapshotTesting
 import XCTest
 
-#if !os(Linux)
-public typealias SnapshotTestCase = XCTestCase
-#endif
-
 open class TestCase: SnapshotTestCase {
   override open func setUp() {
     super.setUp()
