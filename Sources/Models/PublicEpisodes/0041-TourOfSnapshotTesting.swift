3f29338e584271b25b6e5616596fab4973052e86
Brandon Williams <mbw234@gmail.com>
2019-03-05 15:02:14 -0500

    Typo fix: official -> officially

--- Sources/Models/PublicEpisodes/0041-TourOfSnapshotTesting.swift
+++ Sources/Models/PublicEpisodes/0041-TourOfSnapshotTesting.swift
@@ -2,7 +2,7 @@ import Foundation
 
 public let ep41 = Episode(
   blurb: """
-Our snapshot testing library is now official open source! In order to show just how easy it is to integrate the library into any existing code base, we add some snapshot tests to a popular open source library for attributed strings. This gives us the chance to see how easy it is to write all new, domain-specific snapshot strategies from scratch.
+Our snapshot testing library is now officially open source! In order to show just how easy it is to integrate the library into any existing code base, we add some snapshot tests to a popular open source library for attributed strings. This gives us the chance to see how easy it is to write all new, domain-specific snapshot strategies from scratch.
 """,
   codeSampleDirectory: "",
   exercises: exercises,
