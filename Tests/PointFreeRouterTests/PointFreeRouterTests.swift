860a7811b1ec5c3b16a9406f5a9d1a09f4cf1484
Stephen Celis <stephen@stephencelis.com>
2019-03-12 14:11:13 -0400

    Fix

--- Tests/PointFreeRouterTests/PointFreeRouterTests.swift
+++ Tests/PointFreeRouterTests/PointFreeRouterTests.swift
@@ -5,7 +5,7 @@ import XCTest
 
 class PointFreeRouterTests: XCTestCase {
   func testUpdateProfile() {
-    let router = pointFreeRouter(appSecret: "deadbeef", mailgunApiKey: "deadbeef")
+    let router = pointFreeRouter
     let profileData = ProfileData(
       email: "blobby@blob.co",
       extraInvoiceInfo: nil,
