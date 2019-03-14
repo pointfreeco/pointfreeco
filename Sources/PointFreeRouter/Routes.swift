ddacc33974cca5de6b67e62073a6ee7cae6ae264
Stephen Celis <stephen.celis@gmail.com>
2019-03-14 16:54:17 -0400

    Add logging on unrecognized Stripe hooks (#421)
    * Add logging on unrecognized Stripe hooks
    
    * Fallthrough fatal
    
    
    Former-commit-id: 0a8aee3ee09cb2735984519ac3fcf60339d64fd4

--- Sources/PointFreeRouter/Routes.swift
+++ Sources/PointFreeRouter/Routes.swift
@@ -68,8 +68,9 @@ public enum Route: DerivePartialIsos, Equatable {
     case stripe(_Stripe)
 
     public enum _Stripe: DerivePartialIsos, Equatable {
-      case event(Event<Either<Invoice, Stripe.Subscription>>)
-      case `fallthrough`
+      case knownEvent(Event<Either<Invoice, Stripe.Subscription>>)
+      case unknownEvent(Event<Prelude.Unit>)
+      case fatal
     }
   }
 }
@@ -180,7 +181,7 @@ private let routers: [Router<Route>] = [
   .useEpisodeCredit
     <¢> post %> lit("episodes") %> pathParam(.tagged(.int)) <% lit("credit") <% end,
 
-  .webhooks <<< .stripe <<< .event
+  .webhooks <<< .stripe <<< .knownEvent
     <¢> post %> lit("webhooks") %> lit("stripe")
     %> jsonBody(
       Stripe.Event<Either<Stripe.Invoice, Stripe.Subscription>>.self,
@@ -189,7 +190,16 @@ private let routers: [Router<Route>] = [
     )
     <% end,
 
-  .webhooks <<< .stripe <<< .fallthrough
+  .webhooks <<< .stripe <<< .unknownEvent
+    <¢> post %> lit("webhooks") %> lit("stripe")
+    %> jsonBody(
+      Stripe.Event<Prelude.Unit>.self,
+      encoder: Stripe.jsonEncoder,
+      decoder: Stripe.jsonDecoder
+    )
+    <% end,
+
+  .webhooks <<< .stripe <<< .fatal
     <¢> post %> lit("webhooks") %> lit("stripe") <% end,
 ]
 
