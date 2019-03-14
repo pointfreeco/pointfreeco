9cbcd7a09edf495656d577c100766b1c3891fc28
Stephen Celis <stephen@stephencelis.com>
2019-03-14 09:32:47 -0400

    even more secrets

--- Sources/PointFree/Bootstrap.swift
+++ Sources/PointFree/Bootstrap.swift
@@ -67,7 +67,7 @@ private let loadEnvVars = { (_: Prelude.Unit) -> EitherIO<Error, Prelude.Unit> i
   )
   Current.mailgun = .init(
     apiKey: Current.envVars.mailgun.apiKey,
-    appSecret: Current.envVars.appSecret.rawValue,
+    appSecret: Current.envVars.appSecret,
     domain: Current.envVars.mailgun.domain,
     logger: Current.logger
   )
