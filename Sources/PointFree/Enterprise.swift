25923e41feaff430664a3ef7df28950de7e4e00d
Stephen Celis <stephen@stephencelis.com>
2019-03-19 11:25:49 -0400

    Fetch enterprise account

--- Sources/PointFree/Enterprise.swift
+++ Sources/PointFree/Enterprise.swift
@@ -6,6 +6,7 @@ import HtmlCssSupport
 import HttpPipeline
 import HttpPipelineHtmlSupport
 import Models
+import PointFreePrelude
 import PointFreeRouter
 import Prelude
 import Styleguide
@@ -13,4 +14,19 @@ import UrlFormEncoding
 import Views
 
 let enterpriseResponse: Middleware<StatusLineOpen, ResponseEnded, EnterpriseAccount.Domain, Data>
-  = hole()
+  = fetchEnterpriseAccount
+    <| hole()
+
+func fetchEnterpriseAccount(
+  _ middleware: @escaping Middleware<StatusLineOpen, ResponseEnded, EnterpriseAccount?, Data>
+  )
+  -> Middleware<StatusLineOpen, ResponseEnded, EnterpriseAccount.Domain, Data> {
+
+    return { conn in
+      return Current.database.fetchEnterpriseAccount(conn.data)
+        .mapExcept(requireSome)
+        .run
+        .map(^\.right >>> const >>> conn.map)
+        .flatMap(middleware)
+    }
+}
