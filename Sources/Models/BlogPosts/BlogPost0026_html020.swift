c544b4ea8f0e00dd1c43e43f05a52845202d24b7
Stephen Celis <stephen@stephencelis.com>
2019-05-08 22:41:05 -0400

    QuikFiks
    Former-commit-id: da376b8947585b7415a568af41d5f04a678e6f23

--- Sources/Models/BlogPosts/BlogPost0026_html020.swift
+++ Sources/Models/BlogPosts/BlogPost0026_html020.swift
@@ -71,7 +71,7 @@ You can snapshot test swift-html's `Node` type using the `html` strategy.
       content: """
 import Html
 import HtmlSnapshotTesting
-import SnpashotTesting
+import SnapshotTesting
 import XCTest
 
 class MyWebPageTests: XCTestCase {
