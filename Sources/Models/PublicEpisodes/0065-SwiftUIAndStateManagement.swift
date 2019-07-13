b6fdc6c968c8d88ce5553e2423f151924a6f9193
Brandon Williams <mbw234@gmail.com>
2019-07-13 14:55:40 -0400

    Episode 65 stub

---
+++ Sources/Models/PublicEpisodes/0065-SwiftUIAndStateManagement.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+public let ep65 = Episode(
+  // TODO: workshop
+  blurb: """
+Let's begin exploring application architecture by understanding what are the common problems we encounter when trying to build large, complex applications. We will build an app in SwiftUI to see how Apple's new framework approaches solving these problems.
+""",
+  codeSampleDirectory: "0065-swiftui-and-state-management-pt1", // TODO
+  exercises: exercises,
+  fullVideo: .init(
+    bytesLength: 0, // todo
+    downloadUrl: "todo",
+    streamingSource: "todo"
+  ),
+  id: 65,
+  // todo: cloudfront
+  image: "https://s3.amazonaws.com/pointfreeco-episodes-processed/0065-swiftui-and-state-management-pt1/poster.jpg",
+  itunesImage: "https://s3.amazonaws.com/pointfreeco-episodes-processed/0065-swiftui-and-state-management-pt1/itunes-poster.jpg",
+  length: 26 * 60 + 45,
+  permission: .free,
+  previousEpisodeInCollection: nil,
+  publishedAt: .init(timeIntervalSince1970: 1563170400),
+  references: [
+    // todo
+
+    // swiftui tutorials
+    // 
+  ],
+  sequence: 65,
+  title: "SwiftUI and State Management: Part 1",
+  trailerVideo: .init(
+    bytesLength: 0, // todo
+    downloadUrl: "todo",
+    streamingSource: "todo"
+  ),
+  transcriptBlocks: transcriptBlocks
+)
+
+private let exercises: [Episode.Exercise] = [
+  // todo
+]
+
+private let transcriptBlocks: [Episode.TranscriptBlock] = [
+  // todo
+]
--- Sources/Models/PublicEpisodes/AllPublicEpisodes.swift
+++ Sources/Models/PublicEpisodes/AllPublicEpisodes.swift
@@ -12,4 +12,5 @@ public let allPublicEpisodes: [Episode] = [
   ep49,
   ep50,
   ep55,
+  ep65,
 ]
