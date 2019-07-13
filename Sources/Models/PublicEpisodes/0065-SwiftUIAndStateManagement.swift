52716f76c5c2cfc9fca0dd2767c5332daf4a79d9
Brandon Williams <mbw234@gmail.com>
2019-07-13 17:20:35 -0400

    Some references and exercises.
    Former-commit-id: 83d19891392fb3bc570d733839a64b3d5c52d984

--- Sources/Models/PublicEpisodes/0065-SwiftUIAndStateManagement.swift
+++ Sources/Models/PublicEpisodes/0065-SwiftUIAndStateManagement.swift
@@ -21,10 +21,8 @@ Let's begin exploring application architecture by understanding what are the com
   previousEpisodeInCollection: nil,
   publishedAt: .init(timeIntervalSince1970: 1563170400),
   references: [
+    .swiftUiTutorials
     // todo
-
-    // swiftui tutorials
-    // 
   ],
   sequence: 65,
   title: "SwiftUI and State Management: Part 1",
@@ -37,6 +35,19 @@ Let's begin exploring application architecture by understanding what are the com
 )
 
 private let exercises: [Episode.Exercise] = [
+  .init(problem: """
+Search for an algorithm online that checks if an integer is prime, and port it to Swift.
+"""),
+  .init(problem: """
+To present modals in SwiftUI one uses the `presentation` method on views that takes a single argument of an optional `Modal` value. If this value is present then the modal will be presented, and if it's `nil` the modal will be dismissed (or if no modal is showing, nothing will happen).
+
+Add an additional `@State` value to our `CounterView` and use it to show and hide the modal when the "Is this prime?" button is tapped.
+"""),
+  .init(problem: """
+Add a `var favoritePrimes: [Int]` field to our `AppState`, and make sure to ping `didChange` when this value is mutated.
+
+Use this new `favoritePrimes` state to render a "Add to favorite primes" / "Remove from favorite primes" button in the modal. Also hook up the action on this button to remove or add the current counter value to the list of favorite primes.
+""")
   // todo
 ]
 
