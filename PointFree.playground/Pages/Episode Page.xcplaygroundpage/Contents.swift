d02fce487fb95ccb411f15ed59f1bd654242f3ca
Brandon Williams <mbw234@gmail.com>
2019-08-08 09:48:22 -0400

    clean up

--- PointFree.playground/Pages/Episode Page.xcplaygroundpage/Contents.swift
+++ PointFree.playground/Pages/Episode Page.xcplaygroundpage/Contents.swift
@@ -1,29 +1,17 @@
-//import PointFree
-import Models
+import HttpPipeline
+import PlaygroundSupport
+@testable import PointFree
+@testable import PointFreeTestSupport
+import WebKit
 
-1
+Current = .mock
 
-//
-let eps = allPublicEpisodes + allPrivateEpisodes
+let req = request(to: Route.episode(.right(1)))
+let result = siteMiddleware(connection(from: req)).perform()
+let htmlStr = String(decoding: result.response.body, as: UTF8.self)
 
-eps
-  .flatMap { $0.references }
-  .count
-eps
-  .flatMap { $0.exercises }
-  .count
+let webView = WKWebView(frame: .init(x: 0, y: 0, width: 832, height: 750))
+webView.loadHTMLString(htmlStr, baseURL: nil)
+print(htmlStr)
 
-//
-//
-//
-////Current = .mock
-////
-////let req = request(to: Route.episode(.right(1)))
-////let result = siteMiddleware(connection(from: req)).perform()
-////let htmlStr = String(decoding: result.response.body, as: UTF8.self)
-////
-////let webView = WKWebView(frame: .init(x: 0, y: 0, width: 832, height: 750))
-////webView.loadHTMLString(htmlStr, baseURL: nil)
-////print(htmlStr)
-////
-////PlaygroundPage.current.liveView = webView
+PlaygroundPage.current.liveView = webView
