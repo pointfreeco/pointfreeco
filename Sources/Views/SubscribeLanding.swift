380a756532fc18229da2d37ff72d23dec2582d25
Brandon Williams <mbw234@gmail.com>
2019-08-11 22:51:09 -0400

    Testimonials on new pricing page (#466)
    * Testimonials module in new pricing page.
    
    * wip
    
    * wip
    
    * snaps
    
    * target blank
    
    
    Former-commit-id: 67de571f74886accef969adbbedbce1785439b22

--- Sources/Views/SubscribeLanding.swift
+++ Sources/Views/SubscribeLanding.swift
@@ -527,12 +527,96 @@ private let faqItems = Faq.allFaqs.flatMap { faq in
 }
 
 private let whatPeopleAreSaying = [
-  div(
-    [],
+  gridRow(
     [
-      h3(
-        [`class`([Class.pf.type.responsiveTitle3])],
-        ["What people are saying"]
+      `class`([
+        Class.grid.between(.desktop)
+        ]),
+    ],
+    [
+      gridColumn(
+        sizes: [.mobile: 12],
+        [
+          `class`([
+            Class.padding([.mobile: [.leftRight: 2, .top: 0, .bottom: 3], .desktop: [.leftRight: 4, .top: 0, .bottom: 3]]),
+            Class.grid.center(.desktop),
+            ]),
+        ],
+        [
+          div(
+            [
+              `class`([Class.border.top, Class.padding([.mobile: [.top: 3], .desktop: [.top: 4]])]),
+              style(borderColor(top: Colors.gray850))
+            ],
+            [
+              h3(
+                [`class`([Class.pf.type.responsiveTitle3])],
+                ["What people are saying"]
+              )
+            ]
+          )
+        ]
+      ),
+      div(
+        [
+          `class`([
+            Class.flex.flex,
+            Class.flex.none,
+            Class.size.width100pct,
+            Class.margin([.mobile: [.bottom: 4]]),
+            Class.layout.overflowAuto(.x),
+            testimonialContainer
+            ]),
+        ],
+        Testimonial.all.map { testimonial in
+          div(
+            [
+              `class`([
+                Class.flex.column,
+                Class.flex.flex,
+                Class.pf.colors.bg.gray900,
+                Class.padding([.mobile: [.all: 3]]),
+                Class.margin([.mobile: [.leftRight: 2]]),
+                testimonialItem
+                ]),
+            ],
+            [
+              a(
+                [
+                  href(testimonial.tweetUrl),
+                  target(.blank),
+                  rel(.init(rawValue: "noopener noreferrer")),
+                  `class`([
+                    Class.pf.colors.fg.black,
+                    Class.pf.type.body.leading
+                    ]),
+                  style(flex(grow: 1, shrink: 0, basis: .auto))
+                ],
+                [.text("“\(testimonial.quote)”")]
+              ),
+              a(
+                [
+                  href("https://www.twitter.com/\(testimonial.twitterHandle)"),
+                  `class`([
+                    Class.pf.colors.fg.black,
+                    Class.pf.type.body.leading,
+
+                    ]),
+                ],
+                [
+                  twitterIconImg(fill: "1DA1F3"),
+                  span(
+                    [
+                      `class`([Class.type.medium]),
+                      style(margin(left: .px(3)))
+                    ],
+                    [.text(testimonial.subscriber ?? "@\(testimonial.twitterHandle)")]
+                  )
+                ]
+              )
+            ]
+          )
+        }
       )
     ]
   )
@@ -634,20 +718,35 @@ public let extraSubscriptionLandingStyles =
   Breakpoint.desktop.query(only: screen) {
     extraSubscriptionLandingDesktopStyles
     }
-    <> Breakpoint.mobile.querySelfAndBigger(only: screen) {
-      planItem % width(.pct(100))
-}
+    <> planItem % width(.pct(100))
+    <> testimonialContainer % (
+      height(.px(360))
+        <> key("-webkit-overflow-scrolling", "touch")
+    )
+    <> testimonialItem % (
+      flex(grow: 0, shrink: 0, basis: .auto)
+        <> width(.px(260))
+        <> height(.px(380))
+)
 
-private let extraSubscriptionLandingDesktopStyles =
+private let desktopBorderStyles =
   darkRightBorder % key("border-right", "1px solid #333")
     <> lightRightBorder % key("border-right", "1px solid #e8e8e8")
     <> lightBottomBorder % key("border-bottom", "1px solid #e8e8e8")
+
+private let extraSubscriptionLandingDesktopStyles: Stylesheet =
+  desktopBorderStyles
     <> planItem % width(.pct(25))
+    <> testimonialContainer % height(.px(400))
+    <> testimonialItem % width(.px(340))
+    <> testimonialItem % height(.px(380))
 
 private let darkRightBorder = CssSelector.class("dark-right-border-d")
 private let lightRightBorder = CssSelector.class("light-right-border-d")
 private let lightBottomBorder = CssSelector.class("light-bottom-border-d")
 private let planItem = CssSelector.class("plan-item")
+private let testimonialContainer = CssSelector.class("testimonial-container")
+private let testimonialItem = CssSelector.class("testimonial-item")
 
 private struct Faq {
   let question: String
@@ -716,4 +815,229 @@ We transcribe each video by hand so you can search and reference easily. Click o
 directly to the video.
 """
   )
+}
+
+private struct Testimonial {
+  let quote: String
+  let subscriber: String?
+  let tweetUrl: String
+  let twitterHandle: String
+
+  static let all: [Testimonial] = [
+    Testimonial(
+      quote: """
+There clearly was a before and an after @pointfreeco for me. I've always been an FP enthusiast intimidated by the F-word, but they made that accessible to the rest of us. Highly recommended!
+""",
+      subscriber: "Romain Pouclet",
+      tweetUrl: "https://twitter.com/Palleas/status/1023976413429260288",
+      twitterHandle: "Palleas"
+    ),
+
+    Testimonial(
+      quote: """
+After diving into @pointfreeco series reading Real World Haskell doesn’t seem all that intimidating after all. Major takeaway: the lesser is word “monad” is mentioned the better😅
+""",
+      subscriber: "Ilya",
+      tweetUrl: "https://twitter.com/rehsals/status/1144282266367070209",
+      twitterHandle: "rehsals"
+    ),
+
+    Testimonial(
+      quote: """
+So many concepts presented at #WWDC19  reminded me of @pointfreeco video series. 👏👏 So happy I watched it before coming to San Jose.
+""",
+      subscriber: "Oscar Alvarez",
+      tweetUrl: "https://twitter.com/iojcar/status/1136719341376790528",
+      twitterHandle: "iOjCaR"
+    ),
+
+    Testimonial(
+      quote: """
+@pointfreeco Talk about being ahead of the curve guys… DSLs, Playground Driven Dev, FRP. Great job. I’m sure you inspired many Apple devs. We know who to look to for where to focus next!
+""",
+      subscriber: "Sam Rayner",
+      tweetUrl: "https://twitter.com/samrayner/status/1136634106618568704",
+      twitterHandle: "samrayner"
+    ),
+
+    Testimonial(
+      quote: """
+In February this year I bought the annual subscription pointfree.co and after I watched all videos and played with the sample code and libraries github.com/pointfreeco. I can say it was the best money I spent in the last 12 months.
+""",
+      subscriber: "Luca Ceppelli",
+      tweetUrl: "https://twitter.com/lucaceppelli/status/1136290297242165249",
+      twitterHandle: "lucaceppelli"
+    ),
+
+    Testimonial(
+      quote: """
+I've just subscribed to @pointfreeco for a year. I feel their content pushes the boundary of my knowledge, and it's fun to watch!
+""",
+      subscriber: "Ferran Pujol Camins",
+      tweetUrl: "https://twitter.com/ferranpujolca/status/1130908056169136130",
+      twitterHandle: "ferranpujolca"
+    ),
+
+    Testimonial(
+      quote: """
+Every episode has been amazing on Pointfree, yet somehow, you've managed to make these Parser combinator episodes even better!!! ⭐️⭐️⭐️⭐️⭐️
+""",
+      subscriber: "Mike Abidakun",
+      tweetUrl: "https://twitter.com/mabidakun/status/1129050657783263232",
+      twitterHandle: "mabidakun"
+    ),
+
+    Testimonial(
+      quote: """
+We have this thing called WWTV at #PlanGrid where we mostly just listen to @mbrandonw and @stephencelis talk about functions.
+""",
+      subscriber: "Arjun Nayini",
+      tweetUrl: "https://twitter.com/anayini/status/1129104381566001152",
+      twitterHandle: "anayini"
+    ),
+
+    Testimonial(
+      quote: """
+Thanks @mbrandonw @stephencelis for the very pedagogical series with @pointfreeco Excited and looking forward to learn from the series
+""",
+      subscriber: "Prakash Rajendran",
+      tweetUrl: "https://twitter.com/dearprakash/status/1063165370159259648",
+      twitterHandle: "dearprakash"
+    ),
+
+    Testimonial(
+      quote: """
+Just became a subscriber! I'm binge watching episodes now! Great content! I'm learning so much from you guys. The repo for the site is the best go - to reference for a well done project and swift-web is something I am definitely going to use in my projects. Thanks for everything!
+""",
+      subscriber: "William Savary",
+      tweetUrl: "https://twitter.com/NSHumanBeing/status/1043141855884587008",
+      twitterHandle: "NSHumanBeing"
+    ),
+
+    Testimonial(
+      quote: """
+Due to the amount of discussions that reference @pointfreeco, we added their logo as an emoji in our slack.
+""",
+      subscriber: "Rui Peres",
+      tweetUrl: "https://twitter.com/peres/status/1020263301039689733",
+      twitterHandle: "peres"
+    ),
+
+    Testimonial(
+      quote: """
+Every single episode of @pointfreeco has been mind blowing. I feel like I've grown a lot as a developer since I started learning Swift and this kind of tutorials definitely help. Functional High Five to @mbrandonw and @stephencelis 🙌🏼
+""",
+      subscriber: "Romain Pouclet",
+      tweetUrl: "https://twitter.com/Palleas/status/978997094408212480",
+      twitterHandle: "Palleas"
+    ),
+
+    Testimonial(
+      quote: """
+Watching the key path @pointfreeco episodes, and I am like 🤯🤯🤯. Super cool
+""",
+      subscriber: "Felipe Espinoza",
+      tweetUrl: "https://twitter.com/fespinozacast/status/978997512500666368",
+      twitterHandle: "fespinozacast"
+    ),
+
+    Testimonial(
+      quote: """
+tfw you are excited for a 4 hour train ride because you'll have time to watch the new @pointfreeco episode 🤓🏔🚂 #MathInTheAlps #typehype
+""",
+      subscriber: "Meghan Kane",
+      tweetUrl: "https://twitter.com/meghafon/status/978624999866105859",
+      twitterHandle: "meghafon"
+    ),
+
+    Testimonial(
+      quote: """
+@pointfreeco ❤️: Thank you! 🧠: … The brain can’t say anything. It is blown away (🤯)!
+""",
+      subscriber: "Rajiv Jhoomuck",
+      tweetUrl: "https://twitter.com/rajivjhoomuck/status/973178777768480771",
+      twitterHandle: "rajivjhoomuck"
+    ),
+
+    Testimonial(
+      quote: """
+the @pointfreeco videos are fantastic, and such a gentle introduction to important ideas — seriously worth a look regardless of functional experience level
+""",
+      subscriber: "tom burns",
+      tweetUrl: "https://twitter.com/tomburns/status/972535717300666368",
+      twitterHandle: "tomburns"
+    ),
+
+    Testimonial(
+      quote: """
+I haven't really done much Swift, but watched this out of curiosity, & it's really well done. Perfectly walks through the mindset of a dev w/ an OO background & addresses their concerns one by one with solid info explaining the benefits of taking the FP approach. (The how & why.)
+""",
+      subscriber: "Josh Burgess",
+      tweetUrl: "https://twitter.com/_joshburgess/status/971169503890624513",
+      twitterHandle: "_joshburgess"
+    ),
+
+    Testimonial(
+      quote: """
+I listened to the first two episodes of @pointfreeco this weekend and it was the best presentation of FP fundamentals I've seen. Very thoughtful layout and progression of the material and motivations behind each introduced concept. Looking forward to watching the rest!
+""",
+      subscriber: "Christina Lee",
+      tweetUrl: "https://twitter.com/RunChristinaRun/status/968920979320709120",
+      twitterHandle: "RunChristinaRun"
+    ),
+
+    Testimonial(
+      quote: """
+Really love this episode - thanks @mbrandonw + @stephencelis! Understanding Swift types in terms of algebraic data types is such an elegant way of seeing the # of possible values your Swift types will represent 🤯 #Simplifyallthethings #GoodbyeComplexity
+""",
+      subscriber: "Meghan Kane",
+      tweetUrl: "https://twitter.com/meghafon/status/966766186221461504",
+      twitterHandle: "meghafon"
+    ),
+
+    Testimonial(
+      quote: """
+Point-Free is really challenging my perspective and approach to Swift. Props to @mbrandonw and @stephencelis for creating an interesting and engaging video series!
+""",
+      subscriber: "Hesham Salman",
+      tweetUrl: "https://twitter.com/_IronHam/status/967496514506543104",
+      twitterHandle: "_IronHam"
+    ),
+
+    Testimonial(
+      quote: """
+I really love the dynamics of @pointfreeco. The dance of “this is super nice because…” “yes, BUT….”. they clearly show what’s good, what’s not so good and keep continuously improving.
+""",
+      subscriber: "Alejandro Martinez",
+      tweetUrl: "https://twitter.com/alexito4/status/1158982960466550784",
+      twitterHandle: "alexito4"
+    ),
+
+    Testimonial(
+      quote: """
+Just finished the mini-series on enum properties by @pointfreeco! They pointed out what’s missing from enums in Swift and used SwiftSyntax to generate code to add the missing parts. Thanks for your work @stephencelis and @mbrandonw! #pointfree
+""",
+      subscriber: "David Piper",
+      tweetUrl: "https://twitter.com/HeyDaveTheDev/status/1142664959509368832",
+      twitterHandle: "HeyDaveTheDev"
+    ),
+
+    Testimonial(
+      quote: """
+So many concepts presented at #WWDC19  reminded me of @pointfreeco video series. 👏👏 So happy I watched it before coming to San Jose.
+""",
+      subscriber: "Oscar Alvarez",
+      tweetUrl: "https://twitter.com/iOjCaR/status/1136719341376790528",
+      twitterHandle: "iOjCaR"
+    ),
+
+    Testimonial(
+      quote: """
+I just watched the episode of @pointfreeco where Brandon and Stephen explain how curry is derived and it ALMOST literally blew my mind. Decomposed things are so simple to grasp ❤ I love how this series decompose and explains things very granular and simple
+""",
+      subscriber: "Esteban Torres",
+      tweetUrl: "https://twitter.com/esttorhe/status/1063420584044965888",
+      twitterHandle: "esttorhe"
+    )
+  ]
 }
--- Tests/PointFreeTests/SubscriptionLandingTests.swift
+++ Tests/PointFreeTests/SubscriptionLandingTests.swift
@@ -37,8 +37,8 @@ class SubscriptionLandingTests: TestCase {
       assertSnapshots(
         matching: conn |> siteMiddleware,
         as: [
-          "desktop": .ioConnWebView(size: .init(width: 1080, height: 3600)),
-          "mobile": .ioConnWebView(size: .init(width: 400, height: 3900))
+          "desktop": .ioConnWebView(size: .init(width: 1080, height: 4000)),
+          "mobile": .ioConnWebView(size: .init(width: 400, height: 4400))
         ]
       )
     }
@@ -62,8 +62,8 @@ class SubscriptionLandingTests: TestCase {
       assertSnapshots(
         matching: conn |> siteMiddleware,
         as: [
-          "desktop": .ioConnWebView(size: .init(width: 1080, height: 3600)),
-          "mobile": .ioConnWebView(size: .init(width: 400, height: 3900))
+          "desktop": .ioConnWebView(size: .init(width: 1080, height: 4200)),
+          "mobile": .ioConnWebView(size: .init(width: 400, height: 4600))
         ]
       )
     }
