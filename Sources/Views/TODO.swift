2755dd60514fb73b98504bb709ccfa316a27b426
Stephen Celis <stephen@stephencelis.com>
2019-09-14 23:15:55 -0400

    Upgrade Subscribe Confirmation HTML

--- Sources/Views/TODO.swift
+++ Sources/Views/TODO.swift
@@ -1,5 +1,34 @@
+import Css
 import Html
+import HtmlUpgrade
 
-public func playsinline(_ value: Bool) -> Attribute<Tag.Video> {
+public func playsinline(_ value: Bool) -> Html.Attribute<Html.Tag.Video> {
   return .init("playslinline", value ? "" : nil)
+}
+
+extension HtmlUpgrade.Attribute {
+  public static func id<T>(_ idSelector: CssSelector) -> HtmlUpgrade.Attribute<T> {
+    return .init("id", idSelector.idString ?? "")
+  }
+
+  public static func style<T>(_ style: Stylesheet) -> HtmlUpgrade.Attribute<T> {
+    return .init("style", render(config: Config.inline, css: style))
+  }
+}
+
+extension HtmlUpgrade.Attribute where Element: HtmlUpgrade.HasFor {
+  public static func `for`(_ idSelector: CssSelector) -> HtmlUpgrade.Attribute<Element> {
+    return .init("for", idSelector.idString ?? "")
+  }
+}
+
+extension HtmlUpgrade.ChildOf where Element == HtmlUpgrade.Tag.Head {
+  public static func style(
+    attributes: [HtmlUpgrade.Attribute<HtmlUpgrade.Tag.Style>] = [],
+    _ css: Stylesheet,
+    config: Css.Config = .compact
+    )
+    -> HtmlUpgrade.ChildOf<HtmlUpgrade.Tag.Head> {
+      return .style(unsafe: render(config: config, css: css))
+  }
 }
