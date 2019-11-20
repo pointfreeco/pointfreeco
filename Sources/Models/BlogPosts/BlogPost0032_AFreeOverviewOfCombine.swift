5e77fb7ec26d477528dab66637dd37a1e994e476
Stephen Celis <stephen@stephencelis.com>
2019-11-20 09:05:41 -0500

    Wrap up

--- Sources/Models/BlogPosts/BlogPost0032_AFreeOverviewOfCombine.swift
+++ Sources/Models/BlogPosts/BlogPost0032_AFreeOverviewOfCombine.swift
@@ -8,16 +8,17 @@ Two free videos exploring Apple's new Combine framework, its core components, an
   contentBlocks: [
     Episode.TranscriptBlock(
       content: #"""
----
-
 At this year's WWDC, Apple introduced [the Combine Framework](https://developer.apple.com/documentation/combine), a composable library for handling asynchronous events over time, providing another alternative to open source libraries like [ReactiveSwift](https://github.com/ReactiveCocoa/ReactiveSwift) and [RxSwift](https://github.com/ReactiveX/RxSwift).
 
 The Combine framework is seriously powerful, and is responsible for handling a lot of [SwiftUI](https://developer.apple.com/xcode/swiftui/)'s high-level state management solutions under the hood! It's a great library in its own right, and a worthy addition to your own libraries and application code.
 
 The past couple weeks we have released two completely free videos dedicated to studying the Combine framework from first principles and showing how you can incorporate it into a library or application of your own:
 
-- [The Combine Framework and Effects: Part 1](https://www.pointfree.co/episodes/ep80-the-combine-framework-and-effects-part-1): In this video, we explore Combine's core components, including the `Publisher` and `Subscriber` protocols. We also cover some convenience functions and operators, which are layered over these more basic units.
-- [The Combine Framework and Effects: Part 2](https://www.pointfree.co/episodes/ep81-the-combine-framework-and-effects-part-2): In this video we show how the Combine framework can be used to describe side effects in a reducer-based architecture, like [Redux](https://redux.js.org). We take an existing library we've been experimenting with and use Combine to help solve a critical problem.
+  - [The Combine Framework and Effects: Part 1](https://www.pointfree.co/episodes/ep80-the-combine-framework-and-effects-part-1):
+    In this video, we explore Combine's core components, including the `Publisher` and `Subscriber` protocols. We also cover some convenience functions and operators, which are layered over these more basic units.
+
+  - [The Combine Framework and Effects: Part 2](https://www.pointfree.co/episodes/ep81-the-combine-framework-and-effects-part-2):
+    In this video we show how the Combine framework can be used to describe side effects in a reducer-based architecture, like [Redux](https://redux.js.org). We take an existing library we've been experimenting with and use Combine to help solve a critical problem.
 
 If you're curious about what SwiftUI has to say about architecture (and what it doesn't have to say), we also have 3 free videos that explore just that:
 
@@ -31,8 +32,8 @@ We're pretty excited about what Apple introduced this year and we can't wait to 
       type: .paragraph
     )
   ],
-  coverImage: "TODO",
-  id: 32, // TODO
-  publishedAt: .distantFuture, // TODO
+  coverImage: "https://i.vimeocdn.com/video/829976257.jpg",
+  id: 32,
+  publishedAt: Date(timeIntervalSince1970: 1574229600),
   title: "A Crash Course in Combine"
 )
