89a9dc93c30e9369bfb531a75ea2e9a8f528f620
Stephen Celis <stephen@stephencelis.com>
2019-12-01 19:07:05 -0500

    CM19
    Former-commit-id: 7c69072c74e3ffa4f35a56467acc1fcc71383c04

---
+++ Sources/Models/BlogPosts/BlogPost0033_CyberMonday2019.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+public let post0033_CyberMondaySale = BlogPost(
+  author: .pointfree,
+  blurb: """
+For 24 hours only, we're offering personal Point-Free subscriptions for 30% off the first year!
+""",
+  contentBlocks: [
+    Episode.TranscriptBlock(
+      content: #"""
+Point-Free is getting into the Cyber Monday spirit by offering 30% off the first year to first-time subscribers!
+
+[Click here](https://www.pointfree.co/discounts/cyber-monday-2019) to redeem the coupon code now. The offer will only remain valid through December 2, 2019.
+"""#,
+      timestamp: nil,
+      type: .paragraph
+    )
+  ],
+  coverImage: "https://d3rccdn33rt8ze.cloudfront.net/social-assets/twitter-card-large.png",
+  id: 33,
+  publishedAt: Date(timeIntervalSince1970: 1575266400),
+  title: "Cyber Monday Sale: 30% Off Point-Free"
+)
