a9dfc62710872f26fb2c3e6e9d07fc77d8825285
Brandon Williams <mbw234@gmail.com>
2019-12-18 07:14:47 -0500

    fix

--- Sources/Models/BlogPosts/BlogPost0034_TestingSwiftUI.swift
+++ Sources/Models/BlogPosts/BlogPost0034_TestingSwiftUI.swift
@@ -10,7 +10,7 @@ A free video exploring how to test SwiftUI.
       content: #"""
 This past WWDC, Apple introduced [SwiftUI](https://developer.apple.com/xcode/swiftui/), a rethinking of how to build UI on Apple's platforms. It allows us to think of our views in a declarative manner so that we can simply describe the view hierarchy rather than think about all the messy details of how to coordinate various objects to get UI on the screen.
 
-Even though it's early days, Apple has given us a ton of guidance on how to use this technology, including [an in-depth collection of tutorials](https://developer.apple.com/tutorials/SwiftUI), but one thing that's still missing is how to _test_ SwiftUI.
+Even though it's early days, Apple has given us a ton of guidance on how to use this technology, including an in-depth collection of [tutorials](https://developer.apple.com/tutorials/SwiftUI), but one thing that's still missing is how to _test_ SwiftUI.
 
 This week we explore just that in [a free video](https://www.pointfree.co/episodes/ep85-testable-state-management-the-point)! We explore what it means to test SwiftUI logic and its various state management solutions, including `@Binding`, `@ObservedObject`, and `@State`. [Click here](https://www.pointfree.co/episodes/ep85-testable-state-management-the-point) to watch it today!
 
