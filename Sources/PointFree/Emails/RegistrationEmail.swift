55f5b086867880cef425824eb1366426d80aa4cc
Brandon Williams <mbw234@gmail.com>
2019-12-20 20:35:31 -0500

    wip

--- Sources/PointFree/Emails/RegistrationEmail.swift
+++ Sources/PointFree/Emails/RegistrationEmail.swift
@@ -18,31 +18,39 @@ let registrationEmailView = simpleEmailLayout(registrationEmailBody) <<< { user 
   )
 }
 
-private func registrationEmailBody(user: GitHubUser) -> [Node] {
-  return [
-    emailTable([style(contentTableStyles)], [
-      tr([
-        td([valign(.top)], [
-          div([`class`([Class.padding([.mobile: [.all: 2]])])], [
-            h3([`class`([Class.pf.type.responsiveTitle3])], ["Thanks for signing up!"]),
-            p([`class`([Class.padding([.mobile: [.topBottom: 2]])])], [
-              "You’re one step closer to our video series!"
-              ]),
-
-            p([`class`([Class.padding([.mobile: [.bottom: 2]])])], [
-              """
+private func registrationEmailBody(user: GitHubUser) -> Node {
+  return .emailTable(
+    attribute: [.style(contentTableStyles)],
+    .tr(
+      .td(
+        attribute: [.valign(.top)],
+        .div(
+          attribute: [.class([Class.padding([.mobile: [.all: 2]])])],
+          .h3(
+            attribute: [.class([Class.pf.type.responsiveTitle3])], "Thanks for signing up!"),
+          .p(
+            attribute: [.class([Class.padding([.mobile: [.topBottom: 2]])])],
+            "You’re one step closer to our video series!"
+          ),
+          .p(
+            attribute: [.class([Class.padding([.mobile: [.bottom: 2]])])],
+            """
             To get all that Point-Free has to offer, choose from one of our monthly or yearly plans by clicking
             the link below!
             """
-              ]),
-
-            p([`class`([Class.padding([.mobile: [.topBottom: 2]])])], [
-              a([href(url(to: .pricingLanding)), `class`([Class.pf.components.button(color: .purple)])],
-                ["Choose a subscription plan!"])
-              ])
-            ])
-          ])
-        ])
-      ])
-  ]
+          ),
+          .p(
+            attribute: [.class([Class.padding([.mobile: [.topBottom: 2]])])],
+            .a(
+              attribute: [
+                .href(url(to: .pricingLanding)),
+                .class([Class.pf.components.button(color: .purple)])
+              ],
+              "Choose a subscription plan!"
+            )
+          )
+        )
+      )
+    )
+  )
 }
