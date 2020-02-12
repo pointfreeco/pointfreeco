72e34bfcfbd680a99c375ab33711f765ec2f6a23
Stephen Celis <stephen@stephencelis.com>
2020-02-12 08:19:38 -0500

    GitHub Actions + Swift 5.2 Prep (#554)
    * Swift 5.2 Nightly Support
    
    * GitHub Actions Linux
    
    * wip
    
    * wip
    
    * wip
    
    * Remove sourcery
    
    * wip
    
    * wip
    
    * Partial revert
    
    * 5.1
    
    * wip
    
    * wip
    
    * wip
    
    * Set CI

---
+++ Tests/PointFreeTests/DatabaseTests.swift
@@ -0,0 +1,84 @@
+import Database
+import DatabaseTestSupport
+import GitHub
+import PointFreeTestSupport
+import Prelude
+import Models
+import ModelsTestSupport
+import GitHubTestSupport
+import Logging
+import SnapshotTesting
+import XCTest
+@testable import PointFree
+
+
+final class DatabaseTests: TestCase {
+  func testUpsertUser_FetchUserById() throws {
+    let userA = try Current.database.upsertUser(.mock, "hello@pointfree.co").run.perform().unwrap()
+    let userB = try Current.database.fetchUserById(userA!.id).run.perform().unwrap()
+    XCTAssertEqual(userA?.id, userB?.id)
+    XCTAssertEqual("hello@pointfree.co", userB?.email.rawValue)
+  }
+
+  func testFetchEnterpriseAccount() {
+    let user = Current.database.registerUser(.mock, "blob@pointfree.co").run.perform().right!!
+    let subscription = Current.database.createSubscription(.mock, user.id, true).run.perform().right!!
+
+    let createdAccount = Current.database.createEnterpriseAccount(
+      "Blob, Inc.",
+      "blob.biz",
+      subscription.id
+      )
+      .run
+      .perform()
+      .right!!
+
+    let fetchedAccount = Current.database.fetchEnterpriseAccountForDomain(createdAccount.domain)
+      .run
+      .perform()
+      .right!!
+
+    XCTAssertEqual(createdAccount, fetchedAccount)
+    XCTAssertEqual("Blob, Inc.", createdAccount.companyName)
+    XCTAssertEqual("blob.biz", createdAccount.domain)
+    XCTAssertEqual(subscription.id, createdAccount.subscriptionId)
+  }
+
+  func testCreateSubscription_OwnerIsNotTakingSeat() {
+    let user = Current.database.registerUser(.mock, "blob@pointfree.co")
+      .run
+      .perform()
+      .right!!
+
+    _ = Current.database.createSubscription(.mock, user.id, false)
+      .run
+      .perform()
+      .right!!
+
+    let freshUser = Current.database.fetchUserById(user.id)
+      .run
+      .perform()
+      .right!!
+
+    XCTAssertEqual(nil, freshUser.subscriptionId)
+  }
+
+  func testCreateSubscription_OwnerIsTakingSeat() {
+    let user = Current.database.registerUser(.mock, "blob@pointfree.co")
+      .run
+      .perform()
+      .right!!
+
+    let subscription = Current.database.createSubscription(.mock, user.id, true)
+      .run
+      .perform()
+      .right!!
+
+    let freshUser = Current.database.fetchUserById(user.id)
+      .run
+      .perform()
+      .right!!
+
+    XCTAssertEqual(subscription.id, freshUser.subscriptionId)
+  }
+}
