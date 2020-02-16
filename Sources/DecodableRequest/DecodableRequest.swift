1ab0a457f3b7a707a5f39fa71e47a3e77ac784d6
Brandon Williams <mbw234@gmail.com>
2020-02-16 14:04:25 -0800

    linux fix

--- Sources/DecodableRequest/DecodableRequest.swift
+++ Sources/DecodableRequest/DecodableRequest.swift
@@ -1,4 +1,7 @@
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 import Tagged
 
 public typealias DecodableRequest<A> = Tagged<A, URLRequest> where A: Decodable
