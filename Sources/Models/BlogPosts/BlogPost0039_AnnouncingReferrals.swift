c3edca6ef4a10b82e600ab37da3928a95add2040
Brandon Williams <mbw234@gmail.com>
2020-02-20 16:39:37 -0800

    small updates

--- Sources/Models/BlogPosts/BlogPost0039_AnnouncingReferrals.swift
+++ Sources/Models/BlogPosts/BlogPost0039_AnnouncingReferrals.swift
@@ -3,20 +3,14 @@ import Foundation
 public let post0039_Referrals = BlogPost(
   author: .pointfree,
   blurb: """
-Share Point-Free with your friends and save!
+Refer a friend to Point-Free and get a free month!
 """,
   contentBlocks: [
     .init(
       content: #"""
----
+Today we're excited to announce support for referral bonuses! When friends and colleagues of yours subscribe to Point-Free with your referral link, both of you will get _one month free_.
 
-> When a friend you refer subscribes to Point-Free, both of you will get one month free!
-
----
-
-Today we're excited to announce support for referral bonuses! When friends and colleagues of yours subscribe to Point-Free with a referral link, both of you will get _one month free_.
-
-When you [subscribe to Point-Free](/pricing) your referral link will appear on [your account page](/account).
+If you currently [subscribe](/pricing) to Point-Free you can find your referral link on [your account page](/account).
 """#,
       timestamp: nil,
       type: .paragraph
@@ -25,5 +19,5 @@ When you [subscribe to Point-Free](/pricing) your referral link will appear on [
   coverImage: nil,
   id: 39,
   publishedAt: Date(timeIntervalSince1970: 1582261200),
-  title: "Announcing: Point-Free Referral Bonuses!"
+  title: "Share Point-Free with friends and save!"
 )
