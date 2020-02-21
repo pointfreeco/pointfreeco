3276a01a6bcc35a4f562e7c05e69c1c3e964320f
Stephen Celis <stephen@stephencelis.com>
2020-02-20 23:39:21 -0500

    Simple Referrals Blog Post (#577)
    * Simple Referrals Blog Post
    
    * small updates
    
    Co-authored-by: Brandon Williams <mbw234@gmail.com>

---
+++ Sources/Models/BlogPosts/BlogPost0039_AnnouncingReferrals.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+public let post0039_Referrals = BlogPost(
+  author: .pointfree,
+  blurb: """
+Refer a friend to Point-Free and get a free month!
+""",
+  contentBlocks: [
+    .init(
+      content: #"""
+Today we're excited to announce support for referral bonuses! When friends and colleagues of yours subscribe to Point-Free with your referral link, both of you will get _one month free_.
+
+If you currently [subscribe](/pricing) to Point-Free you can find your referral link on [your account page](/account).
+"""#,
+      timestamp: nil,
+      type: .paragraph
+    ),
+  ],
+  coverImage: nil,
+  id: 39,
+  publishedAt: Date(timeIntervalSince1970: 1582261200),
+  title: "Share Point-Free with friends and save!"
+)
