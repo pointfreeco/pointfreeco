80b57269fcbee7e772482a1cc721eac1328ac3e2
Stephen Celis <stephen@stephencelis.com>
2020-03-03 16:54:52 -0500

    Episode Collection Model + Stubs

---
+++ Sources/Models/Collections/DomainSpecificLanguages.swift
@@ -0,0 +1,42 @@
+extension Episode.Collection {
+  public static let domainSpecificLanguages = Self(
+    blurb: #"""
+TODO
+"""#
+    sections: [
+      .init(
+        blurb: #"""
+TODO
+"""#,
+        coreLessons: [
+          .init(episode: .ep26_domainSpecificLanguages_pt1),
+          .init(episode: .ep27_domainSpecificLanguages_pt2),
+        ],
+        related: [
+          // TODO
+        ],
+        title: "Introduction to DSLs",
+        whereToGoFromHere: #"""
+TODO
+"""#
+      ),
+      .init(
+        blurb: #"""
+TODO
+"""#,
+        coreLessons: [
+          .init(episode: .ep28_anHtmlDsl),
+          .init(episode: .ep29_dslsVsTemplatingLanguages),
+        ],
+        related: [
+          // TODO
+        ],
+        title: "Introduction to DSLs",
+        whereToGoFromHere: #"""
+TODO
+"""#
+      ),
+    ],
+    title: "Domain Specific Languages"
+  )
+}
