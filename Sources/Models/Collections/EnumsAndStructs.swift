57f0ebd3ce32ec77ff8219c9e274eb8abc0dcb64
Stephen Celis <stephen@stephencelis.com>
2020-03-04 12:46:25 -0500

    Episode Collection Model + Stubs (#593)
    * Episode Collection Model + Stubs
    
    * Fix
    
    
    Former-commit-id: 4d1e2ff55d2752f356170209e37e052ef8bba061

---
+++ Sources/Models/Collections/EnumsAndStructs.swift
@@ -0,0 +1,59 @@
+extension Episode.Collection {
+  public static let enumsAndStructs = Self(
+    blurb: #"""
+TODO
+"""#,
+    sections: [
+      .init(
+        blurb: #"""
+TODO
+"""#,
+        coreLessons: [
+          .init(episode: .ep51_structs🤝Enums),
+          .init(episode: .ep52_enumProperties),
+        ],
+        related: [
+          .init(
+            blurb: #"""
+TODO
+"""#,
+            content: .episode(.ep53_swiftSyntaxEnumProperties)
+          ),
+          .init(
+            blurb: #"""
+TODO
+"""#,
+            content: .collection(.algebraicDataTypes)
+          )
+        ],
+        title: "Enum Properties",
+        whereToGoFromHere: #"""
+TODO
+"""#
+      ),
+      .init(
+        blurb: #"""
+TODO
+"""#,
+        coreLessons: [
+          .init(episode: .ep87_theCaseForCasePaths_pt1),
+          .init(episode: .ep88_theCaseForCasePaths_pt2),
+          .init(episode: .ep89_theCaseForCasePaths_pt3),
+        ],
+        related: [
+          .init(
+            blurb: #"""
+TODO
+"""#,
+            content: .episode(.ep90_composingArchitectureWithCasePaths))
+          // TODO: swift-case-paths
+        ],
+        title: "Case Paths",
+        whereToGoFromHere: #"""
+TODO
+"""#
+      ),
+    ],
+    title: "Enums and Structs"
+  )
+}
