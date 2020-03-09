2175110eec02ccb7fb3c932985727a8b4a1c515e
Brandon Williams <mbw234@gmail.com>
2020-03-09 10:53:38 -0700

    wip

--- Sources/Views/Collections/CollectionIndex.swift
+++ Sources/Views/Collections/CollectionIndex.swift
@@ -11,7 +11,7 @@ public let collectionIndex: Node = [
   .gridRow(
     attributes: [
       .class([
-        Class.padding([.mobile: [.leftRight: 2, .top: 3], .desktop: [.leftRight: 4, .top: 4]]),
+        Class.padding([.mobile: [.top: 3], .desktop: [.top: 4]]),
         Class.grid.between(.desktop)
       ]),
       .style(
@@ -44,8 +44,7 @@ public let collectionIndex: Node = [
       .style(maxWidth(.px(1080)) <> margin(topBottom: nil, leftRight: .auto))
     ],
     .fragment(
-      []
-//      Episode.Collection.
+      Episode.Collection.all.map(collectionItem(collection:))
     )
   )
 ]
@@ -60,36 +59,73 @@ private func collectionItem(collection: Episode.Collection) -> ChildOf<Tag.Ul> {
       ]),
       .style(width(.pct(50)))
     ],
-    .div(
+    .a(
       attributes: [
         .class([
-          Class.pf.colors.bg.gray900,
           Class.flex.column,
-          Class.padding([.mobile: [.all: 2]]),
           Class.size.width100pct,
           Class.flex.flex,
+          Class.border.rounded.all
         ]),
+        // TODO: figure out force unwrap
+        .href(url(to: .collections(.show(collection.slug!))))
       ],
-      .p(
+      .div(
         attributes: [
-          .class([
-            Class.type.list.styleNone,
-            Class.padding([.mobile: [.all: 0]]),
-            Class.pf.colors.fg.gray400,
-            Class.pf.type.body.regular,
-            Class.typeScale([.mobile: .r1, .desktop: .r0_875]),
-            Class.pf.colors.fg.gray400
-            ]),
-          .style(flex(grow: 1, shrink: 0, basis: .auto))
+        .class([
+          Class.flex.flex,
+          Class.flex.justify.center,
+          Class.flex.align.center
+        ]),
+          .style(unsafe: #"""
+background: rgb(110,56,186);
+background: linear-gradient(0deg, rgba(110,56,186,1) 0%, rgba(151,77,255,1) 25%, rgba(121,242,176,1) 100%);
+"""#)
         ],
-        // TODO: handle force unwrap
-        .text(collection.blurb!)
-        // TODO: link
+        .img(
+          base64: collectionsIconSvgBase64,
+          type: .image(.svg),
+          alt: "",
+          attributes: [
+            .style(margin(topBottom: .rem(6)))
+          ]
+        )
+      ),
+      .div(
+        attributes: [
+          .class([Class.padding([.mobile: [.all: 2]])])
+        ],
+        .h6(
+          attributes: [
+            .class([Class.pf.type.responsiveTitle7])
+          ],
+          .text("Collection")
+        ),
+        .h4(
+          attributes: [
+            .class([Class.pf.type.responsiveTitle4])
+          ],
+          // TODO: figure out force unwrap
+          .text(collection.title!)
+        ),
+        .p(
+          attributes: [
+            .class([
+              Class.padding([.mobile: [.all: 0]]),
+              Class.pf.colors.fg.gray400,
+              Class.pf.type.body.regular,
+              Class.typeScale([.mobile: .r1, .desktop: .r0_875]),
+              Class.pf.colors.fg.gray400
+            ]),
+            .style(flex(grow: 1, shrink: 0, basis: .auto))
+          ],
+          // TODO: handle force unwrap
+          .text(collection.blurb!)
+          // TODO: link
+        )
       )
     )
   )
 }
-//background: rgb(110,56,186);
-//background: linear-gradient(0deg, rgba(110,56,186,1) 0%, rgba(151,77,255,1) 25%, rgba(121,242,176,1) 100%);
 
 
