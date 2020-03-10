196ee4e1c67c8d034e2d0862508538e24603b526
Stephen Celis <stephen@stephencelis.com>
2020-03-09 20:53:41 -0400

    Collections Finesse (#613)
    Former-commit-id: aabf3d65a2e5796ee41e8c34ee32abbbbc36a1e1

--- Sources/PointFree/Collections/CollectionShow.swift
+++ Sources/PointFree/Collections/CollectionShow.swift
@@ -14,22 +14,31 @@ let collectionMiddleware
     )
     <<< fetchCollectionMiddleware
     <| map(lower)
-    >>> writeStatus(.ok)
-    >=> respond(
-      view: collectionShow,
-      layoutData: { currentUser, currentSubscriberState, currentRoute, collection in
-        SimplePageLayoutData(
-          currentRoute: currentRoute,
-          currentSubscriberState: currentSubscriberState,
-          currentUser: currentUser,
-          data: collection,
-          description: collection.blurb,
-          extraStyles: collectionsStylesheet,
-          style: .base(.some(.minimal(.black))),
-          title: collection.title
+    >>> { conn in
+      let (user, subscriberState, route, collection) = conn.data
+      if collection.sections.count == 1 {
+        return conn.map(const((user, subscriberState, route, collection, collection.sections[0])))
+          |> collectionSectionEndpoint
+      } else {
+        return conn
+          |> writeStatus(.ok)
+          >=> respond(
+            view: collectionShow,
+            layoutData: { currentUser, currentSubscriberState, currentRoute, collection in
+              SimplePageLayoutData(
+                currentRoute: currentRoute,
+                currentSubscriberState: currentSubscriberState,
+                currentUser: currentUser,
+                data: collection,
+                description: collection.blurb,
+                extraStyles: collectionsStylesheet,
+                style: .base(.some(.minimal(.black))),
+                title: collection.title
+              )
+          }
         )
-    }
-)
+      }
+}
 
 private let fetchCollectionMiddleware
   : MT<
