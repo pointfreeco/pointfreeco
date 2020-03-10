d53fb3d3ec16427a1e974413acc93c5f28128718
Stephen Celis <stephen@stephencelis.com>
2020-03-09 21:54:01 -0400

    snaps

--- Tests/PointFreeTests/HomeTests.swift
+++ Tests/PointFreeTests/HomeTests.swift
@@ -14,7 +14,6 @@ import XCTest
 class HomeTests: TestCase {
   override func setUp() {
     super.setUp()
-//    record = true
 
     var e1 = Episode.ep10_aTaleOfTwoFlatMaps
     e1.permission = .subscriberOnly
