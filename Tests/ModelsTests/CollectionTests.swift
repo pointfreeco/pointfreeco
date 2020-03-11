0f43b7c3c1dabcefa7506381b25d667642bdf642
Brandon Williams <mbw234@gmail.com>
2020-03-11 15:02:52 -0700

    Make collection references lazy so that we can do cyclical references. (#631)
    * Make collection references lazy so that we can do cyclical references.
    
    * fix equality func
    
    
    Former-commit-id: f76edf1f8ef2c1887aa0cdffa39ae43ca888f926

---
+++ Tests/ModelsTests/CollectionTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import Models
+
+final class CollectionTests: XCTestCase {
+  func testAllCollections() {
+    XCTAssertEqual(Episode.Collection.all, Episode.Collection.all)
+  }
+}
