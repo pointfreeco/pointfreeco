bd4fb93bcc3fbd5ac7039323178e96158d22ab2f
Stephen Celis <stephen@stephencelis.com>
2020-03-23 13:02:29 -0400

    Bump
    Former-commit-id: b4d4d51330e47fb5266a0c15f4d7fff82fbfb21a

---
+++ Sources/Models/Episodes/0095-AdaptiveStateManagementPt2.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+extension Episode {
+  public static let ep95_adaptiveStateManagement_pt2 = Episode(
+    blurb: """
+In solving a potential performance problem with how the composable architecture interfaces with SwiftUI, we're given a wonderful opportunity to make it more adaptive to many situations.
+""",
+    codeSampleDirectory: "0095-adaptive-state-management-pt2",
+    exercises: _exercises,
+    id: 95,
+    image: "https://i.vimeocdn.com/video/867881736.jpg",
+    length: 40*60 + 25,
+    permission: .subscriberOnly,
+    previousEpisodeInCollection: nil,
+    publishedAt: Date(timeIntervalSince1970: 1584939600),
+    references: [
+      // TODO
+    ],
+    sequence: 95,
+    subtitle: nil,
+    title: "Adaptive State Management: State",
+    trailerVideo: .init(
+      bytesLength: 42191494,
+      downloadUrl: "https://player.vimeo.com/external/399723100.hd.mp4?s=e4fe717ad3522f4cd45899582c69bc98dac29670&profile_id=175&download=1",
+      streamingSource: "https://player.vimeo.com/video/399723100"
+    )
+  )
+}
+
+private let _exercises: [Episode.Exercise] = [
+  // TODO
+  // TODO: Exercise for computed property state
+]
