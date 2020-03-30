b7037d05365e07c175016fdda13f4fffbc86ee9f
Stephen Celis <stephen@stephencelis.com>
2020-03-29 21:53:44 -0400

    96

---
+++ Sources/Models/Episodes/0096-AdaptiveStateManagementPt3.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+extension Episode {
+  public static let ep96_adaptiveStateManagement_pt3 = Episode(
+    blurb: """
+When we fixed a performance problem in the Composable Architecture it gave us an opportunity to adapt the state of our application to many situations. We'll take these learnings to see how our application's actions may adapt as well.
+""",
+    codeSampleDirectory: "0096-adaptive-state-management-pt3",
+    exercises: _exercises,
+    id: 96,
+    image: "TODO",
+    length: 0, // TODO
+    permission: .subscriberOnly,
+    previousEpisodeInCollection: nil,
+    publishedAt: Date(timeIntervalSince1970: 1585544400),
+    references: [
+      // TODO
+    ],
+    sequence: 96,
+    subtitle: nil,
+    title: "Adaptive State Management: Actions",
+    trailerVideo: .init(
+      bytesLength: 34672144,
+      downloadUrl: "https://player.vimeo.com/external/401892473.hd.mp4?s=262d18bedcefeeccc9851209d8f5ae9ba30d0bbe&profile_id=175&download=1",
+      streamingSource: "https://player.vimeo.com/video/401892473"
+    )
+  )
+}
+
+private let _exercises: [Episode.Exercise] = [
+  // TODO
+]
