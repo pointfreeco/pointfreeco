86ad9d4d5c89f2ae60247d292f44038bb613dfd0
Brandon Williams <mbw234@gmail.com>
2020-04-05 21:49:15 -0700

    Update transcripts.
    Former-commit-id: 1abce772580d6d1df9c2cfdf33cd0be64b132a00

---
+++ Sources/Models/Episodes/0097-AdaptiveStateManagementPt4.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+extension Episode {
+  public static let ep97_adaptiveStateManagement_pt4 = Episode(
+    blurb: """
+We've now shown that the composable architecture is also quite adaptive, allowing us to transform state and actions into very domain specific situations. Let's exercise those muscles by creating a macOS app from our iOS app and demonstrating that it's the perfect tool for creating cross-platform applications.
+""",
+    codeSampleDirectory: "0097-adaptive-state-management-pt4",
+    exercises: _exercises,
+    id: 97,
+    image: "https://i.vimeocdn.com/video/874349070.jpg",
+    length: 27 * 60 + 26,
+    permission: .subscriberOnly,
+    previousEpisodeInCollection: 96,
+    publishedAt: Date(timeIntervalSince1970: 1586149200),
+    references: [
+      // TODO
+    ],
+    sequence: 97,
+    subtitle: nil,
+    title: "Adaptive State Management: The Point",
+    trailerVideo: .init(
+      bytesLength: 62_095_501,
+      downloadUrl: "https://player.vimeo.com/external/404459141.hd.mp4?s=aa1de98dd9c23fa6298bf077c8b551b50f57cafd&profile_id=175&download=1",
+      streamingSource: "https://player.vimeo.com/video/404459141"
+    )
+  )
+}
+
+private let _exercises: [Episode.Exercise] = [
+  // TODO
+]
