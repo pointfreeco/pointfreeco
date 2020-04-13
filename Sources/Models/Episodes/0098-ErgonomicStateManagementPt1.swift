b9c61ac9d9bcde5e77609d9d4d63c15306edd352
Stephen Celis <stephen@stephencelis.com>
2020-04-12 21:51:40 -0400

    99 prep
    Former-commit-id: 4362de3ef3966baaf6677b426d99d906383ed2ed

---
+++ Sources/Models/Episodes/0098-ErgonomicStateManagementPt1.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+extension Episode {
+  public static let ep98_ergonomicStateManagement_pt1 = Episode(
+    blurb: """
+Over many episodes we've now built out an architecture that not only solves the problems we set out to solve, but we've made it performant and adaptable. But before sharing it with everyone, let's polish it with a focus on ergonomics.
+""",
+    codeSampleDirectory: "TODO",
+    exercises: _exercises,
+    fullVideo: nil,
+    id: 98,
+    image: "TODO",
+    length: 27*60 + 45,
+    permission: .subscriberOnly,
+    previousEpisodeInCollection: 97,
+    publishedAt: Date(timeIntervalSince1970: 1586754000),
+    references: [
+      // TODO
+    ],
+    sequence: 98,
+    subtitle: nil,
+    title: "Ergonomic State Management: Part 1", // TODO: Polished State Management?
+    trailerVideo: .init(
+      bytesLength: 0, // TODO
+      downloadUrl: "TODO",
+      streamingSource: "TODO"
+    )
+  )
+}
+
+private let _exercises: [Episode.Exercise] = [
+  // TODO
+]
