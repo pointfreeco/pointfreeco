9985ef5f863bf170a604bbb9379308b6974e6b17
Stephen Celis <stephen@stephencelis.com>
2020-04-16 11:27:31 -0400

    fix
    Former-commit-id: a9925db86271fe2fb912ce34a366295d75de19bf

--- Sources/Models/Episodes/0098-ErgonomicStateManagementPt1.swift
+++ Sources/Models/Episodes/0098-ErgonomicStateManagementPt1.swift
@@ -5,7 +5,7 @@ extension Episode {
     blurb: """
 The Composable Architecture is robust and solves all of the problems we set out to solve (and more), but we haven't given enough attention to ergonomics. We will enhance one of its core units to be a little friendlier to use and extend, which will bring us one step closing to being ready for production.
 """,
-    codeSampleDirectory: "ergonomic-state-management-pt1",
+    codeSampleDirectory: "0098-ergonomic-state-management-pt1",
     exercises: _exercises,
     fullVideo: nil,
     id: 98,
