15ae13c629f979d5adb1fdde89943443039e0e2b
Stephen Celis <stephen@stephencelis.com>
2020-05-10 22:20:52 -0400

    bump
    Former-commit-id: b32e1d55bf61d45eec52d750f557741432d68424

--- Sources/Models/Episodes/AllEpisodes.swift
+++ Sources/Models/Episodes/AllEpisodes.swift
@@ -100,8 +100,8 @@ extension Episode {
     .ep97_adaptiveStateManagement_pt4,
     .ep98_ergonomicStateManagement_pt1,
     .ep99_ergonomicStateManagement_pt2,
-    .ep100_ATourOfTheComposableArchitecture,
-//    .ep101_ATourOfTheComposableArchitecture_pt2,
+    .ep100_ATourOfTheComposableArchitecture_pt1,
+    .ep101_ATourOfTheComposableArchitecture_pt2,
 //    .ep102_ATourOfTheComposableArchitecture_pt3,
 //    .ep103_ATourOfTheComposableArchitecture_pt4,
   ]
