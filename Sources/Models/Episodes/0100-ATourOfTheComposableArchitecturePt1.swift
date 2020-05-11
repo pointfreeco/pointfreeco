c9182d3c264a26b77d952f4296f469c2947f2b92
Stephen Celis <stephen@stephencelis.com>
2020-05-10 22:20:52 -0400

    bump
    Former-commit-id: 8a1307a756813a9265267ad9eac2e22335fc2408

--- Sources/Models/Episodes/0100-ATourOfTheComposableArchitecturePt1.swift
+++ Sources/Models/Episodes/0100-ATourOfTheComposableArchitecturePt1.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 extension Episode {
-  public static let ep100_ATourOfTheComposableArchitecture = Episode(
+  public static let ep100_ATourOfTheComposableArchitecture_pt1 = Episode(
     blurb: """
 It's our 100th episode 🎉! To celebrate, we are finally releasing the Composable Architecture as an open source library, which means you can start using it in your applications today! Let's take a tour of the library, see how it's changed from what we built in earlier episodes, and build a brand new app with it.
 """,
