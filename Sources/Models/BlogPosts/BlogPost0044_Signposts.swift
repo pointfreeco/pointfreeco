020c4691662b84dab4086de0c3fdf819aef36d5d
Stephen Celis <stephen@stephencelis.com>
2020-05-26 12:45:11 -0400

    wip

--- Sources/Models/BlogPosts/BlogPost0044_Signposts.swift
+++ Sources/Models/BlogPosts/BlogPost0044_Signposts.swift
@@ -27,7 +27,7 @@ Today we are releasing first-party support for instrumenting features built in t
 
 ## Signposts
 
-In 2018 Apple introduced [signposts](https://developer.apple.com/documentation/os/logging/recording_performance_data) (see the WWDC video [here](https://developer.apple.com/videos/play/wwdc2018/405/)), a lightweight and efficient way to measure the performance of any part of your application. By setting up "beginning" and "ending" signposts around a task, you can get immediate aggregate stats on that task in Instruments.app, such as number of times that code path is executed, maximum and minimum duration to execute, as well as average time to execute and standard deviation.
+In 2018 Apple introduced [signposts](https://developer.apple.com/documentation/os/logging/recording_performance_data) (see the WWDC video [here](https://developer.apple.com/videos/play/wwdc2018/405/)), a lightweight and efficient way to measure the performance of any part of your application. By setting up "beginning" and "ending" signposts around a task, you can get immediate aggregate stats on that task in Instruments, such as number of times that code path is executed, maximum and minimum duration to execute, as well as average time to execute and standard deviation.
 
 Signposts, and more generally `os_log`, were designed to be lightweight and highly performant so that we shouldn't be afraid of dropping in dozens or hundreds of this measurements in our application. However, doing that at your whim without any regard to the code quality of your application is likely to lead to a lot of disparate metrics cluttering your logs and application logic.
 
@@ -35,7 +35,7 @@ Lucky for us, features built in the [Composable Architecture](https://www.pointf
 
 ## How?
 
-To instrument any feature built in the Composable Arhcitecture simply drop `.signpost()` on your reducer:
+To instrument any feature built in the Composable Architecture simply tack `.signpost()` onto your reducer:
 
 ```swift
 let featureReducer = Reducer<FeatureState, FeatureAction, FeatureEnvironment> {
@@ -50,9 +50,9 @@ let featureReducer = Reducer<FeatureState, FeatureAction, FeatureEnvironment> {
 
 That's it 🤯.
 
-That will set up beginning and ending signposts for each reducer invocation, allowing you to see how many times each action is sent and how long each reducer takes to execute. This can help find any business logic that is taking too long to execute, and which might benefit by being moved to an effect that is executed on a background thread.
+This will set up beginning and ending signposts for each reducer invocation, allowing you to see how many times each action is sent and how long each reducer takes to execute. This can help find any business logic that is taking too long to execute, which might benefit from being moved to an effect that is executed on a background thread.
 
-This method will also start a signpost when the effect returned from the reducer starts, and end the signpost when the effect completes (either successfully, failed or cancelled). This gives you insight into what effects are currently executing and how long it takes them to finish. This is particularly useful for inspecting the lifecycle of long-living effects. For example, if you start an effect (e.g. a location manager) in `onAppear` and forget to tear down the effect in `onDisappear`, it will clearly show in instruments that the effect was never completed.
+This method will also start a signpost when an effect returned from the reducer starts, and end the signpost when the effect completes (either successfully or by cancellation). This gives you insight into what effects are currently executing and how long it takes them to finish. This is particularly useful for inspecting the lifecycle of long-living effects. For example, if you fire up a long-living effect (e.g. a location manager) in `onAppear` and forget to tear down the effect in `onDisappear`, it will clearly show in instruments that the effect was never completed.
 
 To read the stats from these signposts simply run your application in instruments (⌘I), start with a blank instrument, add the `os_signpost` library by tapping the `+` icon in the top-right, and then start recording your app by tapping the red button in the top-left.
 
@@ -60,7 +60,7 @@ You can apply this method locally if you are interested in just a certain featur
 
 ## Start instrumenting today
 
-We've just released version TODO of the Composable Architecture, and so you can start using this new feature immediately. Let us [know](https://twitter.com/pointfreeco) what you think!
+We've just released version TODO of the Composable Architecture, and so you can start using this new feature immediately. [Let us know](https://twitter.com/pointfreeco) what you think!
 """,
       type: .paragraph
     )
