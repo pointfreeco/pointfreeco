d15c0df9e557b2e26a3f8e79d3715d7d3712da3d
Stephen Celis <stephen@stephencelis.com>
2020-05-27 15:46:29 -0400

    Subscription Deactivation (#667)
    * Subscription Deactivation
    
    * wrong test
    
    * add test
    
    * fix
    
    * Update Sources/Models/SubscriberState.swift
    
    Co-authored-by: Brandon Williams <mbw234@gmail.com>
    
    * fix
    
    Co-authored-by: Brandon Williams <mbw234@gmail.com>

--- Sources/Models/Subscription.swift
+++ Sources/Models/Subscription.swift
@@ -3,6 +3,7 @@ import Stripe
 import Tagged
 
 public struct Subscription: Decodable {
+  public var deactivated: Bool
   public var id: Id
   public var stripeSubscriptionId: Stripe.Subscription.Id
   public var stripeSubscriptionStatus: Stripe.Subscription.Status
@@ -10,12 +11,14 @@ public struct Subscription: Decodable {
   public var userId: User.Id
 
   public init(
+    deactivated: Bool,
     id: Id,
     stripeSubscriptionId: Stripe.Subscription.Id,
     stripeSubscriptionStatus: Stripe.Subscription.Status,
     teamInviteCode: TeamInviteCode,
     userId: User.Id
     ) {
+    self.deactivated = deactivated
     self.id = id
     self.stripeSubscriptionId = stripeSubscriptionId
     self.stripeSubscriptionStatus = stripeSubscriptionStatus
@@ -27,6 +30,7 @@ public struct Subscription: Decodable {
   public typealias TeamInviteCode = Tagged<(Subscription, teamInviteCode: ()), String>
 
   private enum CodingKeys: String, CodingKey {
+    case deactivated
     case id
     case stripeSubscriptionId = "stripe_subscription_id"
     case stripeSubscriptionStatus = "stripe_subscription_status"
