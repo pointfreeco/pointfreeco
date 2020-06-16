f537820472589b9987618d49853a1dab447abd90
Brandon Williams <mbw234@gmail.com>
2020-06-16 09:53:27 -0500

    wip
    Former-commit-id: 5254f1dfb87f08f6d4c63d1a3b631e11d8e3531e

--- Sources/Models/BlogPosts/BlogPost0046-BIPOC_OrginizationDiscount.swift
+++ Sources/Models/BlogPosts/BlogPost0046-BIPOC_OrginizationDiscount.swift
@@ -7,9 +7,14 @@ We are offering free, lifetime subscriptions to Point-Free to any current or pas
 """,
   contentBlocks: [
 #"""
-We currently offer two ways of obtaining a Point-Free subscription at a discount: currently enrolled [students](/blog/posts/10-announcing-student-discounts) can get 50% off their subscription, and people living in certain [regions](/blog/posts/42-regional-discounts) can get 50% off to help make up for currency discrepencies between the United States and other countries.
+Today we are offering free, lifetime [Point-Free](https://www.pointfree.co) subscriptions to any current or past student of certain orginizations that focus on training and education of BIPOC in tech. We will continue to the grow the list of supported organizations as we reach out to more, but for now we support the following:
 
-We would like to extend this further by offering free, lifetime subscriptions to Point-Free to any current or past student of certain orginizations that focus on training and education of BIPOC. If you are such a person and are intersted in gaining access to Point-Free, please [email us](mailto:support@pointfree.co) with proof of enrollment and we'll get you set up right away!
+* [Black Girls Code](https://www.blackgirlscode.com)
+* [Pursuit](https://www.pursuit.org)
+
+If you are a current or past student of one of these organizations and are intersted in gaining access to Point-Free, please [email us](mailto:support@pointfree.co) with proof of enrollment and we'll get you set up right away!
+
+We are incredibly lucky to be able to do what we love by working on Point-Free, but it is thanks to years of opportunity and privilege that were afforded to us based on where we were born and the color of skin. We would like to share this privilege with this offer, which also accompanies two other discounts we offer: [student discounts](/blog/posts/10-announcing-student-discounts) and [regional discounts](/blog/posts/42-regional-discounts). We can, and hope to, do more in the future.
 """#
   ],
   coverImage: nil,
