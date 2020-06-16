e1b03d076aa95df5b337eb5fc96f30be3a1da01b
Brandon Williams <mbw234@gmail.com>
2020-06-16 11:56:46 -0500

    wip
    Former-commit-id: 00e6abd1d4395e35dce505378545e98d28d7d7fc

--- Sources/Models/BlogPosts/BlogPost0046-BIPOC_OrginizationDiscount.swift
+++ Sources/Models/BlogPosts/BlogPost0046-BIPOC_OrginizationDiscount.swift
@@ -7,7 +7,7 @@ We are offering free, lifetime subscriptions to Point-Free to any current or pas
 """,
   contentBlocks: [
 #"""
-We are now offering free, lifetime [Point-Free](https://www.pointfree.co) subscriptions to current and past students of any organization that focuses on tech education and pushing forward equitable change for BIPOC, such as [Black Girls Code](https://www.blackgirlscode.com). If this applies to you and you are interested in gaining access to Point-Free, please [email us](mailto:support@pointfree.co?subject=Student%20Discount) with proof of enrollment and we'll get you set up right away!
+We are now offering free, lifetime [Point-Free](https://www.pointfree.co) subscriptions to current and past students of any organization that focuses on tech education and pushing forward equitable change for BIPOC, such as [Black Girls Code](https://www.blackgirlscode.com). If this applies to you and you are interested in gaining access to Point-Free, please [email us](mailto:support@pointfree.co?subject=Student%20Discount) with some information about your organization and proof of current or past enrollment and we'll get you set up right away!
 
 We are incredibly lucky to be able to do what we love by working on Point-Free, but it is thanks to years of opportunity and privilege that were afforded to us based on where we were born and the color of our skin. We would like to share this privilege with this offer, which now accompanies two other discounts we offer: [student discounts](/blog/posts/10-announcing-student-discounts) and [regional discounts](/blog/posts/42-regional-discounts). We can, and will, do more in the future.
 """#
