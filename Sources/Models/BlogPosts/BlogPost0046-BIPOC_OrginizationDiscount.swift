ac0907bf69f1c89527a2c2638eb14ffbc9035533
Stephen Celis <stephen@stephencelis.com>
2020-06-16 11:23:43 -0400

    Update BlogPost0046-BIPOC_OrginizationDiscount.swift

--- Sources/Models/BlogPosts/BlogPost0046-BIPOC_OrginizationDiscount.swift
+++ Sources/Models/BlogPosts/BlogPost0046-BIPOC_OrginizationDiscount.swift
@@ -3,18 +3,18 @@ import Foundation
 public let post0046_BIPOC_OrginizationDiscount = BlogPost(
   author: .pointfree,
   blurb: """
-We are offering free, lifetime subscriptions to Point-Free to any current or past student of certain orginizations that focus on training and education of BIPOC.
+We are offering free, lifetime subscriptions to Point-Free to any current or past student of certain organizations that focus on training and education of BIPOC.
 """,
   contentBlocks: [
 #"""
-Today we are offering free, lifetime [Point-Free](https://www.pointfree.co) subscriptions to any current or past student of certain orginizations that focus on training and education of BIPOC in tech. We will continue to the grow the list of supported organizations as we reach out to more, but for now we support the following:
+Today we are offering free, lifetime [Point-Free](https://www.pointfree.co) subscriptions to any current or past student of certain organizations that focus on training and education of BIPOC in tech. We will continue to the grow the list of supported organizations as we reach out to more, but for now we support the following:
 
 * [Black Girls Code](https://www.blackgirlscode.com)
 * [Pursuit](https://www.pursuit.org)
 
-If you are a current or past student of one of these organizations and are intersted in gaining access to Point-Free, please [email us](mailto:support@pointfree.co) with proof of enrollment and we'll get you set up right away!
+If you are a current or past student of one of these organizations and are interested in gaining access to Point-Free, please [email us](mailto:support@pointfree.co) with proof of enrollment and we'll get you set up right away!
 
-We are incredibly lucky to be able to do what we love by working on Point-Free, but it is thanks to years of opportunity and privilege that were afforded to us based on where we were born and the color of skin. We would like to share this privilege with this offer, which also accompanies two other discounts we offer: [student discounts](/blog/posts/10-announcing-student-discounts) and [regional discounts](/blog/posts/42-regional-discounts). We can, and hope to, do more in the future.
+We are incredibly lucky to be able to do what we love by working on Point-Free, but it is thanks to years of opportunity and privilege that were afforded to us based on where we were born and the color of our skin. We would like to share this privilege with this offer, which now accompanies two other discounts we offer: [student discounts](/blog/posts/10-announcing-student-discounts) and [regional discounts](/blog/posts/42-regional-discounts). We can, and hope to, do more in the future.
 """#
   ],
   coverImage: nil,
