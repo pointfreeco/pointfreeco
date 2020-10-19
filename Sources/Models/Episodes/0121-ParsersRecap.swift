3f1f8d18e531722c23196b647930992464d2126b
Brandon Williams <mbw234@gmail.com>
2020-10-18 20:39:13 -0400

    Update transcripts.

--- Sources/Models/Episodes/0121-ParsersRecap.swift
+++ Sources/Models/Episodes/0121-ParsersRecap.swift
@@ -3,12 +3,12 @@ import Foundation
 extension Episode {
   public static let ep121_parsersRecap = Episode(
     blurb: """
-Now that we've refamiliarized ourselves with parsing, let's parse something even more complex: XCTest logs. We will parse and pretty-print test output and discover another reusable combinator or two along the way!
+Now that we've refamiliarized ourselves with parsing, let's parse something even more complex: XCTest logs. We will parse and pretty-print the output from `xcodebuild` and discover more reusable combinators along the way.
 """,
     codeSampleDirectory: "0121-parsers-recap-pt3",
     exercises: _exercises,
     id: 121,
-    image: "TODO",
+    image: "https://i.vimeocdn.com/video/977642862.jpg",
     length: 31*60 + 3,
     permission: .subscriberOnly,
     publishedAt: Date(timeIntervalSince1970: 1603083600),
@@ -20,11 +20,8 @@ Now that we've refamiliarized ourselves with parsing, let's parse something even
       ),
     ],
     sequence: 121,
-    subtitle: "Part 3",
-    title: "Parser Combinators Recap",
-    // or:
-//    subtitle: "Part 1",
-//    title: "Parsing and Pretty Printing XCTest Logs",
+    subtitle: "Part 1",
+    title: "Parsing Xcode Logs",
     trailerVideo: .init(
       bytesLength: 45637085,
       vimeoId: 469007590,
