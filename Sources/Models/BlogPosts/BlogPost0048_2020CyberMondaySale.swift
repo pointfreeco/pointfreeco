000bab23e43b5a4274c9133a25f45c409e38386a
Stephen Celis <stephen@stephencelis.com>
2020-11-29 18:19:46 -0500

    Blog Finesse

--- Sources/Models/BlogPosts/BlogPost0048_2020CyberMondaySale.swift
+++ Sources/Models/BlogPosts/BlogPost0048_2020CyberMondaySale.swift
@@ -8,9 +8,23 @@ For the next 24 hours, we're offering personal Point-Free subscriptions for 30% 
   contentBlocks: [
     Episode.TranscriptBlock(
       content: #"""
-We’re happy to announce a rare Point-Free sale this Cyber Monday by offering 30% off the first year!
+We’re happy to announce a [rare Point-Free sale](/discounts/cyber-monday-2020) this Cyber Monday by offering 30% off the first year of your subscription!
 
-[Click here](https://www.pointfree.co/discounts/cyber-monday-2020) to redeem the coupon code now. The offer will only remain valid through November 30, 2020.
+Once subscribed you'll get instant access to all 127 episodes (and growing) of Point-Free, including popular collections that have grown this past year:
+
+- [Composable Architecture](/collections/composable-architecture): We started developing the Composable Architecture from first principles after breaking down the problems app architecture should solve, all the way back in July 2019. We continued to refine things over _17 hours_ (so far) in this collection of episodes, while covering aspects of SwiftUI, Combine, and more general concepts like testing and dependencies! We finally bundled things up into [an open source library](https://github.com/pointfreeco/swift-composable-architecture) so that you can immediately put these concepts to use in your applications!
+
+- [Parsing](/collections/parsing): When we define "parsing" as transforming some kind of unstructured data into something more structured, it turns out that parsing is quite a ubiquitous problem that shows up in a lot of the everyday code we write! It also turns out that functional programming has a lot to say about parsing. We first started covering the topic last year, but many viewers have requested more content, and we have recently picked things up again. Now's the perfect time to jump in!
+
+In addition to these topics, we covered:
+
+- [SwiftUI bindings](/episodes/ep107-composable-swiftui-bindings-the-problem) and how they compose
+- [SwiftUI's new `redacted` API](/episodes/ep115-redacted-swiftui-the-problem)
+- [Combine's Scheduler API](/episodes/ep104-combine-schedulers-testing-time) and how to test Combine code
+- [How to design your app's dependencies](/episodes/ep110-designing-dependencies-the-problem)
+- And what do you get when you design a Swift key path for enums? [Case paths!](https://www.pointfree.co/episodes/ep87-the-case-for-case-paths-introduction)
+
+[Click here](/discounts/cyber-monday-2020) to redeem the coupon code now. The offer will only remain valid through November 30, 2020.
 """#,
       timestamp: nil,
       type: .paragraph
