4b1fa30bd12159c088961de7019bd2884c39c2d9
Brandon Williams <mbw234@gmail.com>
2020-12-20 12:19:02 -0500

    wip

--- Sources/Models/Episodes/0130-ParsingPerformance.swift
+++ Sources/Models/Episodes/0130-ParsingPerformance.swift
@@ -3,7 +3,7 @@ import Foundation
 extension Episode {
   public static let ep130_parsingPerformance = Episode(
     blurb: """
-It is well accepted that hand-rolled, imperative parsers are vastly more performant that parsers built with combinators. However, we show that by employing all of our performance tricks we can get within a stone's throw of the performance of imperative parsers, and with much more maintainable code.
+It is well accepted that hand-rolled, imperative parsers are vastly more performant than parsers built with combinators. However, we show that by employing all of our performance tricks we can get within a stone's throw of the performance of imperative parsers, and with much more maintainable code.
 """,
     codeSampleDirectory: "0130-parsing-performance-pt3",
     exercises: _exercises,
