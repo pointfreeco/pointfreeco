d61471b9dfbf1b70dadb4b55364aa9884f2dcd4b
Stephen Celis <stephen@stephencelis.com>
2021-01-21 14:34:16 -0500

    132 prep

---
+++ Sources/Models/Episodes/0132-ConciseForms.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+extension Episode {
+  public static let ep132_conciseForms = Episode(
+    blurb: """
+TODO
+""",
+    codeSampleDirectory: "0132-concise-forms-pt2",
+    exercises: _exercises,
+    id: 132,
+    image: "TODO",
+    length: 61*60 + 29,
+    permission: .subscriberOnly,
+    publishedAt: Date(timeIntervalSince1970: 1611554400),
+    references: [
+      // TODO
+    ],
+    sequence: 132,
+    subtitle: "Composable Architecture",
+    title: "Concise Forms",
+    trailerVideo: .init(
+      bytesLength: 0, // TODO
+      vimeoId: 0, // TODO
+      vimeoSecret: "TODO"
+    )
+  )
+}
+
+private let _exercises: [Episode.Exercise] = [
+  // TODO
+]
