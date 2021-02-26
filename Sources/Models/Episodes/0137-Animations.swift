b7aec44976041607b196f4b1aa36cebc7f3c06e3
Stephen Celis <stephen@stephencelis.com>
2021-02-26 16:14:55 -0500

    prep

---
+++ Sources/Models/Episodes/0137-Animations.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+extension Episode {
+  public static let ep137_animations = Episode(
+    blurb: """
+TODO
+""",
+    codeSampleDirectory: "0137-swiftui-animation-pt3",
+    exercises: _exercises,
+    id: 137,
+    image: "https://i.vimeocdn.com/video/TODO.jpg",
+    length: 44*60 + 10,
+    permission: .subscriberOnly,
+    publishedAt: .init(timeIntervalSince1970: 1614578400),
+    references: [
+      .init(
+        author: "Point-Free",
+        blurb: "A word game by us, written in the Composable Architecture.",
+        link: "https://www.isowords.xyz",
+        publishedAt: nil,
+        title: "isowords"
+      ),
+    ],
+    sequence: 137,
+    subtitle: "The Point",
+    title: "SwiftUI Animation",
+    trailerVideo: .init(
+      bytesLength: 59535206,
+      vimeoId: 516537386,
+      vimeoSecret: "3bbebea3b8d772d3a4a78f62c13762a41de2d129"
+    )
+  )
+}
+
+private let _exercises: [Episode.Exercise] = [
+  // TODO
+]
