b3e0ba3fab2a76bc96f68cd38a8e9897df34cb62
Brandon Williams <mbrandonw@hey.com>
2021-03-14 18:08:13 -0700

    Update transcripts.

--- Sources/Models/Episodes/0138-BetterTestDependencies.swift
+++ Sources/Models/Episodes/0138-BetterTestDependencies.swift
@@ -3,12 +3,12 @@ import Foundation
 extension Episode {
   public static let ep138_betterTestDependencies = Episode(
     blurb: """
-TODO
+It is possible to write tests in such a way that they precisely describe which dependencies they use and which are not necessary. We explore this by strengthening an existing test suite to be more exhaustive in which dependencies are provided, and show some incredible benefits when doing this.
 """,
     codeSampleDirectory: "0138-better-test-dependencies-pt1",
     exercises: _exercises,
     id: 138,
-    image: "TODO",
+    image: "https://i.vimeocdn.com/video/1084484166.jpg",
     length: 41*60 + 55,
     permission: .subscriberOnly,
     publishedAt: .init(timeIntervalSince1970: 1615784400),
@@ -16,8 +16,8 @@ TODO
       // TODO: dependencies
     ],
     sequence: 138,
-    subtitle: "Better Test Dependencies",
-    title: "Exhaustivity",
+    subtitle: "Exhaustivity",
+    title: "Better Test Dependencies",
     trailerVideo: .init(
       bytesLength: 43986963,
       vimeoId: 522615995,
