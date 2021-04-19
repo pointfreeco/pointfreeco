92b1132022dfe350bf6e52eac850253b1b2124e5
Brandon Williams <mbrandonw@hey.com>
2021-04-18 18:49:28 -0700

    Update transcripts.

--- Sources/Models/Episodes/0142-TourOfIsowords.swift
+++ Sources/Models/Episodes/0142-TourOfIsowords.swift
@@ -3,12 +3,12 @@ import Foundation
 extension Episode {
   public static let ep142_tourOfIsowords = Episode(
     blurb: """
-In past episodes we took a peek behind the curtains of our iOS game, [isowords](https://www.isowords.xyz). Now it's time to draw them wide and dive deep! We will show how to check out and run the project and explore how the Composable Architecture powers the entire application.
+In past episodes we took a peek behind the curtains of our recently released iOS game, [isowords](https://www.isowords.xyz). Now it's time to dive deep into the code base to see how it's built. We'll start by showing our modern approach to project management using SPM and explore how the Composable Architecture powers the entire application.
 """,
     codeSampleDirectory: "0142-tour-of-isowords-pt1",
     exercises: _exercises,
     id: 142,
-    image: "https://i.vimeocdn.com/video/TODO.jpg",
+    image: "https://i.vimeocdn.com/video/1114889004.jpg?mw=1100&mh=619&q=70",
     length: 37*60 + 18,
     permission: .subscriberOnly,
     publishedAt: .init(timeIntervalSince1970: 1618808400),
@@ -16,6 +16,11 @@ In past episodes we took a peek behind the curtains of our iOS game, [isowords](
       .isowords,
       .isowordsGitHub,
       .theComposableArchitecture,
+      reference(
+        forCollection: .composableArchitecture,
+        additionalBlurb: "",
+        collectionUrl: "https://www.pointfree.co/collections/composable-architecture"
+      )
     ],
     sequence: 142,
     subtitle: "Part 1",
