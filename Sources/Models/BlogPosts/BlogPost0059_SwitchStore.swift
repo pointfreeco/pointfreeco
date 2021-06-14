b5a62cd139714e11dff2cb6c12ec601ef3cdd811
Stephen Celis <stephen@stephencelis.com>
2021-06-13 22:57:44 -0500

    wip

--- Sources/Models/BlogPosts/BlogPost0059_SwitchStore.swift
+++ Sources/Models/BlogPosts/BlogPost0059_SwitchStore.swift
@@ -8,13 +8,13 @@ We are adding new tools for handling enum state in the Composable Architecture, 
   contentBlocks: [
     .init(
       content: """
-This week's [episode](/TODO) took a deep dive into how we can embrace some of Swift's most important data modeling tools (optionals and enums) in the Composable Architecture without sacrificing composition of our application's behavior. We showed that one our very own case study applications, the [Tic-Tac-Toe app](/TODO) demo, modeled its root state in a less than ideal fashion: using two optional values instead of an enum. This allows invalid states to be representable in our application, which leaks into application logic making it more complex, and so we'd love to make those states [unrepresentable](/episodes/ep4-algebraic-data-types) by the compiler.
+This week's [episode](/episodes/ep149-derived-behavior-optionals-and-enums) took a deep dive into how we can embrace some of Swift's most important data modeling tools (optionals and enums) in the Composable Architecture without sacrificing composition of our application's behavior. We showed that one our very own case study applications, the [Tic-Tac-Toe app](https://github.com/pointfreeco/swift-composable-architecture/tree/7b42aaced0153c67e6f97659fd7bcfd605428243/Examples/TicTacToe) demo, modeled its root state in a less than ideal fashion: using two optional values instead of an enum. This allows invalid states to be representable in our application, which leaks into application logic making it more complex, and so we'd love to make those states [unrepresentable](/episodes/ep4-algebraic-data-types) by the compiler.
 
-Unfortunately, the [Composable Architecture](/collections/composable-architecture) does not come with the tools necessary to properly use enums for state... well, until today that is. We are [releasing](/TODO) a new version of the library that adds a `pullback` method on `Reducer` and a `SwitchStore` view that are specifically tuned for breaking down behaviors modeled on enum state into behavior for each case of the enum.
+Unfortunately, the [Composable Architecture](/collections/composable-architecture) does not come with the tools necessary to properly use enums for state... well, until today that is. We are [releasing](https://github.com/pointfreeco/swift-composable-architecture/releases/tag/0.19.0) a new version of the library that adds a `pullback` method on `Reducer` and a `SwitchStore` view that are specifically tuned for breaking down behaviors modeled on enum state into behavior for each case of the enum.
 
 ## `Reducer.pullback`
 
-We often want to model the state of a part of our applications using an enum to represent mutually exclusive states. For example, at the root of our application we may separate the logged in and logged out state into cases of an enum:
+We often want to model the state of a part of our applications using an enum to represent mutually exclusive states. For example, at the root of our application we may separate the logged-in and logged-out states into cases of an enum:
 
 ```swift
 enum AppState {
@@ -23,11 +23,11 @@ enum AppState {
 }
 ```
 
-In the Composable Architecture we like to define reducers on sub-state and then use the `pullback` and `combine` operators to piece multiple reducers together into one big reducer that operates on bigger pieces of state. The `pullback` operator accomplishes this by using a `WritableKeyPath` to extract out sub-state, operate on it, and then plug it back into the whole state. So we would hope we could define a reducer for each of the logged in domain and logged out domain that could then be pieced together to operate on the entire app domain.
+In the Composable Architecture we like to define reducers on sub-state and then use the `pullback` and `combine` operators to piece multiple reducers together into one big reducer that operates on bigger pieces of state. The `pullback` operator accomplishes this by using a `WritableKeyPath` to extract out sub-state, operate on it, and then plug it back into the whole state. So we would hope we could define a reducer for each of the logged-in domain and logged-out domain that could then be pieced together to operate on the entire app domain.
 
-However, when state is modeled as an enum we do not have access to key paths. We instead need a way to try to extract a particular case from the state enum, operate on it, and then embed it back into the state enum. This is precisely what [case paths](/TODO) excel at, which is the analagous concept for key paths, but tuned specifically for enums instead.
+However, when state is modeled as an enum we do not have access to key paths. We instead need a way to try to extract a particular case from the state enum, operate on it, and then embed it back into the state enum. This is precisely what [case paths](/collections/enums-and-structs/case-paths) excel at, which is the analogous concept for key paths, but tuned specifically for enums instead.
 
-So, rather than pulling back a reducer along a key path to substate we can instead pull back along a _case path_ to a subcase:
+So, rather than pulling back a reducer along a key path to some sub-state we can instead pull back along a _case path_ to a sub-case:
 
 ```swift
 let loggedInReducer: Reducer<LoggedInState, LoggedInAction, LoggedInEnvironment> = ...
@@ -68,13 +68,13 @@ SwitchStore(self.store) {
 
 ## Performance
 
-The `SwitchStore` view has a few tricks up its sleeve in order to do its job with the best performance possible. If implemented naively, the `SwitchStore` will re-compute its body when any piece of state changes inside the enum. However, we only care when the state enum changes from one case to another case so that we can show the respective view. We don't need to know about all the changes within a particular case.
+The `SwitchStore` view has a few tricks up its sleeve in order to do its job with the best performance possible. If implemented naively, the `SwitchStore` would re-compute its body when any piece of state changes inside the enum. However, we only care when the state enum changes from one case to another case so that we can show the respective view. We don't need to know about all the changes within a particular case.
 
 By taking advantage of Swift's powerful metadata embedded in every Swift program we can write a [function](/TODO) that determines the case of any enum, and so so blazingly fast. This metadata is the same info that SwiftUI uses to its seemingly magic behavior.
 
 ## Try it today
 
-
+Upgrade to the latest version of [swift-composable-architecture](https://github.com/pointfreeco/swift-composable-architecture/releases/tag/0.19.0) to immediately gain access to these tools today, and enhance your applications with the expressiveness of enum state.
 """,
       type: .paragraph
     )
