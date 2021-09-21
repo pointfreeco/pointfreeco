b38592c117f3bebd715f7a1993c4a3ec47c40f68
Stephen Celis <stephen@stephencelis.com>
2021-09-21 14:05:08 -0400

    Fix

--- Sources/Models/Episodes/0160-NavigationTabsAndAlerts.swift
+++ Sources/Models/Episodes/0160-NavigationTabsAndAlerts.swift
@@ -5,7 +5,7 @@ extension Episode {
     blurb: """
 Navigation is a really, really complex topic, and it's going to take us many episodes go deep into it. We will begin our journey by coming up with a precise definition of what "navigation" is, and by exploring a couple simpler forms of navigation.
 """,
-    codeSampleDirectory: "TODO",
+    codeSampleDirectory: "0160-navigation-pt1",
     exercises: _exercises,
     id: 160,
     image: "https://i.vimeocdn.com/video/1246096458-90742d78eb8e3e0c8eebd9ac7f94066075d84238b67345c0957a08b9e871d5d8-d?mw=1600&mh=900&q=70",
