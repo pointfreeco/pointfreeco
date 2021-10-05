9c811130854776c3f73066ec3c5e68c2d8f00c5b
Stephen Celis <stephen@stephencelis.com>
2021-10-05 18:28:12 -0400

    wip

--- Tests/PointFreeTests/EmailTests/WelcomeEmailTests.swift
+++ Tests/PointFreeTests/EmailTests/WelcomeEmailTests.swift
@@ -37,11 +37,13 @@ final class WelcomeEmailTests: TestCase {
   }
 
   func testWelcomeEmail1() {
-    #if os(Linux)
     let emailNodes = welcomeEmailView("", welcomeEmail1Content)(.newUser)
 
+    #if os(Linux)
     assertSnapshot(matching: emailNodes, as: .html)
+    #endif
 
+    #if os(Mac)
     if self.isScreenshotTestingAvailable {
       let webView = WKWebView(frame: NSRect(x: 0, y: 0, width: 600, height: 800))
       webView.loadHTMLString(render(emailNodes), baseURL: nil)
@@ -52,11 +54,13 @@ final class WelcomeEmailTests: TestCase {
   }
 
   func testWelcomeEmail2() {
-    #if os(Linux)
     let emailNodes = welcomeEmailView("", welcomeEmail2Content)(.newUser)
 
+    #if os(Linux)
     assertSnapshot(matching: emailNodes, as: .html)
+    #endif
 
+    #if os(Mac)
     if self.isScreenshotTestingAvailable {
       let webView = WKWebView(frame: NSRect(x: 0, y: 0, width: 600, height: 800))
       webView.loadHTMLString(render(emailNodes), baseURL: nil)
