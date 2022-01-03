b97daa9d9cfce73735b5d07fe62ccb14af707148
Brandon Williams <mbrandonw@hey.com>
2022-01-03 09:52:10 -0800

    Fix blog post todo.

--- Sources/Models/BlogPosts/BlogPost0070_UnobtrusiveRuntimeWarnings.swift
+++ Sources/Models/BlogPosts/BlogPost0070_UnobtrusiveRuntimeWarnings.swift
@@ -3,7 +3,7 @@ import Foundation
 public let post0070_UnobtrusiveRuntimeWarnings = BlogPost(
   author: .pointfree,
   blurb: """
-TODO
+Runtime warnings in libraries are a great way to notify your users that something unexpected has happened, or that an API is being used in an incorrect manner. In this post we give an overview of some techniques that can be employed easily today, as well as discuss a technique for surfacing runtime warnings that is both very visible and unobtrusive.
 """,
   contentBlocks: [
     .init(
