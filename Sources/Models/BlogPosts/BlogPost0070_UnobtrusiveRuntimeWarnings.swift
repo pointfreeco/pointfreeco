6868d87fbdf14491d3a8798708302eff44c2d056
Stephen Celis <stephen@stephencelis.com>
2022-01-04 10:23:55 -0600

    simplify

--- Sources/Models/BlogPosts/BlogPost0070_UnobtrusiveRuntimeWarnings.swift
+++ Sources/Models/BlogPosts/BlogPost0070_UnobtrusiveRuntimeWarnings.swift
@@ -45,12 +45,12 @@ However, raising `SIGTRAP` like this when the debugger is not attached will cras
 @inline(__always) func breakpoint(_ message: @autoclosure () -> String = "") {
   #if DEBUG
     // https://github.com/bitstadium/HockeySDK-iOS/blob/c6e8d1e940299bec0c0585b1f7b86baf3b17fc82/Classes/BITHockeyHelper.m#L346-L370
-    var name: [Int32] = [CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid()]
-    var info: kinfo_proc = kinfo_proc()
+    var name = [CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid()]
+    var info = kinfo_proc()
     var info_size = MemoryLayout<kinfo_proc>.size
 
-    let isDebuggerAttached =
-      sysctl(&name, 4, &info, &info_size, nil, 0) != -1 && info.kp_proc.p_flag & P_TRACED != 0
+    let isDebuggerAttached = sysctl(&name, 4, &info, &info_size, nil, 0) != -1
+      && info.kp_proc.p_flag & P_TRACED != 0
 
     if isDebuggerAttached {
       fputs(
