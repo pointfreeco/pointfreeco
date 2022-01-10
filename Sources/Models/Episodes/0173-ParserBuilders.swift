ac573f4f887b908e32b2db42d32644dcc59d8c58
Stephen Celis <stephen@stephencelis.com>
2022-01-10 17:01:06 -0500

    fix

--- Sources/Models/Episodes/0173-ParserBuilders.swift
+++ Sources/Models/Episodes/0173-ParserBuilders.swift
@@ -12,10 +12,14 @@ Let’s revisit a favorite topic: parsing! After a short recap, we will theorize
     permission: .subscriberOnly,
     publishedAt: Date(timeIntervalSince1970: 1641794400),
     references: [
-      reference(
-        forCollection: .parsing,
-        additionalBlurb: "",
-        collectionUrl: "https://www.pointfree.co/collections/parsing"
+      .init(
+        author: "Brandon Williams & Stephen Celis",
+        blurb: #"""
+> Parsing is a surprisingly ubiquitous problem in programming. Every time we construct an integer or a URL from a string, we are technically doing parsing. After demonstrating the many types of parsing that Apple gives us access to, we will take a step back and define the essence of parsing in a single type. That type supports many wonderful types of compositions, and allows us to break large, complex parsing problems into small, understandable units.
+"""#,
+        link: "https://www.pointfree.co/collections/parsing",
+        publishedAt: nil,
+        title: "Collection: Parsing"
       ),
       .swiftParsing,
       .init(
