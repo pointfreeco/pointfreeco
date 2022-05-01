8ad159ebbf8c626748f46b0c203f869d37d5eb48
Brandon Williams <mbrandonw@hey.com>
2022-05-01 07:53:14 -0700

    VaporRouting blog post.

---
+++ Sources/Models/BlogPosts/BlogPost0075_VaporRouting.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+public let post0075_VaporRouting = BlogPost(
+  author: .pointfree,
+  blurb: """
+TODO
+""",
+  contentBlocks: [
+    // TODO
+  ],
+  coverImage: nil, // TODO
+  id: 75,
+  publishedAt:  Date(timeIntervalSince1970: 1651467600),
+  title: "Open Sourcing vapor-routing"
+)
