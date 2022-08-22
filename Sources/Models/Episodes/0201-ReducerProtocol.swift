118b2a30053784d929170eab22adcdcdaca647d1
Stephen Celis <stephen@stephencelis.com>
2022-08-21 22:37:49 -0400

    bump

--- Sources/Models/Episodes/0201-ReducerProtocol.swift
+++ Sources/Models/Episodes/0201-ReducerProtocol.swift
@@ -3,7 +3,7 @@ import Foundation
 extension Episode {
   public static let ep201_reducerProtocol = Episode(
     blurb: """
-      TODO
+      The Composable Architecture was first released over two years ago, and the core ergonomics haven't changed much since then. It's time to change that: we are going to improve the ergonomics of nearly every facet of creating a feature with the library, and make all new patterns possible.
       """,
     codeSampleDirectory: "0201-reducer-protocol-pt1",
     exercises: _exercises,
@@ -18,11 +18,11 @@ extension Episode {
     subtitle: "The Problem",
     title: "Reducer Protocol",
     trailerVideo: .init(
-      bytesLength: 0,  // TODO
+      bytesLength: 101_300_000,
       downloadUrls: .s3(
-        hd1080: "TODO",
-        hd720: "TODO",
-        sd540: "TODO"
+        hd1080: "0201-trailer-1080p-1d183f8801624cc8a5b6eef34802f030",
+        hd720: "0201-trailer-720p-0f909c5fb9494f8493694f7569b401bb",
+        sd540: "0201-trailer-540p-459b52d3fbe0464e95b5d8d7a2dfb9d0"
       ),
       vimeoId: 740_853_246
     )
