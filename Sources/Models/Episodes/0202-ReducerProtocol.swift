1731f79ae07291ca374444870a20f009665b80bb
stephencelis <stephencelis@users.noreply.github.com>
2022-08-27 20:55:46 +0000

    Run swift-format

--- Sources/Models/Episodes/0202-ReducerProtocol.swift
+++ Sources/Models/Episodes/0202-ReducerProtocol.swift
@@ -10,7 +10,7 @@ extension Episode {
     id: 202,
     length: 52 * 60 + 54,
     permission: .subscriberOnly,
-    publishedAt: Date(timeIntervalSince1970: 1661749200),
+    publishedAt: Date(timeIntervalSince1970: 1_661_749_200),
     references: [
       // TODO
     ],
@@ -24,7 +24,7 @@ extension Episode {
         hd720: "0202-trailer-720p-9044c6654a0c47fa80d95f7059aa4cec",
         sd540: "0202-trailer-540p-7ae2d9c8305043d9ad59d1399678340c"
       ),
-      vimeoId: 742392148
+      vimeoId: 742_392_148
     )
   )
 }
