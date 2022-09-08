aaec22b831fcdb5974356cba976ac72684e761a5
Brandon Williams <mbrandonw@hey.com>
2022-09-08 12:01:43 -0400

    wip

--- Sources/Models/BlogPosts/BlogPost0080_TCAPerformance.swift
+++ Sources/Models/BlogPosts/BlogPost0080_TCAPerformance.swift
@@ -120,6 +120,6 @@ public let post0080_TCAPerformance = BlogPost(
   ],
   coverImage: nil,
   id: 80,
-  publishedAt: Date(timeIntervalSince1970: 1662526800),
+  publishedAt: Date(timeIntervalSince1970: 1662613200),
   title: "Improving Composable Architecture performance"
 )
