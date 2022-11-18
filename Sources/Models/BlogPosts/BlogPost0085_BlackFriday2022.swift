e34805710e7410b5f1065d8f4d1fafdf28cdab31
Brandon Williams <mbrandonw@hey.com>
2022-11-18 15:57:31 -0500

    wip

--- Sources/Models/BlogPosts/BlogPost0085_BlackFriday2022.swift
+++ Sources/Models/BlogPosts/BlogPost0085_BlackFriday2022.swift
@@ -3,76 +3,77 @@ import Foundation
 public let post0085_BlackFriday2022 = BlogPost(
   author: .pointfree,
   blurb: """
-    For the next 24 hours, we're offering a 30% discount on personal Point-Free subscriptions for
-    the first year!
+    We're offering a 30% discount for the first year of a Point-Free subscription!
     """,
   contentBlocks: [
     .init(
       content: """
-        We do this only a few times a year: we're having a [rare, one-day Point-Free
-        sale](/discounts/black-friday-2022) this Black Friday by offering 30% off the first year of
+        We do this only a few times a year: we're having a [rare, Point-Free
+        sale][black-friday-sale] this Black Friday by offering 30% off the first year of
         your subscription!
 
-        Once subscribed you'll get instant access to all 214 episodes (126 hours of video and
-        growing!) of Point-Free content, including popular collections that were created or expanded
-        this past year:
+        Once subscribed you'll get instant access to all [214 episodes][pf] (126 hours of video and
+        growing!) of Point-Free content. This includes popular collections that were created or
+        expanded this year, as well as all of the material we have planned for 2023!
 
-        <!--
-        ### [SwiftUI Navigation](/collections/swiftui/navigation)
+        Here are just a few of our recent additions:
 
-        Navigation is a really, _really_ complex topic, and it took us 9 episodes (and nearly 6
-        hours!) to build up the concepts from first principles. Along the way we wrote a powerful
-        set of tools that allow one to model their domains in the most concise way possible while
-        still making it ergonomic and simple to use SwiftUI's navigation APIs. We even open sourced
-        a [library](https://github.com/pointfreeco/swiftui-navigation) from the content of these
-        episodes!
+        ## [Concurrency][concurrency-collection]
 
-        ### [Dependencies](/collections/dependencies)
+        Swift 5.6 brought all new concurrency tools to the language, including async/await, actors,
+        "sendability", streams, structured concurrency, clocks and more. Understanding all of these
+        tools at once can be overwhelming, so we thought it would be best to uncover them from the
+        perspective of the past tools on Apple's platforms.
 
-        Dependencies can wreak havoc on a codebase, and that's why it's important to understand what
-        a dependency is and the best way to control it. We devoted an entire
-        [series](/collections/dependencies/designing-dependencies) of episodes to defining
-        dependencies and showing how to control them. And then another whole
-        [series](/collections/dependencies/better-test-dependencies) on how to make tests stronger
-        and more exhaustive by using better designed test dependencies.
+        This includes threads, queues, Combine publishers and more. Each of those concurrency
+        tools are powerful in their own way, and even have many features that async/await has,
+        but also have significant shortcomings. Understanding this can help us understand why
+        Swift's native tools were designed the way they were, and help us leverage them to the
+        best of their abilities.
 
-        ### [Tour of isowords](https://www.pointfree.co/collections/tours/isowords)
+        ## [SwiftUI navigation][swiftui-nav-collection]
 
-        We spend a lot of time talking about software best practices, but we also practice what we
-        preach. To demonstrate this we open sourced an entire application (a game no less!) to show
-        how to build a large project in SwiftUI, using the [Composable
-        Architecture](https://github.com/pointfreeco/swift-composable-architecture), with a full test
-        suite, in a modern and modular Xcode project style, and more.
+        Navigation in SwiftUI is complex, but it doesn't need to be. In our [12-part
+        series][swiftui-nav-collection] we show how all of SwiftUI's seemingly disparate forms of
+        navigation (sheets, covers, popovers, links, alerts, and more!) can all be unified under
+        essentially one API. This greatly simplifies the process of modeling the state for
+        navigation in your features, and can even make your code safer and more concise.
 
-        ### [WWDC 2021](/collections/wwdc)
+        ## [Composable Architecture][tca-collection]
 
-        We devoted a series of episodes to exploring some of the cool features announced at WWDC
-        this year, including
-        [.refreshable](/collections/wwdc/wwdc-2021/ep153-async-refreshable-swiftui),
-        [@FocusState](/collections/wwdc/wwdc-2021/ep155-swiftui-focus-state), and
-        [.searchable](/collections/wwdc/wwdc-2021/ep156-searchable-swiftui-part-1). We first explore
-        these APIs from first principles, and then apply them to the [Composable
-        Architecture](https://github.com/pointfreeco/swift-composable-architecture).
+        We expanded our Composable Architecture collection of episodes by modernizing nearly
+        every aspect of the library. It now has first class support for structured concurrency in
+        effects, which allows you to use async/await and tie the lifetime of effects to the lifetime
+        of views.
 
-        ### [SwiftUI Animation](/collections/swiftui/animations)
+        And we introduced a protocol for defining reducers, which has much better ergnomics for
+        implementing and composing features together. It also unlocks a whole new way of managing
+        dependencies that makes is easier and safer to access dependencies from any part of your
+        application code base.
 
-        Animation is one of the most impressive features of SwiftUI, but the APIs can be quite
-        mysterious. Our series of episodes shows how each API can be used and builds a tool for
-        "animating" Combine schedulers, which has amazing applications for vanilla SwiftUI _and_
-        [Composable Architecture](https://github.com/pointfreeco/swift-composable-architecture)
-        applications.
+        ## [Parsers][parsers-collection]
+
+        We made significant updates to our parser library, including better error handling and
+        error messaging, a new builder-style of creating parsers, and the ability to "invert"
+        parsers, which allows you to print values back into a string format. These features make
+        our parser library one of the most powerful ways to turn unstructured data into structured
+        data, and our episodes show how we built it from the ground up.
 
         ## Subscribe today!
 
-        We have plenty of exciting topics planned for 2022, including all new episodes on parsing
-        (improving ergonomics, "reversible" parsing, applications to iOS routing, and _more_),
-        navigation in the
-        [Composable Architecture](https://github.com/pointfreeco/swift-composable-architecture),
-        deep explorations into Swift concurrency, and more!
+        We have plenty of exciting topics planned for 2023, including first class navigation
+        tools for the Composable Architecture, more improvements to our parser builders, as well
+        as all new styles of content (live streams!).
 
-        [Click here](/discounts/cyber-monday-2021) to redeem the coupon code now. The offer is valid
-        for only one day, so you better hurry!
-        -->
+        Be sure to [subscribe today][black-friday-sale] to get access to all of this and more. The
+        offer is valid for only a few days, so you better hurry!
+
+        [tca-collection]: /collections/composable-architecture
+        [concurrency-collection]: /collections/concurrency
+        [black-friday-sale]: /discounts/black-friday-2022
+        [swiftui-nav-collection]: /collections/swiftui/navigation
+        [parsers-collection]: /collections/parsing
+        [pf]: /
         """,
       type: .paragraph
     )
