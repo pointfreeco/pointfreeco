dd77cce841306b53eec8bd0fe0a900dbe4237c61
Brandon Williams <mbrandonw@hey.com>
2022-11-18 12:10:29 -0500

    wip

---
+++ Sources/Models/BlogPosts/BlogPost0085_BlackFriday2022.swift
@@ -0,0 +1,85 @@
+import Foundation
+
+public let post0085_BlackFriday2022 = BlogPost(
+  author: .pointfree,
+  blurb: """
+    For the next 24 hours, we're offering a 30% discount on personal Point-Free subscriptions for
+    the first year!
+    """,
+  contentBlocks: [
+    .init(
+      content: """
+        We do this only a few times a year: we're having a [rare, one-day Point-Free
+        sale](/discounts/black-friday-2022) this Black Friday by offering 30% off the first year of
+        your subscription!
+
+        Once subscribed you'll get instant access to all 214 episodes (126 hours of video and
+        growing!) of Point-Free content, including popular collections that were created or expanded
+        this past year:
+
+        <!--
+        ### [SwiftUI Navigation](/collections/swiftui/navigation)
+
+        Navigation is a really, _really_ complex topic, and it took us 9 episodes (and nearly 6
+        hours!) to build up the concepts from first principles. Along the way we wrote a powerful
+        set of tools that allow one to model their domains in the most concise way possible while
+        still making it ergonomic and simple to use SwiftUI's navigation APIs. We even open sourced
+        a [library](https://github.com/pointfreeco/swiftui-navigation) from the content of these
+        episodes!
+
+        ### [Dependencies](/collections/dependencies)
+
+        Dependencies can wreak havoc on a codebase, and that's why it's important to understand what
+        a dependency is and the best way to control it. We devoted an entire
+        [series](/collections/dependencies/designing-dependencies) of episodes to defining
+        dependencies and showing how to control them. And then another whole
+        [series](/collections/dependencies/better-test-dependencies) on how to make tests stronger
+        and more exhaustive by using better designed test dependencies.
+
+        ### [Tour of isowords](https://www.pointfree.co/collections/tours/isowords)
+
+        We spend a lot of time talking about software best practices, but we also practice what we
+        preach. To demonstrate this we open sourced an entire application (a game no less!) to show
+        how to build a large project in SwiftUI, using the [Composable
+        Architecture](https://github.com/pointfreeco/swift-composable-architecture), with a full test
+        suite, in a modern and modular Xcode project style, and more.
+
+        ### [WWDC 2021](/collections/wwdc)
+
+        We devoted a series of episodes to exploring some of the cool features announced at WWDC
+        this year, including
+        [.refreshable](/collections/wwdc/wwdc-2021/ep153-async-refreshable-swiftui),
+        [@FocusState](/collections/wwdc/wwdc-2021/ep155-swiftui-focus-state), and
+        [.searchable](/collections/wwdc/wwdc-2021/ep156-searchable-swiftui-part-1). We first explore
+        these APIs from first principles, and then apply them to the [Composable
+        Architecture](https://github.com/pointfreeco/swift-composable-architecture).
+
+        ### [SwiftUI Animation](/collections/swiftui/animations)
+
+        Animation is one of the most impressive features of SwiftUI, but the APIs can be quite
+        mysterious. Our series of episodes shows how each API can be used and builds a tool for
+        "animating" Combine schedulers, which has amazing applications for vanilla SwiftUI _and_
+        [Composable Architecture](https://github.com/pointfreeco/swift-composable-architecture)
+        applications.
+
+        ## Subscribe today!
+
+        We have plenty of exciting topics planned for 2022, including all new episodes on parsing
+        (improving ergonomics, "reversible" parsing, applications to iOS routing, and _more_),
+        navigation in the
+        [Composable Architecture](https://github.com/pointfreeco/swift-composable-architecture),
+        deep explorations into Swift concurrency, and more!
+
+        [Click here](/discounts/cyber-monday-2021) to redeem the coupon code now. The offer is valid
+        for only one day, so you better hurry!
+        -->
+        """,
+      type: .paragraph
+    )
+  ],
+  coverImage: nil,
+  hidden: false,
+  id: 85,
+  publishedAt: Date(timeIntervalSince1970: 1669356000),
+  title: "Black Friday Sale: 30% Off Point-Free"
+)
