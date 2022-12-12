a37bf8dd033394be1c95128918ef507aca9ed999
Brandon Williams <mbrandonw@hey.com>
2022-12-12 13:52:35 -0800

    wip

--- Tests/ModelsTests/GiftFormDataTests.swift
+++ Tests/ModelsTests/GiftFormDataTests.swift
@@ -5,4 +5,67 @@ import XCTest
 @testable import Models
 
 final class GiftFormDataTests: TestCase {
+  func testCodable_PaymentIntent() throws {
+    let giftFormData = GiftFormData(
+      deliverAt: nil,
+      fromEmail: "blob@pointfree.co",
+      fromName: "Blob",
+      message: "Happy Birthday!",
+      monthsFree: 12,
+      paymentType: .paymentIntentID("pi_deadbeef"),
+      toEmail: "blob.jr",
+      toName: "Blob Jr."
+    )
+
+    let data = try encoder.encode(giftFormData)
+    _assertInlineSnapshot(matching: String(decoding: data, as: UTF8.self), as: .lines, with: """
+    {
+      "fromEmail" : "blob@pointfree.co",
+      "fromName" : "Blob",
+      "message" : "Happy Birthday!",
+      "monthsFree" : "12",
+      "stripePaymentIntentId" : "pi_deadbeef",
+      "toEmail" : "blob.jr",
+      "toName" : "Blob Jr."
+    }
+    """)
+
+    let roundtripGiftFormData = try JSONDecoder().decode(GiftFormData.self, from: data)
+    XCTAssertEqual(roundtripGiftFormData, giftFormData)
+  }
+
+  func testCodable_PaymentMethod() throws {
+    let giftFormData = GiftFormData(
+      deliverAt: nil,
+      fromEmail: "blob@pointfree.co",
+      fromName: "Blob",
+      message: "Happy Birthday!",
+      monthsFree: 12,
+      paymentType: .paymentMethodID("pm_deadbeef"),
+      toEmail: "blob.jr",
+      toName: "Blob Jr."
+    )
+
+    let data = try encoder.encode(giftFormData)
+    _assertInlineSnapshot(matching: String(decoding: data, as: UTF8.self), as: .lines, with: """
+    {
+      "fromEmail" : "blob@pointfree.co",
+      "fromName" : "Blob",
+      "message" : "Happy Birthday!",
+      "monthsFree" : "12",
+      "paymentMethodID" : "pm_deadbeef",
+      "toEmail" : "blob.jr",
+      "toName" : "Blob Jr."
+    }
+    """)
+
+    let roundtripGiftFormData = try JSONDecoder().decode(GiftFormData.self, from: data)
+    XCTAssertEqual(roundtripGiftFormData, giftFormData)
+  }
 }
+
+private let encoder = {
+  let _encoder = JSONEncoder()
+  _encoder.outputFormatting = [.sortedKeys, .prettyPrinted]
+  return _encoder
+}()
