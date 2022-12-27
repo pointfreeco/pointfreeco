d8473626df7f6f04de1db1e5ef24b674585a0568
Stephen Celis <stephen@stephencelis.com>
2022-12-27 13:28:50 -0500

    wip

--- Package.swift
+++ Package.swift
@@ -45,8 +45,7 @@ var package = Package(
     .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", branch: "async"),
     .package(url: "https://github.com/pointfreeco/swift-tagged", from: "0.9.0"),
     .package(url: "https://github.com/pointfreeco/swift-url-routing", from: "0.1.0"),
-//    .package(url: "https://github.com/pointfreeco/swift-web", revision: "13db455"),
-    .package(path: "../swift-web"),
+    .package(url: "https://github.com/pointfreeco/swift-web", revision: "a33fcff"),
     .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "0.2.0"),
   ],
   targets: [
