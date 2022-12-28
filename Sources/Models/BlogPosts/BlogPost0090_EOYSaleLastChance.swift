eb54e4b107e7de92e29484277137c851275dc18b
stephencelis <stephencelis@users.noreply.github.com>
2022-12-28 17:16:27 +0000

    Run swift-format

--- Sources/Models/BlogPosts/BlogPost0090_EOYSaleLastChance.swift
+++ Sources/Models/BlogPosts/BlogPost0090_EOYSaleLastChance.swift
@@ -59,6 +59,6 @@ public let post0090_2022EOYSaleLastChance = BlogPost(
   coverImage: nil,
   hidden: true,
   id: 90,
-  publishedAt: Date(timeIntervalSince1970: 1672246800),
+  publishedAt: Date(timeIntervalSince1970: 1_672_246_800),
   title: "Last chance to save 25% off Point-Free"
 )
