f024b0102b30987e128eb70855aea0c07d7257f0
Stephen Celis <stephen@stephencelis.com>
2023-01-07 12:43:55 -0500

    wip

--- Sources/PointFree/Bootstrap.swift
+++ Sources/PointFree/Bootstrap.swift
@@ -15,16 +15,20 @@ public func bootstrap() -> EitherIO<Error, Prelude.Unit> {
   Backtrace.install()
 
   return EitherIO.debug(prefix: "⚠️ Bootstrapping PointFree...")
-    .flatMap(const(connectToPostgres))
+    .flatMap(const(connectToPostgres()))
     .flatMap(const(.debug(prefix: "✅ PointFree Bootstrapped!")))
 }
 
 private let stepDivider = EitherIO.debug(prefix: "  -----------------------------")
 
-private let connectToPostgres =
-  EitherIO.debug(prefix: "  ⚠️ Connecting to PostgreSQL at \(DependencyValues._current.envVars.postgres.databaseUrl)")
-  .flatMap { _ in EitherIO { try await DependencyValues._current.database.migrate() } }
+private func connectToPostgres() -> EitherIO<Error, Prelude.Unit> {
+  @Dependency(\.envVars.postgres.databaseUrl) var databaseUrl
+  @Dependency(\.database.migrate) var migrate
+
+  return EitherIO.debug(prefix: "  ⚠️ Connecting to PostgreSQL at \(databaseUrl)")
+  .flatMap { _ in EitherIO { try await migrate() } }
   .catch { EitherIO.debug(prefix: "  ❌ Error! \($0)").flatMap(const(throwE($0))) }
   .retry(maxRetries: 999_999, backoff: const(.seconds(1)))
   .flatMap(const(.debug(prefix: "  ✅ Connected to PostgreSQL!")))
   .flatMap(const(stepDivider))
+}
