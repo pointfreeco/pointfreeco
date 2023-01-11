f1328fe23e957d9d5d2a77fefca7dc35eebf05da
Brandon Williams <mbrandonw@hey.com>
2023-01-11 12:20:57 -0800

    wip

---
+++ Sources/Server/Server.swift
@@ -0,0 +1,30 @@
+import Dependencies
+import HttpPipeline
+import NIO
+import PointFree
+import Prelude
+
+@main
+struct Server {
+  static func main() async throws {
+    @Dependency(\.envVars) var envVars
+    @Dependency(\.mainEventLoopGroup) var eventLoopGroup: any EventLoopGroup
+
+    // Bootstrap
+    _ =
+    try await PointFree
+      .bootstrap()
+      .run
+      .performAsync()
+      .unwrap()
+
+    // Server
+    run(
+      siteMiddleware,
+      on: envVars.port,
+      eventLoopGroup: eventLoopGroup,
+      gzip: true,
+      baseUrl: envVars.baseUrl
+    )
+  }
+}
