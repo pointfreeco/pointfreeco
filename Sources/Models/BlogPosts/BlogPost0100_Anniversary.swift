685b11cbbbdf596499df6b446e502dbbfaa4750e
Brandon Williams <mbrandonw@hey.com>
2023-01-23 13:30:05 -0800

    wip

--- Sources/Models/BlogPosts/BlogPost0100_Anniversary.swift
+++ Sources/Models/BlogPosts/BlogPost0100_Anniversary.swift
@@ -48,10 +48,11 @@ public let post0100_Anniversary = BlogPost(
         ## Point-Free community slack
 
         We currently field questions and comments from many different places: emails sent to us
-        personally, [discussions][tca-discussions] on our GitHub repos, the [Swift
-        forums][tca-forums], and there's a [popular Slack](http://iosdevelopers.slack.com) with an
-        active #ComposableArchitecture channel. We really enjoy interacting with the community, but
-        it can be difficult to keep track of conversations in so many places.
+        personally, [Twitter](http://twitter.com/pointfreeco), [discussions][tca-discussions] on our
+        GitHub repos, the [Swift forums][tca-forums], and there's a [popular
+        Slack](http://iosdevelopers.slack.com) with an active #ComposableArchitecture channel. We
+        really enjoy interacting with the community, but it can be difficult to keep track of
+        conversations in so many places.
 
         That is why we are excited to announce a new Slack just for the Point-Free community:
 
