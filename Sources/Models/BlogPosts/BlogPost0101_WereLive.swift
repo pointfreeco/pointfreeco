ab2a620b3ac7f92551f55f781cf9ec5193eb028a
Brandon Williams <135203+mbrandonw@users.noreply.github.com>
2023-02-01 11:15:08 -0800

    Update BlogPost0101_WereLive.swift

--- Sources/Models/BlogPosts/BlogPost0101_WereLive.swift
+++ Sources/Models/BlogPosts/BlogPost0101_WereLive.swift
@@ -26,6 +26,7 @@ public let post0101_Anniversary = BlogPost(
     ),
   ],
   coverImage: nil,
+  hidden: true,
   id: 101,
   publishedAt: yearMonthDayFormatter.date(from: "2023-02-01")!
     .addingTimeInterval(60 * 60 * 17 - 60 * 40),  // 4:20pm GMT
