b696604a054aab31345ea36eda421e6e341233ff
Brandon Williams <mbrandonw@hey.com>
2023-03-01 13:30:09 -0800

    wip

--- Tests/TranscriptsTests/TranscriptsTests.swift
+++ Tests/TranscriptsTests/TranscriptsTests.swift
@@ -1,26 +1,26 @@
-import CustomDump
-import Transcripts
-import XCTest
+#if !OSS
+  import CustomDump
+  import Transcripts
+  import XCTest
 
-final class TranscriptsTests: XCTestCase {
-  func testBasics() throws {
-    #if !OSS
+  final class TranscriptsTests: XCTestCase {
+    func testBasics() throws {
       Episode.bootstrapPrivateEpisodes()
-    #endif
-    for episode in Episode.all where episode.permission == .free {
-      print("Episode #", episode.sequence)
-      let text = String(Substring(try blocksParser.print(Array(episode.transcriptBlocks))))
-      try! Data(text.utf8).write(
-        to: URL(fileURLWithPath: #filePath)
-          .deletingLastPathComponent()
-          .deletingLastPathComponent()
-          .deletingLastPathComponent()
-          .appendingPathComponent("Sources")
-          .appendingPathComponent("Transcripts")
-          .appendingPathComponent("Resources")
-          .appendingPathComponent(String(format: "%04d", episode.sequence.rawValue))
-          .appendingPathExtension("md")
-      )
+      for episode in Episode.all where episode.permission == .free {
+        print("Episode #", episode.sequence)
+        let text = String(Substring(try blocksParser.print(Array(episode.transcriptBlocks))))
+        try! Data(text.utf8).write(
+          to: URL(fileURLWithPath: #filePath)
+            .deletingLastPathComponent()
+            .deletingLastPathComponent()
+            .deletingLastPathComponent()
+            .appendingPathComponent("Sources")
+            .appendingPathComponent("Transcripts")
+            .appendingPathComponent("Resources")
+            .appendingPathComponent(String(format: "%04d", episode.sequence.rawValue))
+            .appendingPathExtension("md")
+        )
+      }
     }
   }
-}
+#endif
