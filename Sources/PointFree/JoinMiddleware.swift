34871be6c315e09f547d0106c77ffa4c14557462
Brandon Williams <mbrandonw@hey.com>
2023-06-02 17:00:33 -0400

    wip

--- Sources/PointFree/JoinMiddleware.swift
+++ Sources/PointFree/JoinMiddleware.swift
@@ -5,16 +5,78 @@ import HttpPipeline
 import PointFreeRouter
 
 func joinMiddleware(_ conn: Conn<StatusLineOpen, Join>) async -> Conn<ResponseEnded, Data> {
+  @Dependency(\.currentUser) var currentUser
   @Dependency(\.database) var database
+  @Dependency(\.fireAndForget) var fireAndForget
 
   switch conn.data {
-  case let .code(code):
+  case let .join(code: code, email: email):
+    guard let currentUser = currentUser
+    else {
+      return conn
+        .redirect(to: .join(.landing(code: code))) {
+          $0.flash(.notice, "You must be logged in to complete that action.")
+        }
+    }
+
+    let subscription: Subscription
+    do {
+      subscription = try await database.fetchSubscriptionByTeamInviteCode(code)
+    } catch {
+      return conn
+        .redirect(to: .home) {
+          $0.flash(.error, "Could not find that team.")
+        }
+    }
+
+    guard let email = email
+    else {
+      do {
+        try await database.addUserIdToSubscriptionId(currentUser.id, subscription.id)
+        fireAndForget {
+          // TODO: send emails to new subscriber and owner
+        }
+        return conn
+          .redirect(to: .account()) {
+            $0.flash(.notice, "You now have access to Point-Free!")
+          }
+      } catch {
+        return conn
+          .redirect(to: .home) {
+            $0.flash(
+              .error,
+              "Could not add to the team. Try again or contact support@pointfree.co."
+            )
+          }
+      }
+    }
+
+    await fireAndForget {
+      try await sendEmail(
+        to: [email],
+        subject: "Confirm your email to join the Point-Free team subscription.",
+        content: .left("Test")
+      )
+    }
+    return conn
+      .redirect(to: .home) {
+        $0.flash(.notice, "Confirmation email sent to \(email.rawValue).")
+      }
+
+  case let .landing(code):
     do {
       let subscription = try await database.fetchSubscriptionByTeamInviteCode(code)
       let isDomain = code.rawValue.contains(".")
       return conn
         .writeStatus(.ok)
-        .respond(text: "Hello: \(code) - \(subscription.stripeSubscriptionId.rawValue)")
+        .respond(
+          html: """
+            <form action="/join/\(code)" method="post">
+              <input type="text" name="email">
+              <input type="hidden" name="code" value="\(code)">
+            </form>
+            """
+        )
     } catch {
       return conn
         .redirect(to: .home) {
--- Sources/PointFreeRouter/JoinRouter.swift
+++ Sources/PointFreeRouter/JoinRouter.swift
@@ -1,14 +1,28 @@
+import EmailAddress
 import Models
 import URLRouting
 
 public enum Join: Equatable {
-  case code(Subscription.TeamInviteCode)
+  case join(code: Subscription.TeamInviteCode, email: EmailAddress?)
+  case landing(code: Subscription.TeamInviteCode)
 }
 
 struct JoinRouter: ParserPrinter {
   var body: some Router<Join> {
     OneOf {
-      Route(.case(Join.code)) {
+      Route(.case(Join.join)) {
+        Method.post
+        Path {
+          Parse(.string.representing(Subscription.TeamInviteCode.self))
+        }
+        Query {
+          Optionally {
+            Field("email", .string.representing(EmailAddress.self))
+          }
+        }
+      }
+
+      Route(.case(Join.landing)) {
         Path {
           Parse(.string.representing(Subscription.TeamInviteCode.self))
         }
