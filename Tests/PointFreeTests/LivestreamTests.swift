25529cd4ad96c464c2021e0f8b92b5242cae1da5
Brandon Williams <mbrandonw@hey.com>
2024-06-03 18:23:54 -0500

    wip

--- Tests/PointFreeTests/LivestreamTests.swift
+++ Tests/PointFreeTests/LivestreamTests.swift
@@ -61,15 +61,15 @@ class LivestreamTests: TestCase {
         Clip(
           id: Clip.ID(rawValue: UUID()),
           blurb: """
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor \
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud \
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor \
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud \
+          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
           """,
           createdAt: Date(),
           description: """
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor \
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud \
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor \
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud \
+          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
           """,
           duration: 100,
           order: 0,
