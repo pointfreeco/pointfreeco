ff51f48035231328a042d1ce44ea3cb2b7ac73f3
Brandon Williams <mbrandonw@hey.com>
2024-06-26 19:36:07 -0400

    fuse inline styles

--- Sources/StyleguideV2/HTML/HTMLInlineStyle.swift
+++ Sources/StyleguideV2/HTML/HTMLInlineStyle.swift
@@ -1,14 +1,23 @@
-struct HTMLInlineStyle<Content: HTML>: HTML {
+public struct HTMLInlineStyle<Content: HTML>: HTML {
   let content: Content
-  let property: String
-  let value: String?
-  let mediaQuery: String?
-  let pseudo: String?
+  var styles: [(String, String?, String?, String?)]
 
-  var body: Never {
+  init(
+    content: Content,
+    property: String,
+    value: String?,
+    mediaQuery: String?,
+    pseudo: String?
+  ) {
+    self.content = content
+    self.styles = [(property, value, mediaQuery, pseudo)]
+  }
+
+  public var body: Never {
     fatalError()
   }
-  static func _render(_ html: HTMLInlineStyle<Content>, into printer: inout HTMLPrinter) {
+
+  public static func _render(_ html: HTMLInlineStyle<Content>, into printer: inout HTMLPrinter) {
 //    let previousClass = printer.attributes["class"]  // TODO: should we optimize this?
 //    defer {
 //      printer.attributes["class"] = previousClass
@@ -17,39 +26,50 @@ struct HTMLInlineStyle<Content: HTML>: HTML {
       Content._render(html.content, into: &printer)
     }
 
-    guard let value = html.value else {
-      return
-    }
+    for (property, value, mediaQuery, pseudo) in html.styles {
+      guard let value = value
+      else {
+        continue
+      }
 
-    // TODO: better hashing/compression (lossless)
-    let className = "\(html.property)-\(value.hashValue)-\(html.mediaQuery?.hashValue ?? 0)"
-    let pseudo = "\(className)\(html.pseudo.map { ":\($0)" } ?? "")"
+      // TODO: better hashing/compression (lossless)
+      let className = "\(property)-\(value.hashValue)-\(mediaQuery?.hashValue ?? 0)"
+      let pseudo = "\(className)\(pseudo.map { ":\($0)" } ?? "")"
 
-    if printer.styles[html.mediaQuery, default: [:]][pseudo] == nil {
-      printer.styles[html.mediaQuery, default: [:]][pseudo] = "\(html.property):\(value)"
+      if printer.styles[mediaQuery, default: [:]][pseudo] == nil {
+        printer.styles[mediaQuery, default: [:]][pseudo] = "\(property):\(value)"
+      }
+      printer.attributes["class", default: ""]!.append("\(className) ")
     }
-    printer.attributes["class", default: ""]!.append("\(className) ")
   }
 }
 
 extension HTML {
-  @HTMLBuilder
   public func inlineStyle(
     _ property: String,
     _ value: String?,
     media mediaQuery: String? = nil,
     pseudo: String? = nil
-  ) -> some HTML {
-    if let value {
-      HTMLInlineStyle(
-        content: self,
-        property: property,
-        value: value,
-        mediaQuery: mediaQuery,
-        pseudo: pseudo
-      )
-    } else {
-      self
-    }
+  ) -> HTMLInlineStyle<Self> {
+    HTMLInlineStyle(
+      content: self,
+      property: property,
+      value: value,
+      mediaQuery: mediaQuery,
+      pseudo: pseudo
+    )
+  }
+}
+
+extension HTMLInlineStyle {
+  public func inlineStyle(
+    _ property: String,
+    _ value: String?,
+    media mediaQuery: String? = nil,
+    pseudo: String? = nil
+  ) -> HTMLInlineStyle {
+    var copy = self
+    copy.styles.append((property, value, mediaQuery, pseudo))
+    return copy
   }
 }
