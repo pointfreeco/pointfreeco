64c723d21160a056a91fb9efa8e18655ecc00172
Brandon Williams <mbrandonw@hey.com>
2024-06-27 10:44:33 -0400

    more work on homepage

--- Sources/Views/HomeV2.swift
+++ Sources/Views/HomeV2.swift
@@ -41,10 +41,26 @@ public struct Home: HTML {
     .inlineStyle("background", "linear-gradient(#121212, #291a40)")
 
     Companies()
-    WhatToExpect()
-    Episodes()
-    Collections()
-    WhatPeopleAreSaying()
+    Module(theme: .offLight) {
+      WhatToExpect()
+    } title: {
+      Header(2) { "What you can expect" }
+    }
+    Module(seeAllRoute: .homeV2, theme: .light) {
+      Episodes()
+    } title: {
+      Header(2) { "What you can expect" }
+    }
+    Module(seeAllRoute: .homeV2, theme: .offLight) {
+      Collections()
+    } title: {
+      Header(2) { "Collections" }
+    }
+    Module(theme: .light) {
+      WhatPeopleAreSaying()
+    } title: {
+      Header(2) { "What people are saying" }
+    }
   }
 }
 
@@ -84,132 +100,133 @@ struct Companies: HTML {
       .column(count: 3, media: .desktop)
       .column(count: 6, media: .mobile)
       .inlineStyle("padding", "1rem")
-      //    sizes: [.mobile: 6, .desktop: 2],
-      //    attributes: [.class([Class.padding([.mobile: [.bottom: 3], .desktop: [.bottom: 0]])])],
-
     }
   }
 }
 
 struct WhatToExpect: HTML {
   var body: some HTML {
-    div {
-      Grid {
-        GridColumn {
-          Header(2) { "What you can expect" }
-            .color(.black)
-            .color(.white, media: .dark)
-        }
-        .column(count: 12)
-        .column(alignment: .center)
-        .inlineStyle("margin", "4rem 0rem")
-
-        for whatToExpect in WhatToExpectItem.all {
-          WhatToExpectColumn(item: whatToExpect)
-        }
-      }
-      .grid(alignment: .start)
-      .inlineStyle("max-width", "1080px")
-      .inlineStyle("margin", "0 auto")
+    for whatToExpect in WhatToExpectItem.all {
+      WhatToExpectColumn(item: whatToExpect)
     }
-    .backgroundColor(.offWhite)
-    .backgroundColor(.offBlack, media: .dark)
   }
-}
 
-struct WhatToExpectColumn: HTML {
-  let item: WhatToExpectItem
-  var body: some HTML {
-    GridColumn {
-      Header(4) {
-        item.title
-      }
-      .color(.black)
-      .color(.white, media: .dark)
-      .inlineStyle("text-align", "center", media: MediaQuery.desktop.rawValue)
+  struct WhatToExpectColumn: HTML {
+    let item: WhatToExpectItem
+    var body: some HTML {
+      GridColumn {
+        Header(4) {
+          item.title
+        }
+        .color(.black)
+        .color(.white, media: .dark)
+        .inlineStyle("text-align", "center", media: MediaQuery.desktop.rawValue)
 
-      Paragraph {
-        item.description
+        Paragraph {
+          item.description
+        }
+        .color(.gray300)
+        .color(.gray850, media: .dark)
+        .inlineStyle("text-align", "center", media: MediaQuery.desktop.rawValue)
       }
-      .color(.gray300)
-      .color(.gray850, media: .dark)
-      .inlineStyle("text-align", "center", media: MediaQuery.desktop.rawValue)
+      .column(count: 12, media: .mobile)
+      .column(count: 6, media: .desktop)
+      .inlineStyle("padding", "0rem 1rem 4rem 2rem", media: MediaQuery.desktop.rawValue, pseudo: "nth-child(even)")
+      .inlineStyle("padding", "0rem 2rem 4rem 1rem", media: MediaQuery.desktop.rawValue, pseudo: "nth-child(odd)")
+      .inlineStyle("padding", "0rem 2rem 3rem 2rem", media: MediaQuery.mobile.rawValue)
     }
-    .column(count: 12, media: .mobile)
-    .column(count: 6, media: .desktop)
-    .inlineStyle("padding", "0rem 1rem 4rem 2rem", media: MediaQuery.desktop.rawValue, pseudo: "nth-child(even)")
-    .inlineStyle("padding", "0rem 2rem 4rem 1rem", media: MediaQuery.desktop.rawValue, pseudo: "nth-child(odd)")
-    .inlineStyle("padding", "0rem 2rem 3rem 2rem", media: MediaQuery.mobile.rawValue)
   }
 }
 
 struct Episodes: HTML {
   var body: some HTML {
-    div {
-      Grid {
-        GridColumn {
-          Header(2) { "Episodes" }
-            .color(.black)
-            .color(.white, media: .dark)
-        }
-        .column(count: 12)
-        .column(alignment: .center)
-        .inlineStyle("margin", "4rem 0rem")
-
-
-      }
-      .grid(alignment: .start)
-      .inlineStyle("max-width", "1080px")
-      .inlineStyle("margin", "0 auto")
-    }
-    .backgroundColor(.white)
-    .backgroundColor(.black, media: .dark)
+    "Episodes"
   }
 }
 
 struct Collections: HTML {
   var body: some HTML {
-    div {
-      Grid {
-        GridColumn {
-          Header(2) { "Collections" }
-            .color(.black)
-            .color(.white, media: .dark)
-        }
-        .column(count: 12)
-        .column(alignment: .center)
-        .inlineStyle("margin", "4rem 0rem")
-
-
-      }
-      .grid(alignment: .start)
-      .inlineStyle("max-width", "1080px")
-      .inlineStyle("margin", "0 auto")
-    }
-    .backgroundColor(.offWhite)
-    .backgroundColor(.offBlack, media: .dark)
+    "Collections"
   }
 }
 
 struct WhatPeopleAreSaying: HTML {
   var body: some HTML {
+    "Testimonials"
+  }
+}
+
+private struct Module<Title: HTML, Content: HTML>: HTML {
+  let title: Title
+  var seeAllRoute: SiteRoute?
+  var theme: Theme
+  let content: Content
+  init(
+    seeAllRoute: SiteRoute? = nil,
+    theme: Theme,
+    @HTMLBuilder content: () -> Content,
+    @HTMLBuilder title: () -> Title
+  ) {
+    self.title = title()
+    self.seeAllRoute = seeAllRoute
+    self.theme = theme
+    self.content = content()
+  }
+
+  enum Theme {
+    case dark
+    case light
+    case offLight
+    var backgroundColor: PointFreeColor {
+      switch self {
+      case .dark: .black
+      case .light: .white
+      case .offLight: .offWhite
+      }
+    }
+    var darkModeBackgroundColor: PointFreeColor {
+      switch self {
+      case .dark: .black
+      case .light: .black
+      case .offLight: .offBlack
+      }
+    }
+    var color: PointFreeColor {
+      switch self {
+      case .dark: .purple
+      case .light: .black
+      case .offLight: .black
+      }
+    }
+    var darkModeColor: PointFreeColor {
+      switch self {
+      case .dark: .purple
+      case .light: .white
+      case .offLight: .white
+      }
+    }
+  }
+
+  var body: some HTML {
     div {
       Grid {
         GridColumn {
-          Header(2) { "What people are saying" }
-            .color(.black)
-            .color(.white, media: .dark)
+          title
+            .color(theme.color)
+            .color(theme.darkModeColor, media: .dark)
         }
         .column(count: 12)
         .column(alignment: .center)
         .inlineStyle("margin", "4rem 0rem")
+
+        content
       }
       .grid(alignment: .start)
       .inlineStyle("max-width", "1080px")
       .inlineStyle("margin", "0 auto")
     }
-    .backgroundColor(.white)
-    .backgroundColor(.black, media: .dark)
+    .backgroundColor(theme.backgroundColor)
+    .backgroundColor(theme.darkModeBackgroundColor, media: .dark)
   }
 }
 
