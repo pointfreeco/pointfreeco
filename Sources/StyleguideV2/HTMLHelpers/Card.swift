c8f258eaf97bcb2d6cd6ac49e06f79afe97075a0
Stephen Celis <stephen@stephencelis.com>
2024-06-28 16:09:05 -0700

    fix

--- Sources/StyleguideV2/HTMLHelpers/Card.swift
+++ Sources/StyleguideV2/HTMLHelpers/Card.swift
@@ -20,7 +20,10 @@ public struct Card<Content: HTML, Header: HTML, Footer: HTML>: HTML {
           .inlineStyle("border-bottom", "1px solid #e8e8e8")
           .inlineStyle("border-bottom", "1px solid #3d3d3d", media: .dark)
         div {
-          content
+          div {
+            content
+          }
+          .inlineStyle("flex-grow", "1")
 
           Grid {
             footer
@@ -28,17 +31,23 @@ public struct Card<Content: HTML, Header: HTML, Footer: HTML>: HTML {
           .color(.gray650.dark(.gray400))
           .grid(alignment: .center)
         }
+        .inlineStyle("display", "flex")
+        .inlineStyle("flex-direction", "column")
+        .inlineStyle("flex-grow", "1")
         .inlineStyle("padding", "0.5rem 1.5rem 1.5rem 1.5rem")
       }
       .backgroundColor(.white.dark(.gray150))
       .inlineStyle("border", "1px #353535 solid", media: .dark)
       .inlineStyle("box-shadow", "0 2px 10px -2px rgba(0,0,0,0.3)")
       .inlineStyle("border-radius", "5px")
+      .inlineStyle("display", "flex")
+      .inlineStyle("flex-direction", "column")
       .inlineStyle("margin", "1rem 0 2rem 0")
       .inlineStyle("overflow", "hidden")
     }
     .column(count: 12)
     .column(count: 4, media: .desktop)
+    .inlineStyle("display", "flex")
     .inlineStyle("padding-left", "0.75rem")
     .inlineStyle("padding-right", "0.75rem")
   }
