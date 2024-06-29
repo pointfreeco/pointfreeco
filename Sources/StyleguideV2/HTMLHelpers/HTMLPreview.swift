1f9d8b8da974f9767abab2e94c2a4e37737f742d
Stephen Celis <stephen@stephencelis.com>
2024-06-28 18:15:00 -0700

    wip

--- Sources/StyleguideV2/HTMLHelpers/HTMLPreview.swift
+++ Sources/StyleguideV2/HTMLHelpers/HTMLPreview.swift
@@ -22,25 +22,4 @@
       webView.loadHTMLString(String(decoding: printer.bytes, as: UTF8.self), baseURL: nil)
     }
   }
-
-  import Html
-
-  public struct NodePreview: NSViewRepresentable {
-    let content: Node
-
-    public init(@NodeBuilder content: () -> Node) {
-      self.content = content()
-    }
-
-    public func makeNSView(context: Context) -> WKWebView {
-      WKWebView(
-        frame: NSRect(x: 0, y: 0, width: 640, height: 480),
-        configuration: WKWebViewConfiguration()
-      )
-    }
-
-    public func updateNSView(_ webView: WKWebView, context: Context) {
-      webView.loadHTMLString(render(content), baseURL: nil)
-    }
-  }
 #endif
