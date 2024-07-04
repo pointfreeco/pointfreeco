8e400769eaa62930a41b3120634618a58494eeb3
Stephen Celis <stephen@stephencelis.com>
2024-07-04 09:00:00 -0700

    wip

--- Sources/Views/PricingLanding.swift
+++ Sources/Views/PricingLanding.swift
@@ -34,106 +34,106 @@ public struct PricingLanding: HTML {
     let stats = EpisodesStats()
 
     PageModule(theme: .content) {
-      VStack(alignment: .center) {
-        LazyVGrid(columns: [.desktop: [1, 1, 1, 1]]) {
-          Lane("Free", annualPricePerMonth: 0) {
-            li { "Weekly newsletter access" }
-            li { "\(stats.freeEpisodes) free episodes with transcripts" }
-            li { "1 free credit to redeem any subscriber-only episode" }
-            li { "Download all episode code samples" }
-          } callToAction: {
-            if currentUser == nil {
-              Button(color: .purple) {
-                "Choose plan"
-              }
-              .attribute("href", siteRouter.path(for: .signUp(redirect: siteRouter.url(for: currentRoute))))
-            } else if subscriberState.isNonSubscriber {
-              Button(color: .purple, style: .outline) {
-                "Your plan"
-              }
-              .inlineStyle("cursor", "not-allowed")
+      LazyVGrid(columns: [.desktop: [1, 1, 1, 1]]) {
+        Lane("Free", annualPricePerMonth: 0) {
+          li { "Weekly newsletter access" }
+          li { "\(stats.freeEpisodes) free episodes with transcripts" }
+          li { "1 free credit to redeem any subscriber-only episode" }
+          li { "Download all episode code samples" }
+        } callToAction: {
+          if currentUser == nil {
+            Button(color: .purple) {
+              "Choose plan"
             }
-          }
-          Lane("Individual", annualPricePerMonth: 14) {
-            "per month,"
-            br()
-            "billed annually"
-          } features: {
-            li { "All \(stats.allEpisodes) episodes with transcripts" }
-            li { "Over \(stats.allHours) hours of video" }
-            li {
-              "Watch past "
-              Link("livestreams", href: siteRouter.path(for: .live(.current)))
+            .attribute("href", siteRouter.path(for: .signUp(redirect: siteRouter.url(for: currentRoute))))
+          } else if subscriberState.isNonSubscriber {
+            Button(color: .purple, style: .outline) {
+              "Your plan"
             }
-            li { "Personal podcast feed for offline viewing" }
-            li { "Download all episode code samples" }
-            li {
-              Link(
-                "Regional",
-                href: siteRouter.path(
-                  for: .subscribeConfirmation(
-                    lane: .personal,
-                    referralCode: nil, // TODO?
-                    useRegionalDiscount: true
-                  )
-                )
-              )
-              " and "
-              Link(
-                "educational",
-                href: siteRouter.path(for: .blog(.show(slug: post0010_studentDiscounts.slug)))
-              )
-              " discounts available"
-            }
-          } callToAction: {
-            if subscriberState.isNonSubscriber {
-              Button(color: .purple) {
-                "Choose plan"
-              }
-              .attribute("href", siteRouter.path(for: .subscribeConfirmation(lane: .personal)))
-            }
-          }
-          Lane("Team", annualPricePerMonth: 12) {
-            "per member, per month,"
-            br()
-            "billed annually"
-          } features: {
-            li { "All personal plan features" }
-            li { "For teams of 2 or more" }
-            li { "Add teammates at any time with prorated billing" }
-            li { "Reassign team mates at any time" }
-          } callToAction: {
-            // TODO: Would be nice to upsell here for personal subscriptions
-            if subscriberState.isNonSubscriber {
-              Button(color: .purple) {
-                "Choose plan"
-              }
-              .attribute("href", siteRouter.path(for: .subscribeConfirmation(lane: .team)))
-            }
-          }
-          Lane("Enterprise") {
-            li { "For large teams" }
-            li { "Unlimited, company-wide access to all content" }
-            li { "Hassle-free team management" }
-            li { "Custom sign up landing page for your company" }
-            li { "Invoiced billing" }
-          } callToAction: {
-            if !subscriberState.isEnterpriseSubscriber {
-              Button(color: .black, style: .outline) {
-                "Contact us"
-              }
-              .attribute("href", "mailto:support@pointfree.co")
-              .linkColor(.black.dark(.white))
-            } else {
-              Button(color: .black, style: .outline) {
-                "Your plan"
-              }
-              .inlineStyle("cursor", "not-allowed")
-            }
+            .inlineStyle("cursor", "not-allowed")
           }
         }
-        .linkUnderline(true)
+        Lane("Individual", annualPricePerMonth: 14) {
+          "per month,"
+          br()
+          "billed annually"
+        } features: {
+          li { "All \(stats.allEpisodes) episodes with transcripts" }
+          li { "Over \(stats.allHours) hours of video" }
+          li {
+            "Watch past "
+            Link("livestreams", href: siteRouter.path(for: .live(.current)))
+          }
+          li { "Personal podcast feed for offline viewing" }
+          li { "Download all episode code samples" }
+          li {
+            Link(
+              "Regional",
+              href: siteRouter.path(
+                for: .subscribeConfirmation(
+                  lane: .personal,
+                  referralCode: nil, // TODO?
+                  useRegionalDiscount: true
+                )
+              )
+            )
+            " and "
+            Link(
+              "educational",
+              href: siteRouter.path(for: .blog(.show(slug: post0010_studentDiscounts.slug)))
+            )
+            " discounts available"
+          }
+        } callToAction: {
+          if subscriberState.isNonSubscriber {
+            Button(color: .purple) {
+              "Choose plan"
+            }
+            .attribute("href", siteRouter.path(for: .subscribeConfirmation(lane: .personal)))
+          }
+        }
+        Lane("Team", annualPricePerMonth: 12) {
+          "per member, per month,"
+          br()
+          "billed annually"
+        } features: {
+          li { "All personal plan features" }
+          li { "For teams of 2 or more" }
+          li { "Add teammates at any time with prorated billing" }
+          li { "Reassign team mates at any time" }
+        } callToAction: {
+          // TODO: Would be nice to upsell here for personal subscriptions
+          if subscriberState.isNonSubscriber {
+            Button(color: .purple) {
+              "Choose plan"
+            }
+            .attribute("href", siteRouter.path(for: .subscribeConfirmation(lane: .team)))
+          }
+        }
+        Lane("Enterprise") {
+          li { "For large teams" }
+          li { "Unlimited, company-wide access to all content" }
+          li { "Hassle-free team management" }
+          li { "Custom sign up landing page for your company" }
+          li { "Invoiced billing" }
+        } callToAction: {
+          if !subscriberState.isEnterpriseSubscriber {
+            Button(color: .black, style: .outline) {
+              "Contact us"
+            }
+            .attribute("href", "mailto:support@pointfree.co")
+            .linkColor(.black.dark(.white))
+          } else {
+            Button(color: .black, style: .outline) {
+              "Your plan"
+            }
+            .inlineStyle("cursor", "not-allowed")
+          }
+        }
+      }
+      .linkUnderline(true)
 
+      VStack(alignment: .center) {
         small {
           "Prices shown with annual billing. When billed month to month, the "
           br()
