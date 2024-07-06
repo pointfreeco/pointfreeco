bc388b69422e636cf1ebcd96a2fbbc1a0fe73465
Brandon Williams <135203+mbrandonw@users.noreply.github.com>
2024-07-06 15:51:56 -0400

    Update to clips pages to new styleguide (#924)
    * Update Clips to new styleguide.
    
    * wip
    
    * wip

--- Sources/Views/ClipsViews.swift
+++ Sources/Views/ClipsViews.swift
@@ -10,358 +10,149 @@ import PointFreePrelude
 import PointFreeRouter
 import Prelude
 import Styleguide
+import StyleguideV2
 
-public func clipView(clip: Models.Clip) -> Node {
-  @Dependency(\.subscriberState) var subscriberState
+public struct ClipsIndex: HTML {
+  let clips: [Models.Clip]
+  public init(clips: [Models.Clip]) {
+    self.clips = clips
+  }
 
-  return [
-    .div(
-      attributes: [
-        .class([
-          Class.pf.colors.bg.black,
-          Class.border.top,
-        ]),
-        .style(key("border-top-color", "#000")),
-      ],
-      .gridRow(
-        attributes: [
-          .class([
-            Class.grid.middle(.desktop),
-            Class.padding([
-              .desktop: [.leftRight: 5],
-              .mobile: [.leftRight: 3, .top: 4, .bottom: 5],
-            ]),
-          ]),
-          .style(maxWidth(.px(1080)) <> margin(topBottom: nil, leftRight: .auto)),
-        ],
-        .gridColumn(
-          sizes: [.mobile: 12],
-          attributes: [],
-          .h1(
-            attributes: [
-              .class([
-                Class.pf.colors.fg.white,
-                Class.pf.type.responsiveTitle2,
-                Class.type.align.center,
-              ]),
-              .style(lineHeight(1.2)),
-            ],
-            .raw(nonBreaking(title: clip.title))
-          ),
-          .div(
-            attributes: [
-              .class([
-                Class.padding([.mobile: [.bottom: 2]]),
-                Class.pf.colors.fg.gray650,
-                Class.pf.type.body.small,
-                Class.type.align.center,
-              ])
-            ],
-            .text(
-              """
-              Episode Clip • Free • \(headerDateFormatter.string(from: clip.createdAt))
-              """)
-          ),
-          .div(
-            attributes: [
-              .class([
-                Class.padding([.mobile: [.top: 1, .leftRight: 0], .desktop: [.leftRight: 4]]),
-                Class.pf.colors.fg.gray850,
-                Class.pf.type.body.regular,
-              ])
-            ],
-            .markdownBlock(clip.description)
-          )
-        )
-      )
-    ),
-
-    .div(
-      attributes: [
-        .class([
-          Class.border.top
-        ]),
-        .style(key("border-top-color", "#333")),
-      ],
-      .gridRow(
-        attributes: [
-          .class([
-            Class.grid.middle(.desktop),
-            Class.padding([
-              .desktop: [.leftRight: 3, .bottom: 4],
-              .mobile: [.leftRight: 1, .bottom: 4],
-            ]),
-          ]),
-          .style(
-            maxWidth(.px(1080))
-              <> margin(topBottom: nil, leftRight: .auto)
-              <> margin(top: .rem(-4))
-          ),
-        ],
-        .gridColumn(
-          sizes: [.mobile: 12],
-          attributes: [
-            .class([
-              Class.margin([.mobile: [.bottom: 4]])
-            ]),
-            .style(
-              boxShadow(
-                hShadow: .rem(0),
-                vShadow: .rem(1),
-                blurRadius: .px(20),
-                color: .rgba(0, 0, 0, 0.2)
-              )
-            ),
-          ],
-          videoView(videoID: clip.vimeoVideoID)
-        ),
-
-        subscriberCalloutView
-      )
-    ),
-  ]
-}
-
-var subscriberCalloutView: Node {
-  @Dependency(\.siteRouter) var siteRouter
-  @Dependency(\.subscriberState) var subscriberState
-
-  guard subscriberState.isNonSubscriber else { return [] }
-
-  return [
-    divider,
-    .gridRow(
-      .gridColumn(
-        sizes: [.desktop: 9, .mobile: 12],
-        attributes: [.style(margin(leftRight: .auto))],
-        .div(
-          attributes: [
-            .class(
-              [
-                Class.margin([.mobile: [.all: 4]]),
-                Class.padding([.mobile: [.all: 3]]),
-                Class.pf.colors.bg.gray900,
-              ]
-            )
-          ],
-          .h4(
-            attributes: [
-              .class(
-                [
-                  Class.pf.type.responsiveTitle4,
-                  Class.padding([.mobile: [.bottom: 2]]),
-                ]
-              )
-            ],
-            "Subscribe to Point-Free"
-          ),
-          .p(
-            "👋 Hey there! See anything you like? You may be interested in ",
-            .a(
-              attributes: [
-                .href(siteRouter.path(for: .pricingLanding)),
-                .class([Class.pf.type.underlineLink]),
-              ],
-              "subscribing"
-            ),
-            " so that you get access to these episodes and all future ones."
-          )
-        )
-      )
-    ),
-  ]
-}
-
-public func clipsView(clips: [Models.Clip]) -> Node {
-  @Dependency(\.siteRouter) var siteRouter
-
-  return [
-    .gridRow(
-      attributes: [
-        .class([
-          Class.padding([.mobile: [.top: 3], .desktop: [.top: 4]]),
-          Class.grid.between(.desktop),
-        ]),
-        .style(
-          maxWidth(.px(1080))
-            <> margin(leftRight: .auto)
-        ),
-      ],
-      .gridColumn(
-        sizes: [.mobile: 12],
-        attributes: [
-          .class([
-            Class.padding([.mobile: [.bottom: 3, .leftRight: 2], .desktop: [.leftRight: 3]])
-          ])
-        ],
-        .h3(
-          attributes: [
-            .class([Class.pf.type.responsiveTitle3])
-          ],
-          "Point-Free clips"
-        ),
-        .p(
-          attributes: [],
-          .text(
-            """
-            A collection of some of our favorite moments from Point-Free episodes.
-            """)
-        )
-      )
-    ),
-    .ul(
-      attributes: [
-        .class([
-          Class.margin([.mobile: [.all: 0]]),
-          Class.padding([.mobile: [.all: 0], .desktop: [.leftRight: 2, .topBottom: 0]]),
-          Class.type.list.styleNone,
-          Class.flex.wrap,
-          Class.flex.flex,
-        ]),
-        .style(maxWidth(.px(1080)) <> margin(topBottom: nil, leftRight: .auto)),
-      ],
-      .fragment(
-        clips.enumerated().map { index, clip in
-          cardView(
-            title: clip.title,
-            subtitle: nil,
-            blurb: clip.blurb,
-            url: siteRouter.path(for: .clips(.clip(videoID: clip.vimeoVideoID))),
-            imageURL: clip.posterURL,
-            index: index
-          )
+  public var body: some HTML {
+    PageHeader(title: "Point-Free Clips") {
+      """
+      A collection of some of our favorite moments from Point-Free episodes.
+      """
+    }
+    PageModule(theme: .content) {
+      LazyVGrid(columns: [1, 1, 1]) {
+        HTMLForEach(clips) { clip in
+          ClipCard(clip)
         }
-      )
-    ),
-  ]
+      }
+    }
+  }
 }
 
-private func videoView(
-  videoID: VimeoVideo.ID
-) -> Node {
-  return .div(
-    attributes: [
-      .class([outerVideoContainerClass]),
-      .style(outerVideoContainerStyle),
-    ],
-    .iframe(
-      attributes: [
-        .class([innerVideoContainerClass]),
-        .src("https://player.vimeo.com/video/\(videoID)?pip=1"),
-        Attribute("frameborder", "0"),
-        Attribute("allow", "autoplay; fullscreen"),
-        Attribute("allowfullscreen", ""),
-      ]
-    ),
-    .script(attributes: [.async(true), .src("https://player.vimeo.com/api/player.js")])
-  )
+public struct ClipView: HTML {
+  let clip: Models.Clip
+  public init(clip: Models.Clip) {
+    self.clip = clip
+  }
+
+  public var body: some HTML {
+    CenterColumn {
+      VStack(alignment: .center) {
+        Header(3) {
+          HTMLText(clip.title)
+        }
+        .color(.offWhite)
+        .inlineStyle("text-align", "center")
+
+        span {
+          """
+          Episode Clip • \(headerDateFormatter.string(from: clip.createdAt))
+          """
+        }
+        .color(.gray800)
+
+        HTMLMarkdown(clip.description)
+          .color(.gray900)
+          .inlineStyle("max-width", "768px")
+      }
+      .inlineStyle("padding", "6rem 2rem")
+      .inlineStyle("padding", "8rem 3rem", media: .desktop)
+    }
+    .inlineStyle("background", "linear-gradient(#121212, #242424)")
+    .inlineStyle("padding-bottom", "2rem")
+
+    CenterColumn {
+      div {
+        div {
+          div {
+            iframe()
+              .attribute("src", "https://player.vimeo.com/video/\(clip.vimeoVideoID)?pip=1")
+              .attribute("frameborder", "0")
+              .attribute("allow", "autoplay; fullscreen")
+              .attribute("allowfullscreen")
+              .inlineStyle("width", "100%")
+              .inlineStyle("height", "100%")
+              .inlineStyle("position", "absolute")
+            script()
+              .attribute("src", "https://player.vimeo.com/api/player.js")
+          }
+          .inlineStyle("width", "100%")
+          .inlineStyle("position", "relative")
+          .inlineStyle("padding-bottom", "56.25%")
+        }
+        .inlineStyle("box-shadow", "0rem 1rem 20px rgba(0,0,0,0.2)")
+        .inlineStyle("margin", "0 4rem", media: .desktop)
+      }
+    }
+    .inlineStyle("margin-top", "-4rem")
+    .inlineStyle("padding-bottom", "4rem")
+  }
 }
 
-private func cardView(
-  title: String,
-  subtitle: String? = nil,
-  blurb: String,
-  url: String,
-  imageURL: String,
-  index: Int
-) -> ChildOf<Tag.Ul> {
-  @Dependency(\.siteRouter) var siteRouter
+struct CenterColumn<Content: HTML>: HTML {
+  @HTMLBuilder let content: Content
+  public var body: some HTML {
+    tag("pf-center-column") {
+      content
+        .inlineStyle("max-width", "1280px")
+        .inlineStyle("width", "100%")
+        .inlineStyle("margin", "0 auto")
+    }
+    .inlineStyle("width", "100%")
+    .inlineStyle("box-sizing", "border-box")
+    .inlineStyle("display", "block")
+  }
+}
 
-  return .li(
-    attributes: [
-      .class([
-        Class.padding([
-          .mobile: [.top: 2, .bottom: 3, .leftRight: 2],
-          .desktop: [.top: 0, .bottom: 4],
-        ]),
-        Class.margin([.mobile: [.all: 0]]),
-        Class.flex.flex,
-        cardClass,
-      ])
-    ],
-    .div(
-      attributes: [
-        .class([
-          Class.flex.column,
-          Class.size.width100pct,
-          Class.flex.flex,
-          Class.border.rounded.all,
-          Class.layout.overflowHidden,
-          Class.border.all,
-          Class.pf.colors.border.gray850,
-        ])
-      ],
-      .a(
-        attributes: [
-          .class([
-            Class.flex.flex,
-            Class.flex.justify.center,
-            Class.flex.align.center,
-          ]),
-          .href(url),
-        ],
-        .img(
-          src: imageURL,
-          alt: "",
-          attributes: [
-            .init("width", "100%")
-          ]
-        )
-      ),
-      .div(
-        attributes: [
-          .class([
-            Class.padding([.mobile: [.top: 3, .leftRight: 3, .bottom: 2]])
-          ])
-        ],
-        .h6(
-          attributes: [
-            .class([
-              Class.pf.colors.fg.gray400,
-              Class.type.normal,
-              Class.pf.type.responsiveTitle8,
-              Class.margin([.mobile: [.all: 0]]),
-            ])
-          ],
-          subtitle.map { .text($0) } ?? []
-        ),
-        .h4(
-          attributes: [
-            .class([
-              Class.pf.type.responsiveTitle4,
-              Class.type.normal,
-              Class.margin([.mobile: [.top: 0]]),
-            ])
-          ],
-          .a(
-            attributes: [
-              .href(url)
-            ],
-            .text(title)
-          )
-        ),
-        .p(
-          attributes: [
-            .class([
-              Class.padding([.mobile: [.all: 0]]),
-              Class.pf.type.body.regular,
-              Class.pf.colors.fg.black,
-            ]),
-            .style(flex(grow: 1, shrink: 0, basis: .auto)),
-          ],
-          .div(.markdownBlock(blurb))
-        )
+#if DEBUG && canImport(SwiftUI)
+import SwiftUI
+import Transcripts
+
+#Preview("Clips Index", traits: .fixedLayout(width: 800, height: 1000)) {
+  HTMLPreview {
+    PageLayout(layoutData: SimplePageLayoutData(title: "")) {
+      ClipsIndex(
+        clips: [
+          .mock,
+          .mock,
+          .mock,
+          .mock,
+        ]
       )
+    }
+  }
+}
+
+#Preview("Clip Show", traits: .fixedLayout(width: 800, height: 1000)) {
+  HTMLPreview {
+    PageLayout(layoutData: SimplePageLayoutData(title: "")) {
+      ClipView(clip: .mock)
+    }
+  }
+}
+
+extension Models.Clip {
+  fileprivate static var mock: Self {
+    Models.Clip(
+      id: Clip.ID(UUID()),
+      blurb: """
+        We often need to perform async work when there is no async context, such as in SwiftUI button action closures. In such cases it seems that you have no choice but to spin up an unstructured Task, but you may have heard that doing so it bad. So what are you to do? Well, there is an easy answer…
+        """,
+      createdAt: Date(),
+      description: """
+        We often need to perform async work when there is no async context, such as in SwiftUI button action closures. In such cases it seems that you have no choice but to spin up an unstructured Task, but you may have heard that doing so it bad. So what are you to do? Well, there is an easy answer…
+        """,
+      duration: 300,
+      order: 1,
+      posterURL: "https://i.vimeocdn.com/video/1864209432-b580f900f7a12b935e0e8c7028c124b5d15c8f80efb688445312650c9b973910-d",
+      title: "How should you perform async work in a non-async context?",
+      vimeoVideoID: 790482468
     )
-  )
+  }
 }
-
-public let cardStyles: Stylesheet =
-  Breakpoint.mobile.query(only: screen) {
-    cardClass % width(.pct(100))
-  }
-  <> Breakpoint.desktop.query(only: screen) {
-    cardClass % width(.pct(50))
-  }
-
-private let cardClass = CssSelector.class("card")
+#endif
