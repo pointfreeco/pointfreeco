7059e4d2a43ccd23f5fb6f44a9b0a083c8c5c9da
Stephen Celis <stephen@stephencelis.com>
2024-07-06 09:55:53 -0700

    wip

--- Sources/PointFree/Blog/BlogPostShow.swift
+++ Sources/PointFree/Blog/BlogPostShow.swift
@@ -37,38 +37,6 @@ func newsletterDetail(
     }
 }
 
-let blogPostShowMiddleware =
-  fetchBlogPostForParam
-  <| writeStatus(.ok)
-  >=> respond(
-    view: blogPostShowView,
-    layoutData: { post in
-      @Dependency(\.assets) var assets
-
-      return SimplePageLayoutData(
-        data: post,
-        description: post.blurb,
-        image: post.coverImage ?? assets.emailHeaderImgSrc,
-        openGraphType: .website,
-        style: .base(.mountains(.blog)),
-        title: post.title,
-        twitterCard: .summaryLargeImage,
-        usePrismJs: true
-      )
-    }
-  )
-
-private func fetchBlogPostForParam(
-  _ middleware: @escaping Middleware<StatusLineOpen, ResponseEnded, BlogPost, Data>
-) -> Middleware<StatusLineOpen, ResponseEnded, Either<String, BlogPost.ID>, Data> {
-  return { conn in
-    guard let post = fetchBlogPost(forParam: conn.data)
-    else { return conn |> redirect(to: .home) }
-
-    return middleware(conn.map(const(post)))
-  }
-}
-
 func fetchBlogPost(forParam param: Either<String, BlogPost.ID>) -> BlogPost? {
   @Dependency(\.blogPosts) var blogPosts
 
--- Sources/Views/Blog/BlogPostShow.swift
+++ Sources/Views/Blog/BlogPostShow.swift
@@ -1,74 +1,35 @@
-import Css
 import Dependencies
-import Foundation
-import FunctionalCss
-import Html
-import HtmlCssSupport
 import Models
-import PointFreeRouter
-import Prelude
-import Styleguide
 import StyleguideV2
 
-public func blogPostShowView(post: BlogPost) -> Node {
-  @Dependency(\.date.now) var now
-  @Dependency(\.subscriberState) var subscriberState
+public struct NewsletterDetail: HTML {
+  @Dependency(\.currentUser) var currentUser
 
-  return [
-    .gridRow(
-      attributes: [.class([Class.padding([.mobile: [.leftRight: 3], .desktop: [.leftRight: 4]])])],
-      [
-        .gridColumn(
-          sizes: [.mobile: 12, .desktop: 9],
-          attributes: [.style(margin(leftRight: .auto))],
-          .div(
-            attributes: [
-              .class([Class.padding([.mobile: [.topBottom: 3], .desktop: [.topBottom: 4]])])
-            ],
-            blogPostContentView(post),
-            postSubscriberCalloutView
-          )
-        )
-      ]
-    )
-  ]
+  var blogPost: BlogPost
+  // TODO: Link to previous/next posts
+  var previousBlogPost: BlogPost?
+  var nextBlogPost: BlogPost?
+
+  public init(blogPost: BlogPost) {
+    self.blogPost = blogPost
+  }
+
+  public var body: some HTML {
+    NewsletterDetailModule(blogPost: blogPost)
+
+    if currentUser == nil {
+      GetStartedModule(style: .gradient)
+    }
+  }
 }
 
-public func blogPostContentView(_ post: BlogPost) -> Node {
-  @Dependency(\.siteRouter) var siteRouter
+struct NewsletterDetailModule: HTML {
+  var blogPost: BlogPost
 
-  return [
-    .h1(
-      attributes: [.class([Class.pf.type.responsiveTitle3])],
-      .a(
-        attributes: [.href(siteRouter.url(for: .blog(.show(slug: post.slug))))],
-        .text(post.title)
-      )
-    ),
-
-    .div(
-      attributes: [
-        .class([Class.flex.flex, Class.flex.items.baseline]),
-        .style(flex(direction: .row)),
-      ],
-      .div(.p(.text(episodeDateFormatter.string(from: post.publishedAt))))
-    ),
-
-    .div(
-      attributes: [
-        .style(width(.rem(3)) <> height(.px(2))),
-        .class(
-          [
-            Class.pf.colors.bg.green,
-            Class.margin([.mobile: [.bottom: 3]]),
-          ]
-        ),
-      ]
-    ),
-
-    Node {
+  var body: some HTML {
+    PageModule(theme: .content) {
       VStack(spacing: 3) {
-        if let content = post.content {
+        if let content = blogPost.content {
           article {
             HTMLMarkdown(content)
               .color(.gray150.dark(.gray800))
@@ -78,55 +39,34 @@ public func blogPostContentView(_ post: BlogPost) -> Node {
         }
       }
       .inlineStyle("margin", "0 auto", media: .desktop)
+      .inlineStyle("width", "60%", media: .desktop)
+    } title: {
+      VStack {
+        Header(3) {
+          HTMLMarkdown(blogPost.title)
+        }
+
+        div {
+          HTMLText(blogPost.publishedAt.weekdayMonthDayYear())
+        }
+        .color(.gray500)
+      }
+      .inlineStyle("text-align", "center")
+      .inlineStyle("width", "100%")
     }
-  ]
+  }
 }
 
-private var postSubscriberCalloutView: Node {
-  @Dependency(\.subscriberState) var subscriberState
-  guard !subscriberState.isActive else { return [] }
+#if canImport(SwiftUI)
+  import SwiftUI
+  import Transcripts
 
-  @Dependency(\.siteRouter) var siteRouter
-
-  return [
-    .hr(attributes: [
-      .class([Class.pf.components.divider, Class.margin([.mobile: [.topBottom: 4]])])
-    ]),
-
-    .div(
-      attributes: [
-        .class(
-          [
-            Class.margin([.mobile: [.leftRight: 3]]),
-            Class.padding([.mobile: [.all: 3]]),
-            Class.pf.colors.bg.gray900,
-          ]
-        )
-      ],
-      .h4(
-        attributes: [
-          .class(
-            [
-              Class.pf.type.responsiveTitle4,
-              Class.padding([.mobile: [.bottom: 2]]),
-            ]
-          )
-        ],
-        "Subscribe to Point-Free"
-      ),
-      .markdownBlock(
-        """
-        👋 Hey there! If you got this far, then you must have enjoyed this post. You may want to
-        also check out [Point-Free](/), a video series covering advanced programming topics in
-        Swift. Consider [subscribing](/pricing) today!
-        """)
-    ),
-  ]
-}
-
-let episodeDateFormatter: DateFormatter = {
-  let df = DateFormatter()
-  df.dateFormat = "EEEE MMM d, yyyy"
-  df.timeZone = TimeZone(secondsFromGMT: 0)
-  return df
-}()
+  #Preview {
+    HTMLPreview {
+      PageLayout(layoutData: SimplePageLayoutData(title: "")) {
+        NewsletterDetail(blogPost: post0129_Perception)
+      }
+    }
+    .frame(width: 1280)
+  }
+#endif
