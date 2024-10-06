b5ca237ea0fdeb8b0150f14ec3f6f35b66183b70
Brandon Williams <mbrandonw@hey.com>
2024-10-06 12:36:21 -0400

    tests

--- Tests/PointFreeTests/AuthTests.swift
+++ Tests/PointFreeTests/AuthTests.swift
@@ -1,5 +1,6 @@
 import Dependencies
 import Either
+import InlineSnapshotTesting
 import HttpPipeline
 import PointFreePrelude
 import PointFreeRouter
@@ -36,7 +37,8 @@ class AuthIntegrationTests: LiveDatabaseTestCase {
     } operation: {
       let result = await siteMiddleware(
         connection(
-          from: request(to: .auth(.gitHubCallback(code: "deabeef", redirect: "/")), session: .loggedOut)
+          from: request(
+            to: .auth(.gitHubCallback(code: "deabeef", redirect: "/")), session: .loggedOut)
         )
       )
       await assertSnapshot(matching: result, as: .conn)
@@ -68,7 +70,8 @@ class AuthIntegrationTests: LiveDatabaseTestCase {
     } operation: {
       let result = await siteMiddleware(
         connection(
-          from: request(to: .auth(.gitHubCallback(code: "deabeef", redirect: "/")), session: .loggedOut)
+          from: request(
+            to: .auth(.gitHubCallback(code: "deabeef", redirect: "/")), session: .loggedOut)
         )
       )
       await assertSnapshot(matching: result, as: .conn)
@@ -107,7 +110,6 @@ class AuthIntegrationTests: LiveDatabaseTestCase {
 
     await assertSnapshot(matching: await siteMiddleware(conn), as: .conn)
   }
-
 
   @MainActor
   func testRegisterWithDuplicateEmail() async throws {
@@ -213,6 +215,79 @@ class AuthTests: TestCase {
       let auth = request(to: .auth(.gitHubCallback(code: "deadbeef", redirect: nil)))
       let conn = connection(from: auth)
       await assertSnapshot(matching: await siteMiddleware(conn), as: .conn)
+    }
+  }
+
+  @MainActor
+  func testAuth_GitHubFailureLanding() async throws {
+    let auth = request(
+      to: .auth(
+        .failureLanding(
+          accessToken: AccessToken(accessToken: "deadbeef"),
+          redirect: nil
+        )
+      )
+    )
+    let conn = connection(from: auth)
+    await assertSnapshot(matching: await siteMiddleware(conn), as: .conn)
+
+    #if !os(Linux)
+      if self.isScreenshotTestingAvailable {
+        await assertSnapshots(
+          matching: await siteMiddleware(conn),
+          as: [
+            "desktop": .connWebView(size: .init(width: 1100, height: 2000)),
+            "mobile": .connWebView(size: .init(width: 500, height: 2000)),
+          ]
+        )
+      }
+    #endif
+  }
+
+  @MainActor
+  func testAuth_UpdateGitHubAccount() async throws {
+    let didUpdateUser = self.expectation(description: "didUpdateUser")
+    await withDependencies {
+      $0.gitHub.fetchUser = { accessToken in
+        if accessToken.accessToken == "deadbeef-new" {
+          return GitHubUser(createdAt: Date(), login: "blob-new", id: 42, name: "Blob New")
+        } else {
+          XCTFail("Unrecognized access token.")
+          return .mock
+        }
+      }
+      $0.database.updateUser = { _, _, _, _, gitHubUserID, gitHubAccessToken, _ in
+        XCTAssertEqual(gitHubUserID, 42)
+        XCTAssertEqual(gitHubAccessToken, AccessToken(accessToken: "deadbeef-new"))
+        didUpdateUser.fulfill()
+      }
+    } operation: {
+      let auth = request(
+        to: .auth(
+          .updateGitHub(
+            accessToken: AccessToken(accessToken: "deadbeef-new"),
+            redirect: nil
+          )
+        )
+      )
+      let conn = connection(from: auth)
+      await assertInlineSnapshot(of: await siteMiddleware(conn), as: .conn) {
+        """
+        POST http://localhost:8080/update-github?acccess_token=deadbeef-new
+        Cookie: pf_session={}
+
+        302 Found
+        Location: /
+        Referrer-Policy: strict-origin-when-cross-origin
+        Set-Cookie: pf_session={"flash":{"message":"Your GitHub account has been updated to @blob-new.","priority":"notice"},"userId":"00000000-0000-0000-0000-000000000000"}; Expires=Sat, 29 Jan 2028 00:00:00 GMT; Path=/
+        X-Content-Type-Options: nosniff
+        X-Download-Options: noopen
+        X-Frame-Options: SAMEORIGIN
+        X-Permitted-Cross-Domain-Policies: none
+        X-XSS-Protection: 1; mode=block
+        """
+      }
+      await fulfillment(of: [didUpdateUser], timeout: 1)
     }
   }
 
