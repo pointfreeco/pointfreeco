fd18d870ef0e135ac340b77de8475196993478dc
Stephen Celis <stephen@stephencelis.com>
2025-05-06 10:54:15 -0700

    Update Sources/PointFreeTestSupport/TestCase.swift

--- Sources/PointFreeTestSupport/TestCase.swift
+++ Sources/PointFreeTestSupport/TestCase.swift
@@ -56,7 +56,7 @@ open class LiveDatabaseTestCase: XCTestCase {
   override open func setUp() async throws {
     try await super.setUp()
     diffTool = "ksdiff"
-    SnapshotTesting.isRecording = true
+    //SnapshotTesting.isRecording = true
     @Dependency(\.database) var database
     try await database.resetForTesting(pool: self.pool)
   }
