d943426b99f68358374792426a2a6fe47728b955
Brandon Williams <mbrandonw@hey.com>
2025-06-20 10:51:14 -0700

    Make chat iframe bigger.

--- Sources/Views/LiveView.swift
+++ Sources/Views/LiveView.swift
@@ -144,7 +144,7 @@ public func liveView() -> Node {
         ])
       ],
       .gridColumn(
-        sizes: [.mobile: 12, .desktop: 9],
+        sizes: [.mobile: 12, .desktop: 8],
         attributes: [
           .class([
             Class.grid.center(.desktop)
@@ -163,7 +163,7 @@ public func liveView() -> Node {
           """)
       ),
       .gridColumn(
-        sizes: [.mobile: 12, .desktop: 3],
+        sizes: [.mobile: 12, .desktop: 4],
         attributes: [
           .class([
             Class.grid.center(.desktop)
@@ -175,7 +175,7 @@ public func liveView() -> Node {
                   width="100%"
                   height="100%"
                   frameborder="0"
-                  style="min-height: 30rem;">
+                  style="min-height: 40rem;">
           </iframe>
           """)
       )
