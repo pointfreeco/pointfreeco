1407e8d0a9b6a725a691c745b0cc81e04c1c69cf
stephencelis <stephencelis@users.noreply.github.com>
2025-12-07 19:30:35 +0000

    Run swift-format

--- Sources/StyleguideV2/HTMLHelpers/HTMLMarkdown.swift
+++ Sources/StyleguideV2/HTMLHelpers/HTMLMarkdown.swift
@@ -482,7 +482,9 @@ private struct Prompt<Content: HTML>: HTML {
       .padding(topBottom: .small, leftRight: .small)
       .inlineStyle("border-radius", "22px")
       .inlineStyle("border", "3px solid rgba(255, 255, 255, 0.5)")
-      .background(PointFreeColor(rawValue: "rgba(255, 255, 255, 0.7)", darkValue: "rgba(15, 23, 42, 0.7)"))
+      .background(
+        PointFreeColor(rawValue: "rgba(255, 255, 255, 0.7)", darkValue: "rgba(15, 23, 42, 0.7)")
+      )
       .inlineStyle("backdrop-filter", "blur(12px)")
       .inlineStyle("box-shadow", "0 0 30px 10px rgba(255, 200, 255, 0.6)")
       .inlineStyle("font-weight", "500")
