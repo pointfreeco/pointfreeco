eea0e22e4046536f58ac25145a14cdbfd3da27b4
Stephen Celis <stephen@stephencelis.com>
2026-01-17 23:17:54 -0800

    wip

--- Sources/Views/PageLayoutViewV2.swift
+++ Sources/Views/PageLayoutViewV2.swift
@@ -1040,7 +1040,7 @@ public struct PrismJSHead: HTML {
         },
       });
       Prism.languages.sql.keyword = [
-        /\b(AND|DEFERRED|IS|NOT|PRAGMA|STRICT)\b/,
+        /\b(AND|DEFERRED|IMMEDIATE|IS|NOT|PRAGMA|STRICT)\b/,
         Prism.languages.sql.keyword
       ];
       """#
