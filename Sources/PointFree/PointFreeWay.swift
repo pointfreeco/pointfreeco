191c2f80d9f40a248552c29da6b33de99ac5b0d6
Brandon Williams <mbrandonw@hey.com>
2026-01-28 15:04:29 -0600

    cover image

--- Sources/PointFree/PointFreeWay.swift
+++ Sources/PointFree/PointFreeWay.swift
@@ -18,6 +18,7 @@ func pointFreeWayMiddleware(
           AI skill documents to best leverage Point-Free's open source libraries and embrace \
           the concepts and best patterns championed by Point-Free for years.
           """,
+        image: "https://imagedelivery.net/6_EEbfI_pxOPJCtc6OUKCg/28d2f776-ab34-449a-6852-18f038942500/public",
         title: "The Point-Free Way"
       )
     ) {
